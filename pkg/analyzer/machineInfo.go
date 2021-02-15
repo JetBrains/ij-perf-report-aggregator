86285fee2b624744294fd08557f04305c9a488eb
develar <develar@gmail.com>
2021-02-15 11:57:24 +0100

    add macmini with m1

--- pkg/analyzer/machineInfo.go
+++ pkg/analyzer/machineInfo.go
@@ -10,6 +10,9 @@ func GetMachineInfo() MachineInfo {
   // Mac mini Space Gray/3.0 GHz 6C/8GB/256GB; Model No. A1993; Part No. MRTT2RU/A; Serial No. C07XX9PFJYVX; Prod.12/2018, for code-sign (ADM-32069) -> ADM-35488
   const macMini = "macMini 2018"
 
+  // Mac Mini M1 Chip with 8‑Core CPU und 8‑Core GPU, SSD 256Gb, RAM 16Gb
+  const macMiniM1 = "macMini M1 2020"
+
   // Core i7-3770 16Gb, Intel SSD 535
   const win = "Windows: i7-3770, 16Gb, Intel SSD 535"
 
@@ -28,6 +31,8 @@ func GetMachineInfo() MachineInfo {
       "intellij-macos-hw-unit-1551": macMini,
       "intellij-macos-hw-unit-1772": macMini,
       "intellij-macos-hw-unit-1773": macMini,
+      "intellij-macos-hw-unit-2204": macMiniM1,
+      "intellij-macos-hw-unit-2205": macMiniM1,
 
       "intellij-windows-hw-unit-498": win,
       "intellij-windows-hw-unit-499": win,

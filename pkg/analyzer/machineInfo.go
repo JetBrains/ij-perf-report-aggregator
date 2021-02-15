c4656378753cbb343768a17a6d05c07100b8b994
develar <develar@gmail.com>
2021-02-15 17:17:04 +0100

    add macmini with m1 (UI)

--- pkg/analyzer/machineInfo.go
+++ pkg/analyzer/machineInfo.go
@@ -25,7 +25,7 @@ func GetMachineInfo() MachineInfo {
   const linux2 = "Linux: i7-3770, 16Gb (10600 1333MHz), SSD"
 
   return MachineInfo{
-    GroupNames: []string{macMini, linux, linux2, win, "linux-blade"},
+    GroupNames: []string{macMini, macMiniM1, linux, linux2, win, "linux-blade"},
     MachineToGroupName: map[string]string{
       "intellij-macos-hw-unit-1550": macMini,
       "intellij-macos-hw-unit-1551": macMini,

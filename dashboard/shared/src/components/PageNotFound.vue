b380aa530dd70e5914792df030e418cfbcdc9853
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2022-03-25 12:51:56 +0100

    PrimeVue PageNotFound

--- dashboard/shared/src/components/PageNotFound.vue
+++ dashboard/shared/src/components/PageNotFound.vue
@@ -1,28 +1,9 @@
 <template>
-  <div>Page not found.</div>
+  <Message
+    severity="error"
+    :sticky="true"
+    :closable="false"
+  >
+    Page Not Found
+  </Message>
 </template>
-<script lang="ts">
-// import "element-plus/theme-chalk/el-message.css"
-
-import { defineComponent, onMounted, onUnmounted } from "vue"
-
-export default defineComponent({
-  name: "PageNotFound",
-  setup() {
-    let messageHandle: MessageHandle | null = null
-    onMounted(() => {
-      messageHandle = ElMessage({
-        message: "Page Not Found",
-        type: "error",
-        duration: 0,
-        showClose: false,
-      })
-    })
-
-    onUnmounted(() => {
-      messageHandle?.close()
-    })
-    return {}
-  }
-})
-</script>

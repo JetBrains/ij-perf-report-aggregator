0fee57f965c651b9de9aaf6b2acfe0159f309eaa
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2022-03-30 10:03:43 +0200

    Fix missing line in divider

--- dashboard/app/src/main.ts
+++ dashboard/app/src/main.ts
@@ -4,9 +4,9 @@ import PrimeVue from "primevue/config"
 import { createApp } from "vue"
 import App from "./App.vue"
 import { createAndConfigureRouter } from "./route"
-import "primevue/resources/themes/tailwind-light/theme.css"
 import "primevue/resources/primevue.min.css"
 import "primeicons/primeicons.css"
+import "primevue/resources/themes/tailwind-light/theme.css"
 
 const app = createApp(App)
 app.use(createAndConfigureRouter())

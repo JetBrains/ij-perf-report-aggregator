d90134777f13289388465491b1150d197ad09bfd
develar <develar@gmail.com>
2022-03-31 08:16:23 +0200

    fix: reduce size of server selector, reduce base font size (14 px instead of 16px as some browsers use)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+import vue from "@vitejs/plugin-vue"
 // @ts-ignore
 import path from "path"
-import vue from "@vitejs/plugin-vue"
-import { PrimeVueResolver } from "unplugin-vue-components/resolvers"
-import { defineConfig } from "vite"
 import AutoImport from "unplugin-auto-import/vite"
+import { PrimeVueResolver, HeadlessUiResolver } from "unplugin-vue-components/resolvers"
 import Components from "unplugin-vue-components/vite"
+import { defineConfig } from "vite"
 import svgLoader from "vite-svg-loader"
 // import visualizer from "rollup-plugin-visualizer"
 
@@ -23,10 +23,29 @@ export default defineConfig({
     vue(),
     svgLoader(),
     AutoImport({
-      resolvers: [PrimeVueResolver() ],
+      resolvers: [PrimeVueResolver()],
     }),
     Components({
-      resolvers: [PrimeVueResolver()],
+      resolvers: [
+        PrimeVueResolver(),
+        HeadlessUiResolver(),
+        name => {
+          // @ts-ignore
+          const kind = process.env.NODE_ENV === "test" ? "" : "esm/"
+          if (name.endsWith("Icon")) {
+            return {
+              path: `@heroicons/vue/outline/${kind}${name}.js`,
+            }
+          }
+          else if (name.endsWith("IconSolid")) {
+            return {
+              path: `@heroicons/vue/solid/${kind}${name.substring(0, name.length - "Solid".length)}.js`,
+            }
+          }
+          else {
+            return null
+          }
+        }],
     }),
   ],
   root: "dashboard/app",

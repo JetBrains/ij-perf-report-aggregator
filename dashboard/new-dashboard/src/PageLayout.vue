0daffb5280480ba96779b0f374dc3d16731d96a1
Egor Ogarkov <egor.ogarkov@jetbrains.com>
2022-09-26 16:33:02 +0200

    Initial page with basic charts

---
+++ dashboard/new-dashboard/src/PageLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <PageHeader />
+  <slot />
+
+  <ChartTooltip
+    ref="tooltip"
+  />
+</template>
+<script setup lang="ts">
+import ChartTooltip from "shared/src/components/ChartTooltip.vue"
+import { chartToolTipKey } from "shared/src/injectionKeys"
+import { provide, ref } from "vue"
+import PageHeader from "./PageHeader.vue"
+
+const tooltip = ref<typeof ChartTooltip>()
+
+provide(chartToolTipKey, tooltip)
+</script>
---
+++ dashboard/new-dashboard/src/components/IntelliJMainDashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <TabView class="text-sm">
+    <TabPanel
+      header="Performance dashboard"
+    >
+      <PerformanceDashboard />
+    </TabPanel>
+    <TabPanel
+      header="Performance tests"
+    >
+      Content II
+    </TabPanel>
+    <TabPanel
+      header="Compare branches"
+    >
+      Content III
+    </TabPanel>
+  </TabView>
+</template>
+
+<script setup lang="ts">
+import TabPanel from "primevue/tabpanel"
+import TabView from "primevue/tabview"
+import PerformanceDashboard from "./inteliJ/PerformanceDashboard.vue"
+</script>

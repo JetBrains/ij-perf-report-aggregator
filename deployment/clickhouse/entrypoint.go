6a434e0fd250c8392235904e96aa6e7fc6ffcdc8
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-11-03 05:50:31 +0100

    feat: use S3 disk for clickhouse (part 10)

--- deployment/clickhouse/entrypoint.go
+++ deployment/clickhouse/entrypoint.go
@@ -14,7 +14,7 @@ import (
 var clickhouseConfig []byte
 
 func main() {
-  s3Url := "https://" + os.Getenv("S3_BUCKET") + ".s3.eu-west-1.amazonaws.com/data/"
+  s3Url := "https://" + getEnvOrFile("S3_BUCKET", "/etc/s3/bucket") + ".s3.eu-west-1.amazonaws.com/data/"
 
   s := strings.NewReplacer(
     "$S3_URL", s3Url,

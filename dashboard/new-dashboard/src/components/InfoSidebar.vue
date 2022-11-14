c1d5992ad9991ab5c5d49ce61bcb6b007c4441a0
Egor Ogarkov <egor.ogarkov@jetbrains.com>
2022-11-14 12:12:03 +0100

    [sidebar]: made it sticky to top

--- dashboard/new-dashboard/src/components/InfoSidebar.vue
+++ dashboard/new-dashboard/src/components/InfoSidebar.vue
@@ -1,9 +1,10 @@
 <template>
   <div
     v-show="vm.visible.value"
-    class="infoSidebar p-5 ml-5 border border-solid rounded-md border-zinc-200 text-gray-500 flex flex-col gap-4"
+    class="infoSidebar p-5 ml-5 border border-solid rounded-md border-zinc-200 text-gray-500 relative"
   >
-    <span class="flex justify-between uppercase text-xs">
+    <div class="flex flex-col gap-4 sticky top-5">
+      <span class="flex justify-between uppercase text-xs">
       {{ vm.data.value?.title }}
 
       <span
@@ -12,51 +13,52 @@
       />
     </span>
 
-    <div class="flex gap-1.5 font-medium text-base items-center">
+      <div class="flex gap-1.5 font-medium text-base items-center">
       <span
         class="w-3 h-3 rounded-full"
         :style="{ backgroundColor: vm.data.value?.color }"
       />
-      {{ vm.data.value?.projectName }}
-    </div>
+        {{ vm.data.value?.projectName }}
+      </div>
 
-    <div class="flex flex-col gap-2">
+      <div class="flex flex-col gap-2">
       <span class="flex gap-1.5 text-sm items-center">
         <CalendarIcon class="w-4 h-4" />
         {{ vm.data.value?.date }} build {{ vm.data.value?.build }}
       </span>
-      <span class="flex gap-1.5 text-sm items-center">
+        <span class="flex gap-1.5 text-sm items-center">
         <ClockIcon class="w-4 h-4" />
         {{ vm.data.value?.duration }}
       </span>
-      <span class="flex gap-1.5 text-sm items-center">
+        <span class="flex gap-1.5 text-sm items-center">
         <ComputerDesktopIcon class="w-4 h-4" />
         {{ vm.data.value?.machineName }}
       </span>
-    </div>
+      </div>
 
-    <div class="flex gap-4 text-blue-500">
-      <a
-        :href="vm.data.value?.changesUrl"
-        target="_blank"
-        class="flex gap-1.5 items-center transition duration-150 ease-out hover:text-blue-600"
-      >
-        <ArrowPathIcon class="w-4 h-4" /> Changes
-      </a>
-      <a
-        :href="vm.data.value?.artifactsUrl"
-        target="_blank"
-        class="flex gap-1.5 items-center transition duration-150 ease-out hover:text-blue-600"
-      >
-        <ServerStackIcon class="w-4 h-4" /> Artifacts
-      </a>
-      <a
-        :href="vm.data.value?.installerUrl"
-        target="_blank"
-        class="flex gap-1.5 items-center transition duration-150 ease-out hover:text-blue-600"
-      >
-        <ArrowDownTrayIcon class="w-4 h-4" /> Snapshot
-      </a>
+      <div class="flex gap-4 text-blue-500">
+        <a
+          :href="vm.data.value?.changesUrl"
+          target="_blank"
+          class="flex gap-1.5 items-center transition duration-150 ease-out hover:text-blue-600"
+        >
+          <ArrowPathIcon class="w-4 h-4" /> Changes
+        </a>
+        <a
+          :href="vm.data.value?.artifactsUrl"
+          target="_blank"
+          class="flex gap-1.5 items-center transition duration-150 ease-out hover:text-blue-600"
+        >
+          <ServerStackIcon class="w-4 h-4" /> Artifacts
+        </a>
+        <a
+          :href="vm.data.value?.installerUrl"
+          target="_blank"
+          class="flex gap-1.5 items-center transition duration-150 ease-out hover:text-blue-600"
+        >
+          <ArrowDownTrayIcon class="w-4 h-4" /> Snapshot
+        </a>
+      </div>
     </div>
   </div>
 </template>

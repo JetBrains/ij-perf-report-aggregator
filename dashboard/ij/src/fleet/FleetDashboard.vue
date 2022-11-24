2482fcf30ab97a2883205c34ac39a6b759dc764f
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2022-11-24 17:37:14 +0100

    Collect additional mode of startup for Fleet

--- dashboard/ij/src/fleet/FleetDashboard.vue
+++ dashboard/ij/src/fleet/FleetDashboard.vue
@@ -8,6 +8,11 @@
       <TimeRangeSelect :configurator="timeRangeConfigurator" />
     </template>
 
+    <div class="relative flex py-5 items-center">
+      <div class="flex-grow border-t border-gray-400" />
+      <span class="flex-shrink mx-4 text-gray-400 text-lg">Remote Mode</span>
+      <div class="flex-grow border-t border-gray-400" />
+    </div>
     <!-- :skip-zero-values="false" because computed measures cannot be filtered -->
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 pt-4">
       <LineChartCard
@@ -38,6 +43,44 @@
       <LineChartCard
         label="window appeared"
         :measures='["window appeared.end"]'
+        :skip-zero-values="false"
+      />
+    </div>
+
+    <div class="relative flex py-5 items-center">
+      <div class="flex-grow border-t border-gray-400" />
+      <span class="flex-shrink mx-4 text-gray-400 text-lg">ShortCircuit</span>
+      <div class="flex-grow border-t border-gray-400" />
+    </div>
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 pt-4">
+      <LineChartCard
+        label="editor appeared"
+        :measures='["shortCircuit.editor appeared.end"]'
+        :skip-zero-values="false"
+      />
+      <LineChartCard
+        label="time to edit"
+        :measures='["shortCircuit.time to edit.end"]'
+        :skip-zero-values="false"
+      />
+      <LineChartCard
+        label="terminal ready"
+        :measures='["shortCircuit.terminal ready.end"]'
+        :skip-zero-values="false"
+      />
+      <LineChartCard
+        label="file tree rendered"
+        :measures='["shortCircuit.file tree rendered.end"]'
+        :skip-zero-values="false"
+      />
+      <LineChartCard
+        label="highlighting done"
+        :measures='["shortCircuit.highlighting done.end"]'
+        :skip-zero-values="false"
+      />
+      <LineChartCard
+        label="window appeared"
+        :measures='["shortCircuit.window appeared.end"]'
         :skip-zero-values="false"
       />
     </div>

a6fae9bc018f8593e9d356d06aab908f4c4f7252
Anna Koehler <anna.marchenko@jetbrains.com>
2022-12-10 13:44:30 +0100

    Update names of use cases on Dashboard with performance metrics

--- dashboard/ij/src/idea/IdeaPerformanceDashboard.vue
+++ dashboard/ij/src/idea/IdeaPerformanceDashboard.vue
@@ -20,39 +20,39 @@
       <TimeRangeSelect :configurator="timeRangeConfigurator" />
     </template>
     <GroupLineChart
-      label="Indexing Long"
+      label="Indexing (Big projects)"
       measure="indexing"
       :projects="['community/indexing', 'intellij_sources/indexing']"
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Scanning Long"
+      label="Scanning (Big projects)"
       measure="scanning"
       :projects="['community/indexing', 'intellij_sources/indexing']"
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Number of indexing runs long"
+      label="Number of indexing runs long (Big projects)"
       measure="numberOfIndexingRuns"
       :projects="['community/indexing', 'intellij_sources/indexing']"
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Time Processing JAVA"
+      label="Processing speed of JAVA files"
       measure="processingSpeed#JAVA"
       :projects="['community/indexing', 'intellij_sources/indexing', 'empty_project/indexing', 'grails/indexing', 'java/indexing', 'kotlin/indexing', 'kotlin_coroutines/indexing',
                   'spring_boot/indexing', 'spring_boot_maven/indexing', 'kotlin_petclinic/indexing']"
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Time Processing KOTLIN"
+      label="Processing speed of KOTLIN files"
       measure="processingSpeed#Kotlin"
       :projects="['community/indexing', 'intellij_sources/indexing', 'empty_project/indexing', 'grails/indexing', 'java/indexing', 'kotlin/indexing', 'kotlin_coroutines/indexing',
                   'spring_boot/indexing', 'spring_boot_maven/indexing', 'kotlin_petclinic/indexing']"
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="VFS Record Storage Indexing"
+      label="Indexing with the new record storage (IntelliJ project)"
       measure="indexing"
       :projects="['vfs-record-storage/in-memory-intellij_sources/indexing', 'vfs-record-storage/in-memory-with-non-strict-names-intellij_sources/indexing',
       'vfs-record-storage/in-memory-with-non-strict-names-streamlined-attributes-intellij_sources/indexing', 'vfs-record-storage/in-memory-with-streamlined-attributes-intellij_sources/indexing',
@@ -60,7 +60,7 @@
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="VFS Record Storage Scanning"
+      label="Scanning with the new record storage (IntelliJ project)"
       measure="scanning"
       :projects="['vfs-record-storage/in-memory-intellij_sources/indexing', 'vfs-record-storage/in-memory-with-non-strict-names-intellij_sources/indexing',
       'vfs-record-storage/in-memory-with-non-strict-names-streamlined-attributes-intellij_sources/indexing', 'vfs-record-storage/in-memory-with-streamlined-attributes-intellij_sources/indexing',
@@ -68,7 +68,7 @@
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="VFS Record Storage Number of indexing runs"
+      label="Number of indexing runs with the new record storage (IntelliJ project)"
       measure="numberOfIndexingRuns"
       :projects="['vfs-record-storage/in-memory-intellij_sources/indexing', 'vfs-record-storage/in-memory-with-non-strict-names-intellij_sources/indexing',
       'vfs-record-storage/in-memory-with-non-strict-names-streamlined-attributes-intellij_sources/indexing', 'vfs-record-storage/in-memory-with-streamlined-attributes-intellij_sources/indexing',
@@ -76,21 +76,21 @@
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Indexing Short"
+      label="Indexing"
       measure="indexing"
       :projects="['empty_project/indexing', 'grails/indexing', 'java/indexing', 'kotlin/indexing', 'kotlin_coroutines/indexing', 
                   'spring_boot/indexing', 'spring_boot_maven/indexing', 'kotlin_petclinic/indexing']"
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Scanning Short"
+      label="Scanning"
       measure="scanning"
       :projects="['empty_project/indexing', 'grails/indexing', 'java/indexing', 'kotlin/indexing', 'kotlin_coroutines/indexing', 
                   'spring_boot/indexing', 'spring_boot_maven/indexing', 'kotlin_petclinic/indexing']"
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Number of indexing runs short"
+      label="Number of indexing runs"
       measure="numberOfIndexingRuns"
       :projects="['empty_project/indexing', 'grails/indexing', 'java/indexing', 'kotlin/indexing', 'kotlin_coroutines/indexing',
                   'spring_boot/indexing', 'spring_boot_maven/indexing', 'kotlin_petclinic/indexing']"
@@ -103,13 +103,13 @@
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Rebuild Long"
+      label="Rebuild (Big projects)"
       measure="build_compilation_duration"
       :projects="['community/rebuild','intellij_sources/rebuild']"
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Rebuild Short"
+      label="Rebuild"
       measure="build_compilation_duration"
       :projects="['grails/rebuild','java/rebuild','spring_boot/rebuild']"
       :server-configurator="serverConfigurator"
@@ -121,28 +121,43 @@
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Find Usages Before Compilation"
+      label="FindUsages PsiManager#getInstance Before and After Compilation"
       measure="findUsages"
-      :projects="['community/findUsages/PsiManager_getInstance_Before', 'community/findUsages/LocalInspectionTool_getID_Before',
-      'community/findUsages/ActionsKt_runReadAction_Before', 'community/findUsages/Application_runReadAction_Before',
-      'community/findUsages/Persistent_absolutePath_Before', 'community/findUsages/PropertyMapping_value_Before',
-      'community/findUsages/RelativeTextEdit_rangeTo_Before', 'community/findUsages/DynamicPluginListener_TOPIC_Before',
-      'community/findUsages/Path_div_Before'
+      :projects="['community/findUsages/PsiManager_getInstance_Before', 'community/findUsages/PsiManager_getInstance_After'
       ]"
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Find Usages After Compilation"
+      label="FindUsages Library#getName Before and After Compilation"
       measure="findUsages"
-      :projects="['community/findUsages/PsiManager_getInstance_After', 'community/findUsages/LocalInspectionTool_getID_After',
-      'community/findUsages/ActionsKt_runReadAction_After', 'community/findUsages/Application_runReadAction_After',
-      'community/findUsages/Persistent_absolutePath_After', 'community/findUsages/PropertyMapping_value_After',
-      'community/findUsages/RelativeTextEdit_rangeTo_After', 'community/findUsages/DynamicPluginListener_TOPIC_After',
-      'community/findUsages/Path_div_After']"
+      :projects="['community/findUsages/Library_getName_Before', 'community/findUsages/Library_getName_After'
+      ]"
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Find Usages with internal mode on Before Compilation"
+      label="FindUsages LocalInspectionTool#getID Before and After Compilation"
+      measure="findUsages"
+      :projects="['community/findUsages/LocalInspectionTool_getID_Before', 'community/findUsages/LocalInspectionTool_getID_After'
+      ]"
+      :server-configurator="serverConfigurator"
+    />
+    <GroupLineChart
+      label="FindUsages ActionsKt#runReadAction and Application#runReadAction Before and After Compilation"
+      measure="findUsages"
+      :projects="['community/findUsages/ActionsKt_runReadAction_Before', 'community/findUsages/Application_runReadAction_Before',
+      'community/findUsages/ActionsKt_runReadAction_After', 'community/findUsages/Application_runReadAction_After'
+      ]"
+      :server-configurator="serverConfigurator"
+    />
+    <GroupLineChart
+      label="FindUsages Persistent#absolutePath and PropertyMapping#value Before and After Compilation"
+      measure="findUsages"
+      :projects="['community/findUsages/Persistent_absolutePath_After', 'community/findUsages/PropertyMapping_value_After'
+      ]"
+      :server-configurator="serverConfigurator"
+    />
+    <GroupLineChart
+      label="Find Usages with idea.is.internal=true Before Compilation"
       measure="findUsages"
       :projects="['intellij_sources/findUsages/PsiManager_getInstance_firstCall', 'intellij_sources/findUsages/PsiManager_getInstance_secondCall', 'intellij_sources/findUsages/PsiManager_getInstance_thirdCallInternalMode']"
       :server-configurator="serverConfigurator"
@@ -160,26 +175,26 @@
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Go to Action"
+      label="Search Everywhere (Go to Action)"
       measure="searchEverywhere_action"
       :projects="['community/go-to-action/SharedIndex']"
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Go to Class"
+      label="Search Everywhere (Go to Class)"
       measure="searchEverywhere_class"
       :projects="['community/go-to-class/EditorImpl','community/go-to-class/SharedIndex']"
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Go to File"
+      label="Search Everywhere (Go to File)"
       measure="searchEverywhere_file"
       :projects="['community/go-to-file/EditorImpl','community/go-to-file/SharedIndex']"
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Show Intentions"
-      measure="test#max_awt_delay"
+      label="Show Intentions (average awt delay)"
+      measure="test#average_awt_delay"
       :projects="['grails/showIntentions/Find cause', 'kotlin/showIntention/Import', 'spring_boot/showIntentions/']"
       :server-configurator="serverConfigurator"
     />
@@ -196,8 +211,14 @@
       :server-configurator="serverConfigurator"
     />
     <GroupLineChart
-      label="Typing"
+      label="Typing during indexing (with changed count of indexing threads)"
       measure="typing"
+      :projects="['typingInJavaFile_16Threads/typing', 'typingInJavaFile_4Threads/typing', 'typingInKotlinFile_16Threads/typing', 'typingInKotlinFile_4Threads/typing']"
+      :server-configurator="serverConfigurator"
+    />
+    <GroupLineChart
+      label="Typing during indexing (average awt delay)"
+      measure="test#average_awt_delay"
       :projects="['typingInJavaFile_16Threads/typing', 'typingInJavaFile_4Threads/typing', 'typingInKotlinFile_16Threads/typing', 'typingInKotlinFile_4Threads/typing']"
       :server-configurator="serverConfigurator"
     />

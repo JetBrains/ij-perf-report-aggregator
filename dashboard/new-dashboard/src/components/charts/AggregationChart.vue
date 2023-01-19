bd7969a2aac8f85174d71f559d0da359ef1776eb
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-01-19 13:01:43 +0100

    Don't show ", ms" in case we have a counter

--- dashboard/new-dashboard/src/components/charts/AggregationChart.vue
+++ dashboard/new-dashboard/src/components/charts/AggregationChart.vue
@@ -9,7 +9,7 @@
         {{ vm.average }}
       </span>
       <div class="text-sm text-neutral-500 font-normal">
-        Avg, ms
+        Avg<div v-if="props.valueUnit !== 'counter'">, ms</div>
       </div>
     </div>
 

9d4d331e4f13a19419ecd37cb0b1b8d47ae6a29e
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-03-13 22:36:28 +0100

    Disable accidents until sqlite is not deployed

--- dashboard/new-dashboard/src/components/phpstorm/PerformanceDashboard.vue
+++ dashboard/new-dashboard/src/components/phpstorm/PerformanceDashboard.vue
@@ -26,11 +26,6 @@
         </DimensionHierarchicalSelect>
       </template>
     </Toolbar>
-    <AccidentWarning
-      :time-range-configurator="timeRangeConfigurator"
-      :branch-configurator="branchConfigurator"
-      :table="dbTable"
-    />
     <main class="flex">
       <div
         ref="container"
@@ -291,7 +286,6 @@ import InfoSidebar from "../InfoSidebar.vue"
 import { InfoSidebarVmImpl } from "../InfoSidebarVm"
 import AggregationChart from "../charts/AggregationChart.vue"
 import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
-import AccidentWarning from "../common/AccidentWarning.vue"
 import BranchSelect from "../common/BranchSelect.vue"
 import TimeRangeSelect from "../common/TimeRangeSelect.vue"
 

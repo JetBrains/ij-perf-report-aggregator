4cb52cb2d9e8ddc8a70a5beb409e2184e51fc28a
Anna Koehler <anna.marchenko@jetbrains.com>
2023-03-16 08:58:11 +0100

    new accidents were added to meta.sql

--- pkg/server/meta.sql
+++ pkg/server/meta.sql
@@ -8,5 +8,14 @@ create table accidents
   db_table      TEXT not null,
   build_number  TEXT
 );
+INSERT INTO accidents (date, affected_test, reason, branch, db_table, build_number) VALUES ('2023-01-13 20:00:00', 'community/go-to-class/EditorImpl', 'IDEA-310587: 1ba879cb4472', 'master', 'perfint', '231.5967');
+INSERT INTO accidents (date, affected_test, reason, branch, db_table, build_number) VALUES ('2023-01-17 00:00:00', 'community/go-to-class/EditorImpl', 'IDEA-311498: 1e4a7cd1c77d', 'master', 'perfint', '231.5362');
+INSERT INTO accidents (date, affected_test, reason, branch, db_table, build_number) VALUES ('2023-01-25 23:00:00', 'intellij_commit/findUsages', 'memory allocation was added to async profiling: 14de0ae', 'master', 'perfintDev', '');
+INSERT INTO accidents (date, affected_test, reason, branch, db_table, build_number) VALUES ('2023-02-01 05:00:00', 'community/go-to-action/SharedIndex', 'cleaning workspace cache has been added and later removed', 'master', 'perfint', '231.6544');
+INSERT INTO accidents (date, affected_test, reason, branch, db_table, build_number) VALUES ('2023-02-11 02:00:00', 'community/go-to-class/EditorImpl', 'changed logic of collecting SE metrics: 83bd9b9bc8bf', 'master', 'perfint', '231.7336');
+INSERT INTO accidents (date, affected_test, reason, branch, db_table, build_number) VALUES ('2023-02-14 23:00:00', 'intellij_commit/findUsages', 'head commit of the tested project was updated: 7e2691a', 'master', 'perfintDev', '');
+INSERT INTO accidents (date, affected_test, reason, branch, db_table, build_number) VALUES ('2023-02-16 16:00:00', 'intellij_commit/localInspection', 'IDEA-313677: e2bba17dbe42', 'master', 'perfintDev', '');
 INSERT INTO accidents (date, affected_test, reason, branch, db_table, build_number) VALUES ('2023-03-04 07:00:00', 'mediawiki/inspection', 'JSHint became initialized', 'master', 'phpstorm', '232.1409');
+INSERT INTO accidents (date, affected_test, reason, branch, db_table, build_number) VALUES ('2023-03-08 23:00:00', 'intellij_commit/completion/java_file', 'a38c21124132', 'master', 'perfintDev', '');
+
 SELECT * FROM accidents

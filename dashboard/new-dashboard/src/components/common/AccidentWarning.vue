02bab1ff5c2fad8b3341584894865f4e3d980deb
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-03-24 19:05:38 +0100

    Refactor meta access to a stand-alone functions

--- dashboard/new-dashboard/src/components/common/AccidentWarning.vue
+++ dashboard/new-dashboard/src/components/common/AccidentWarning.vue
@@ -15,11 +15,10 @@
 <script setup lang="ts">
 import { combineLatest, of } from "rxjs"
 import { DimensionConfigurator } from "shared/src/configurators/DimensionConfigurator"
-import { ServerConfigurator } from "shared/src/configurators/ServerConfigurator"
 import { TimeRange, TimeRangeConfigurator } from "shared/src/configurators/TimeRangeConfigurator"
 import { refToObservable } from "shared/src/configurators/rxjs"
-import { encodeRison } from "shared/src/rison"
 import { ref } from "vue"
+import { Accident, getWarningFromMetaDb } from "../meta"
 
 
 const props = defineProps<{
@@ -29,11 +28,6 @@ const props = defineProps<{
   timeRangeConfigurator: TimeRangeConfigurator
 }>()
 
-class Accident {
-  constructor(readonly affectedTest: string, readonly date: string, readonly reason: string, readonly id: number, readonly buildNumber: string) {
-  }
-}
-
 const warnings = ref<Array<Accident>>()
 
 const selected = props.scenarioConfigurator == null ? null : refToObservable(props.scenarioConfigurator.selected)
@@ -41,47 +35,7 @@ combineLatest([refToObservable(props.branchConfigurator.selected),
   refToObservable(props.timeRangeConfigurator.value),
   selected || of(null),
 ]).subscribe(data => {
-  getWarningFromMetaDb(props.branchConfigurator.selected.value, data[2], props.table)
+  getWarningFromMetaDb(warnings, props.branchConfigurator.selected.value, data[2], props.table, props.timeRangeConfigurator.value.value as TimeRange)
 })
-
-function isDateInsideRange(dateOfAccident: Date, interval: TimeRange): boolean {
-  const currentDate = new Date()
-  const day = 24 * 60 * 60 * 1000
-  const intervalMapping = {
-    "1M": day * 30,
-    "3M": day * 30 * 3,
-    "1y": day * 365,
-    "all": day * 365,
-  }
-  const selectedDate = new Date()
-  selectedDate.setTime(Date.now() - intervalMapping[interval])
-  return dateOfAccident >= selectedDate && dateOfAccident <= currentDate
-}
-
-function getWarningFromMetaDb(branches: Array<string> | string | null, tests: Array<string> | string | null, table: string) {
-  if (branches == null) {
-    return
-  }
-  if (!Array.isArray(branches)) {
-    branches = [branches]
-  }
-  if (tests != null && !Array.isArray(tests)) {
-    tests = [tests]
-  }
-  const url = ServerConfigurator.DEFAULT_SERVER_URL + "/api/meta/"
-  warnings.value = []
-  const data = tests == null ? {table, branches} : {table, branches, tests}
-  fetch(url + encodeRison(data))
-    .then(response => response.json())
-    .then((data: Array<Accident>) => {
-      for (const datum of data) {
-        if (isDateInsideRange(new Date(datum.date), props.timeRangeConfigurator.value.value as TimeRange)) {
-          warnings.value?.push(datum)
-        }
-      }
-    })
-    .catch(error => console.error(error))
-}
-
 </script>
 
---
+++ dashboard/new-dashboard/src/components/meta.ts
@@ -0,0 +1,51 @@
+import { ServerConfigurator } from "shared/src/configurators/ServerConfigurator"
+import { TimeRange } from "shared/src/configurators/TimeRangeConfigurator"
+import { encodeRison } from "shared/src/rison"
+import { Ref } from "vue"
+
+export class Accident {
+  constructor(readonly affectedTest: string, readonly date: string, readonly reason: string, readonly id: number, readonly buildNumber: string) {}
+}
+
+export function isDateInsideRange(dateOfAccident: Date, interval: TimeRange): boolean {
+  const currentDate = new Date()
+  const day = 24 * 60 * 60 * 1000
+  const intervalMapping = {
+    "1M": day * 30,
+    "3M": day * 30 * 3,
+    "1y": day * 365,
+    "all": day * 365,
+  }
+  const selectedDate = new Date()
+  selectedDate.setTime(Date.now() - intervalMapping[interval])
+  return dateOfAccident >= selectedDate && dateOfAccident <= currentDate
+}
+
+export function getWarningFromMetaDb(warnings: Ref<Array<Accident> | undefined>,
+                                     branches: Array<string> | string | null,
+                                     tests: Array<string> | string | null,
+                                     table: string,
+                                     timeRange: TimeRange) {
+  if (branches == null) {
+    return
+  }
+  if (!Array.isArray(branches)) {
+    branches = [branches]
+  }
+  if (tests != null && !Array.isArray(tests)) {
+    tests = [tests]
+  }
+  const url = ServerConfigurator.DEFAULT_SERVER_URL + "/api/meta/"
+  warnings.value = []
+  const data = tests == null ? {table, branches} : {table, branches, tests}
+  fetch(url + encodeRison(data))
+    .then(response => response.json())
+    .then((data: Array<Accident>) => {
+      for (const datum of data) {
+        if (isDateInsideRange(new Date(datum.date), timeRange)) {
+          warnings.value?.push(datum)
+        }
+      }
+    })
+    .catch(error => console.error(error))
+}

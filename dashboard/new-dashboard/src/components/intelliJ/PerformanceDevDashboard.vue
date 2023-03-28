0f1f7c17955d282dd166ecc2b780e9d30738bb1c
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-03-28 13:33:10 +0200

    Migrate to the new chart definition

--- dashboard/new-dashboard/src/components/intelliJ/PerformanceDevDashboard.vue
+++ dashboard/new-dashboard/src/components/intelliJ/PerformanceDevDashboard.vue
@@ -37,85 +37,14 @@
       >
         <section>
           <GroupProjectsChart
-            label="Indexing"
-            measure="indexing"
-            :projects="['intellij_sources/indexing', 'intellij_commit/indexing']"
+            v-for="chart in charts"
+            :key="chart.definition.label"
+            :label="chart.definition.label"
+            :measure="chart.definition.measure"
+            :projects="chart.projects"
             :server-configurator="serverConfigurator"
             :configurators="dashboardConfigurators"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Scanning"
-            measure="scanning"
-            :projects="['intellij_sources/indexing', 'intellij_commit/indexing']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Updating time"
-            measure="updatingTime"
-            :projects="['intellij_sources/indexing', 'intellij_commit/indexing']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Find Usages Java"
-            measure="findUsages"
-            :projects="['intellij_sources/findUsages/Application_runReadAction', 'intellij_sources/findUsages/LocalInspectionTool_getID',
-                        'intellij_sources/findUsages/PsiManager_getInstance', 'intellij_sources/findUsages/PropertyMapping_value',
-                        'intellij_commit/findUsages/Application_runReadAction', 'intellij_commit/findUsages/LocalInspectionTool_getID',
-                        'intellij_commit/findUsages/PsiManager_getInstance', 'intellij_commit/findUsages/PropertyMapping_value']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Find Usages Kotlin"
-            measure="findUsages"
-            :projects="['intellij_sources/findUsages/ActionsKt_runReadAction', 'intellij_sources/findUsages/DynamicPluginListener_TOPIC', 'intellij_sources/findUsages/Path_div',
-                        'intellij_sources/findUsages/Persistent_absolutePath', 'intellij_sources/findUsages/RelativeTextEdit_rangeTo',
-                        'intellij_sources/findUsages/TemporaryFolder_invoke', 'intellij_sources/findUsages/Project_guessProjectDir',
-                        'intellij_commit/findUsages/ActionsKt_runReadAction', 'intellij_commit/findUsages/DynamicPluginListener_TOPIC', 'intellij_commit/findUsages/Path_div',
-                        'intellij_commit/findUsages/Persistent_absolutePath', 'intellij_commit/findUsages/RelativeTextEdit_rangeTo',
-                        'intellij_commit/findUsages/TemporaryFolder_invoke', 'intellij_commit/findUsages/Project_guessProjectDir']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Local Inspection"
-            measure="localInspections"
-            :projects="['intellij_sources/localInspection/java_file','intellij_sources/localInspection/kotlin_file',
-                        'intellij_commit/localInspection/java_file','intellij_commit/localInspection/kotlin_file']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Completion: execution time"
-            measure="completion"
-            :projects="['intellij_sources/completion/java_file','intellij_sources/completion/kotlin_file',
-                        'intellij_commit/completion/java_file','intellij_commit/completion/kotlin_file']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Completion: awt delay"
-            measure="test#average_awt_delay"
-            :projects="['intellij_sources/completion/java_file','intellij_sources/completion/kotlin_file',
-                        'intellij_commit/completion/java_file','intellij_commit/completion/kotlin_file']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
+            :accidents="warnings"
           />
         </section>
       </div>
@@ -125,25 +54,66 @@
 </template>
 
 <script setup lang="ts">
+import { combineLatest } from "rxjs"
 import { PersistentStateManager } from "shared/src/PersistentStateManager"
 import DimensionHierarchicalSelect from "shared/src/components/DimensionHierarchicalSelect.vue"
 import { createBranchConfigurator } from "shared/src/configurators/BranchConfigurator"
 import { MachineConfigurator } from "shared/src/configurators/MachineConfigurator"
 import { privateBuildConfigurator } from "shared/src/configurators/PrivateBuildConfigurator"
 import { ServerConfigurator } from "shared/src/configurators/ServerConfigurator"
-import { TimeRangeConfigurator } from "shared/src/configurators/TimeRangeConfigurator"
+import { TimeRange, TimeRangeConfigurator } from "shared/src/configurators/TimeRangeConfigurator"
+import { refToObservable } from "shared/src/configurators/rxjs"
 import { provideReportUrlProvider } from "shared/src/lineChartTooltipLinkProvider"
+import { Accident, getWarningFromMetaDb } from "shared/src/meta"
 import { provide, ref } from "vue"
 import { useRouter } from "vue-router"
 import { containerKey, sidebarVmKey } from "../../shared/keys"
 import InfoSidebar from "../InfoSidebar.vue"
 import { InfoSidebarVmImpl } from "../InfoSidebarVm"
+import { ChartDefinition, combineCharts } from "../charts/DashboardCharts"
 import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
 import AccidentWarning from "../common/AccidentWarning.vue"
 import BranchSelect from "../common/BranchSelect.vue"
 import TimeRangeSelect from "../common/TimeRangeSelect.vue"
 
 provideReportUrlProvider(false)
+
+const chartsDeclaration: Array<ChartDefinition> = [{
+  labels: ["Indexing", "Scanning", "Updating time"],
+  measures: ["indexing", "scanning", "updatingTime"],
+  projects: ["intellij_sources/indexing", "intellij_commit/indexing"],
+}, {
+  labels: ["Find Usages Java"],
+  measures: ["findUsages"],
+  projects: ["intellij_sources/findUsages/Application_runReadAction", "intellij_sources/findUsages/LocalInspectionTool_getID",
+    "intellij_sources/findUsages/PsiManager_getInstance", "intellij_sources/findUsages/PropertyMapping_value",
+    "intellij_commit/findUsages/Application_runReadAction", "intellij_commit/findUsages/LocalInspectionTool_getID",
+    "intellij_commit/findUsages/PsiManager_getInstance", "intellij_commit/findUsages/PropertyMapping_value"],
+}, {
+  labels: ["Find Usages Kotlin"],
+  measures: ["findUsages"],
+  projects: ["intellij_sources/findUsages/ActionsKt_runReadAction", "intellij_sources/findUsages/DynamicPluginListener_TOPIC", "intellij_sources/findUsages/Path_div",
+    "intellij_sources/findUsages/Persistent_absolutePath", "intellij_sources/findUsages/RelativeTextEdit_rangeTo",
+    "intellij_sources/findUsages/TemporaryFolder_invoke", "intellij_sources/findUsages/Project_guessProjectDir",
+    "intellij_commit/findUsages/ActionsKt_runReadAction", "intellij_commit/findUsages/DynamicPluginListener_TOPIC", "intellij_commit/findUsages/Path_div",
+    "intellij_commit/findUsages/Persistent_absolutePath", "intellij_commit/findUsages/RelativeTextEdit_rangeTo",
+    "intellij_commit/findUsages/TemporaryFolder_invoke", "intellij_commit/findUsages/Project_guessProjectDir"],
+}, {
+  labels: ["Local Inspection"],
+  measures: ["localInspections"],
+  projects: ["intellij_sources/localInspection/java_file", "intellij_sources/localInspection/kotlin_file",
+    "intellij_commit/localInspection/java_file", "intellij_commit/localInspection/kotlin_file"],
+}, {
+  labels: ["Completion: execution time"],
+  measures: ["completion"],
+  projects: ["intellij_sources/completion/java_file", "intellij_sources/completion/kotlin_file",
+    "intellij_commit/completion/java_file", "intellij_commit/completion/kotlin_file"],
+}, {
+  labels: ["Completion: awt delay"],
+  measures: ["test#average_awt_delay"],
+  projects: ["intellij_sources/completion/java_file", "intellij_sources/completion/kotlin_file",
+    "intellij_commit/completion/java_file", "intellij_commit/completion/kotlin_file"],
+}]
 
 const dbName = "perfintDev"
 const dbTable = "idea"
@@ -188,6 +158,13 @@ const dashboardConfigurators = [
 function onChangeRange(value: string) {
   timeRangeConfigurator.value.value = value
 }
+
+const charts = combineCharts(chartsDeclaration)
+const projects = chartsDeclaration.map(it => it.projects).flat(Number.POSITIVE_INFINITY) as Array<string>
+const warnings = ref<Array<Accident>>()
+combineLatest([refToObservable(branchConfigurator.selected), refToObservable(timeRangeConfigurator.value), ]).subscribe(data => {
+  getWarningFromMetaDb(warnings, data[0], projects, dbName+"_"+dbTable, data[1] as TimeRange)
+})
 </script>
 
 <style>

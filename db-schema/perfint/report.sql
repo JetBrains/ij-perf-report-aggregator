99b623daf6edb8e424625368c13458a1e6afe4a8
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-04-10 22:07:33 +0200

    Optimize perfint tables

--- db-schema/perfint/report.sql
+++ db-schema/perfint/report.sql
@@ -10,7 +10,7 @@ create table idea2
   `tc_build_type`         LowCardinality(String) CODEC (ZSTD(20)),
 
   `measures.name`         Array(LowCardinality(String)) CODEC (ZSTD(20)),
-  `measures.value`        Array(Int32) CODEC (Gorilla, ZSTD(20)),
+  `measures.value`        Array(Int32) CODEC (ZSTD(20)),
   `measures.type`         Array(LowCardinality(String)) CODEC (ZSTD(20)),
 
   `build_c1`              UInt8 CODEC (DoubleDelta, ZSTD(20)),
@@ -20,5 +20,6 @@ create table idea2
   `triggeredBy`           LowCardinality(String) CODEC (ZSTD(20))
 )
   engine = MergeTree
-    partition by (toYear(generated_time))
-    order by (machine, branch, project, build_c1, build_c2, build_c3, build_time, generated_time)
+--     partitioning by month gives up to 3x performance boost
+    partition by (toYYYYMM(generated_time))
+    order by (machine, branch, project, build_c1, build_c2, build_c3, generated_time)

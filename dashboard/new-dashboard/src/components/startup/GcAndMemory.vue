016160b8b965d4c2e8c1e03dbee03a75f8cfae8d
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-05-09 14:56:57 +0200

    Add all plots from Pulse to GC and Metrics

--- dashboard/new-dashboard/src/components/startup/GcAndMemory.vue
+++ dashboard/new-dashboard/src/components/startup/GcAndMemory.vue
@@ -1,5 +1,54 @@
 <template>
   <StartupPage>
+    <Divider label="Bootstrap" />
+    <section class="grid grid-cols-2 gap-x-6">
+      <LineChartCard
+        :measures='["bootstrap_d", "appInitPreparation_d", "appInit_d", "pluginDescriptorLoading_d", "euaShowing_d", "appStarter_d"]'
+      />
+      <LineChartCard
+        :measures='["pluginDescriptorInitV18_d", "RunManager initialization"]'
+      />
+    </section>
+
+    <Divider label="Class and Resource Loading" />
+    <LineChartCard
+      :measures='["classLoadingTime", "classLoadingSearchTime", "classLoadingDefineTime"]'
+    />
+    <section class="grid grid-cols-2 gap-x-6">
+      <LineChartCard
+        :measures='["classLoadingCount", "resourceLoadingCount", "classLoadingPreparedCount", "classLoadingLoadedCount"]'
+      />
+      <LineChartCard
+        :measures='["metrics.classLoadingMetrics/inlineCount", "metrics.classLoadingMetrics/companionCount",
+                    "metrics.classLoadingMetrics/lambdaCount", "metrics.classLoadingMetrics/methodHandleCount"]'
+      />
+    </section>
+
+    <Divider label="Services" />
+    <section class="grid grid-cols-2 gap-x-6">
+      <LineChartCard
+        :skip-zero-values="false"
+        :measures='["serviceSyncPreloading_d", "serviceAsyncPreloading_d", "projectServiceSyncPreloading_d", "projectServiceAsyncPreloading_d"]'
+      />
+      <LineChartCard
+        :measures='["projectDumbAware", "appComponentCreation_d", "projectComponentCreation_d"]'
+      />
+    </section>
+
+    <Divider label="Post-opening" />
+    <section class="grid grid-cols-2 gap-x-6">
+      <LineChartCard
+        :measures='["editorRestoring", "editorRestoringTillPaint", "file opening in EDT"]'
+      />
+      <LineChartCard
+        :measures='["splash_i", "startUpCompleted", "metrics.totalOpeningTime/timeFromAppStartTillAnalysisFinished"]'
+      />
+    </section>
+
+    <Divider label="Exit" />
+    <LineChartCard
+      :measures='["metrics.exitMetrics/application.exit", "metrics.exitMetrics/saveSettingsOnExit", "metrics.exitMetrics/disposeProjects"]'
+    />
     <Divider label="Memory" />
     <LineChartCard
       :skip-zero-values="false"

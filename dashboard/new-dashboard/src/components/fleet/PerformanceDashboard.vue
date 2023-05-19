2695282b7025bbe71cd34be7feed305ce369bb4a
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-05-19 12:40:49 +0200

    (refactoring) Extract common dashboard page
    Extract webstorm and fleet perf dashboards

--- dashboard/new-dashboard/src/components/fleet/PerformanceDashboard.vue
+++ dashboard/new-dashboard/src/components/fleet/PerformanceDashboard.vue
@@ -1,229 +1,130 @@
 <template>
-  <div class="flex flex-col gap-5">
-    <Toolbar class="customToolbar">
-      <template #start>
-        <TimeRangeSelect
-          :ranges="TimeRangeConfigurator.timeRanges"
-          :value="timeRangeConfigurator.value.value"
-          :on-change="onChangeRange"
-        >
-          <template #icon>
-            <CalendarIcon class="w-4 h-4 text-gray-500" />
-          </template>
-        </TimeRangeSelect>
-        <BranchSelect
-          :branch-configurator="branchConfigurator"
-          :triggered-by-configurator="triggeredByConfigurator"
-        />
-        <DimensionHierarchicalSelect
-          label="Machine"
-          :dimension="machineConfigurator"
-        >
-          <template #icon>
-            <ComputerDesktopIcon class="w-4 h-4 text-gray-500" />
-          </template>
-        </DimensionHierarchicalSelect>
-      </template>
-    </Toolbar>
-
-    <main class="flex">
-      <div
-        ref="container"
-        class="flex flex-1 flex-col gap-6 overflow-hidden"
-      >
-        <Divider title="Core" />
-        <section>
-          <GroupProjectsChart
-            label="Typing (time)"
-            measure="fleet.test"
-            :projects="['multiCaretTyping', 'stressEnter', 'stressTyping']"
-            value-unit="ns"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-            :accidents="warnings"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Open Project"
-            measure="fleet.test"
-            value-unit="ns"
-            :projects="['openGoDelveProject', 'openJeditermProject', 'openRustSimpleServerProject', 'openSpringPetClinicJavaProject', 'openSpringPetClinicKotlinProject']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-            :accidents="warnings"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Typing (average delay)"
-            measure="awt.delay"
-            value-unit="ns"
-            :projects="['multiCaretTyping', 'stressEnter', 'stressTyping']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-            :accidents="warnings"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Typing (max delay)"
-            measure="max.awt.delay"
-            value-unit="ns"
-            :projects="['multiCaretTyping', 'stressEnter', 'stressTyping']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-            :accidents="warnings"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Highlighting"
-            value-unit="ns"
-            measure="fleet.test"
-            :projects="['stressHighlighting']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-            :accidents="warnings"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Tree"
-            value-unit="ns"
-            measure="fleet.test"
-            :projects="['wideTree', 'deepTree']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-            :accidents="warnings"
-          />
-        </section>
-        <Divider title="PHP" />
-        <section>
-          <GroupProjectsChart
-            label="Typing (time)"
-            value-unit="ns"
-            measure="fleet.test"
-            :projects="['Typing in mPDF', 'Typing in mPDF With Backend', 'Pressing Enter in mPDF']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-            :accidents="warnings"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Typing (average delay)"
-            value-unit="ns"
-            measure="awt.delay"
-            :projects="['Typing in mPDF', 'Typing in mPDF With Backend', 'Pressing Enter in mPDF']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-            :accidents="warnings"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Typing (max delay)"
-            value-unit="ns"
-            measure="max.awt.delay"
-            :projects="['Typing in mPDF', 'Typing in mPDF With Backend', 'Pressing Enter in mPDF']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-            :accidents="warnings"
-          />
-        </section>
-        <section>
-          <GroupProjectsChart
-            label="Other"
-            value-unit="ns"
-            measure="fleet.test"
-            :projects="['Open mPDF', 'Frontend Completion in mPDF']"
-            :server-configurator="serverConfigurator"
-            :configurators="dashboardConfigurators"
-            :accidents="warnings"
-          />
-        </section>
-      </div>
-      <InfoSidebar />
-    </main>
-  </div>
+  <DashboardPage
+    v-slot="{serverConfigurator, dashboardConfigurators, warnings}"
+    db-name="fleet"
+    table="measure"
+    persistent-id="fleetPerf_dashboard"
+    initial-machine="linux-blade-hetzner"
+    :with-installer="false"
+  >
+    <Divider title="Core" />
+    <section>
+      <GroupProjectsChart
+        label="Typing (time)"
+        measure="fleet.test"
+        :projects="['multiCaretTyping', 'stressEnter', 'stressTyping']"
+        value-unit="ns"
+        :server-configurator="serverConfigurator"
+        :configurators="dashboardConfigurators"
+        :accidents="warnings"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Open Project"
+        measure="fleet.test"
+        value-unit="ns"
+        :projects="['openGoDelveProject', 'openJeditermProject', 'openRustSimpleServerProject', 'openSpringPetClinicJavaProject', 'openSpringPetClinicKotlinProject']"
+        :server-configurator="serverConfigurator"
+        :configurators="dashboardConfigurators"
+        :accidents="warnings"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Typing (average delay)"
+        measure="awt.delay"
+        value-unit="ns"
+        :projects="['multiCaretTyping', 'stressEnter', 'stressTyping']"
+        :server-configurator="serverConfigurator"
+        :configurators="dashboardConfigurators"
+        :accidents="warnings"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Typing (max delay)"
+        measure="max.awt.delay"
+        value-unit="ns"
+        :projects="['multiCaretTyping', 'stressEnter', 'stressTyping']"
+        :server-configurator="serverConfigurator"
+        :configurators="dashboardConfigurators"
+        :accidents="warnings"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Highlighting"
+        value-unit="ns"
+        measure="fleet.test"
+        :projects="['stressHighlighting']"
+        :server-configurator="serverConfigurator"
+        :configurators="dashboardConfigurators"
+        :accidents="warnings"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Tree"
+        value-unit="ns"
+        measure="fleet.test"
+        :projects="['wideTree', 'deepTree']"
+        :server-configurator="serverConfigurator"
+        :configurators="dashboardConfigurators"
+        :accidents="warnings"
+      />
+    </section>
+    <Divider title="PHP" />
+    <section>
+      <GroupProjectsChart
+        label="Typing (time)"
+        value-unit="ns"
+        measure="fleet.test"
+        :projects="['Typing in mPDF', 'Typing in mPDF With Backend', 'Pressing Enter in mPDF']"
+        :server-configurator="serverConfigurator"
+        :configurators="dashboardConfigurators"
+        :accidents="warnings"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Typing (average delay)"
+        value-unit="ns"
+        measure="awt.delay"
+        :projects="['Typing in mPDF', 'Typing in mPDF With Backend', 'Pressing Enter in mPDF']"
+        :server-configurator="serverConfigurator"
+        :configurators="dashboardConfigurators"
+        :accidents="warnings"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Typing (max delay)"
+        value-unit="ns"
+        measure="max.awt.delay"
+        :projects="['Typing in mPDF', 'Typing in mPDF With Backend', 'Pressing Enter in mPDF']"
+        :server-configurator="serverConfigurator"
+        :configurators="dashboardConfigurators"
+        :accidents="warnings"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Other"
+        value-unit="ns"
+        measure="fleet.test"
+        :projects="['Open mPDF', 'Frontend Completion in mPDF']"
+        :server-configurator="serverConfigurator"
+        :configurators="dashboardConfigurators"
+        :accidents="warnings"
+      />
+    </section>
+  </DashboardPage>>
 </template>
 
 <script setup lang="ts">
-import { PersistentStateManager } from "shared/src/PersistentStateManager"
-import DimensionHierarchicalSelect from "shared/src/components/DimensionHierarchicalSelect.vue"
-import { createBranchConfigurator } from "shared/src/configurators/BranchConfigurator"
-import { MachineConfigurator } from "shared/src/configurators/MachineConfigurator"
-import { privateBuildConfigurator } from "shared/src/configurators/PrivateBuildConfigurator"
-import { ServerConfigurator } from "shared/src/configurators/ServerConfigurator"
-import { TimeRange, TimeRangeConfigurator } from "shared/src/configurators/TimeRangeConfigurator"
-import { refToObservable } from "shared/src/configurators/rxjs"
-import { provideReportUrlProvider } from "shared/src/lineChartTooltipLinkProvider"
-import { Accident, getAccidentsFromMetaDb } from "shared/src/meta"
-import { provide, ref } from "vue"
-import { useRouter } from "vue-router"
-import { containerKey, sidebarVmKey } from "../../shared/keys"
-import InfoSidebar from "../InfoSidebar.vue"
-import { InfoSidebarVmImpl } from "../InfoSidebarVm"
 import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
-import BranchSelect from "../common/BranchSelect.vue"
+import DashboardPage from "../common/DashboardPage.vue"
 import Divider from "../common/Divider.vue"
-import TimeRangeSelect from "../common/TimeRangeSelect.vue"
 
-provideReportUrlProvider(false)
-
-const dbName = "fleet"
-const dbTable = "measure"
-const initialMachine = "linux-blade-hetzner"
-const container = ref<HTMLElement>()
-const router = useRouter()
-const sidebarVm = new InfoSidebarVmImpl()
-
-provide(containerKey, container)
-provide(sidebarVmKey, sidebarVm)
-
-const serverConfigurator = new ServerConfigurator(dbName, dbTable)
-const persistenceForDashboard = new PersistentStateManager("fleetPerf_dashboard", {
-  machine: initialMachine,
-  project: [],
-  branch: "master",
-}, router)
-
-const timeRangeConfigurator = new TimeRangeConfigurator(persistenceForDashboard)
-
-const branchConfigurator = createBranchConfigurator(serverConfigurator, persistenceForDashboard, [timeRangeConfigurator])
-const machineConfigurator = new MachineConfigurator(
-  serverConfigurator,
-  persistenceForDashboard,
-  [timeRangeConfigurator, branchConfigurator],
-)
-const triggeredByConfigurator = privateBuildConfigurator(
-  serverConfigurator,
-  persistenceForDashboard,
-  [branchConfigurator, timeRangeConfigurator],
-)
-
-const dashboardConfigurators = [
-  branchConfigurator,
-  machineConfigurator,
-  timeRangeConfigurator,
-  triggeredByConfigurator,
-]
-
-function onChangeRange(value: TimeRange) {
-  timeRangeConfigurator.value.value = value
-}
-
-const warnings = ref<Array<Accident>>()
-refToObservable(timeRangeConfigurator.value).subscribe(data => {
-  getAccidentsFromMetaDb(warnings, null, data)
-})
 </script>
-
-<style>
-.customToolbar {
-  background-color: transparent;
-  border: none;
-  padding: 0;
-}
-</style>

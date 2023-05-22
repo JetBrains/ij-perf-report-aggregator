3a0f9d3efcd3a9af9af27f9ff8f957f8db938fee
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2023-05-22 13:24:15 +0300

    Reformatting Maven dashboard

--- dashboard/new-dashboard/src/components/intelliJ/MavenImportPerformanceDashboard.vue
+++ dashboard/new-dashboard/src/components/intelliJ/MavenImportPerformanceDashboard.vue
@@ -15,7 +15,7 @@
         :projects="chart.projects"
       />
     </section>
-  </DashboardPage>>
+  </DashboardPage>
 </template>
 
 <script setup lang="ts">
@@ -147,12 +147,18 @@ const chartsDeclaration: Array<ChartDefinition> = metricsDeclaration.map(metric 
     measures: [metric],
     projects: [
       "project-import-maven-quarkus/measureStartup",
-      "project-import-maven-500-modules/measureStartup", "project-import-maven-1000-modules/measureStartup",
-      "project-import-maven-keycloak/measureStartup", "project-import-maven-javaee7/measureStartup",
-      "project-import-maven-javaee8/measureStartup", "project-import-maven-jersey/measureStartup",
-      "project-import-maven-flink/measureStartup", "project-import-maven-drill/measureStartup",
-      "project-import-maven-azure-sdk-java/measureStartup", "project-import-maven-hive/measureStartup",
-      "project-import-maven-quarkus-to-legacy-model/measureStartup", "project-import-maven-500-modules-to-legacy-model/measureStartup",
+      "project-import-maven-500-modules/measureStartup",
+      "project-import-maven-1000-modules/measureStartup",
+      "project-import-maven-keycloak/measureStartup",
+      "project-import-maven-javaee7/measureStartup",
+      "project-import-maven-javaee8/measureStartup",
+      "project-import-maven-jersey/measureStartup",
+      "project-import-maven-flink/measureStartup",
+      "project-import-maven-drill/measureStartup",
+      "project-import-maven-azure-sdk-java/measureStartup",
+      "project-import-maven-hive/measureStartup",
+      "project-import-maven-quarkus-to-legacy-model/measureStartup",
+      "project-import-maven-500-modules-to-legacy-model/measureStartup",
       "project-import-maven-1000-modules-to-legacy-model/measureStartup",
     ],
   }

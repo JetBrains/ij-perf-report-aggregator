1cb4192b4580ef4f7e99e8ea4a6205c5fda4bea0
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-06-01 21:26:48 +0200

    Let's try to revert one suspicios commit that closes client on query cancel

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -156,7 +156,6 @@ func createStoreForDatabaseUnderLock(name string, t *StatsServer) (*puddle.Pool[
       client, err := ch.Dial(ctx, ch.Options{
         Address:  t.dbUrl,
         Database: name,
-        Logger:   t.logger,
         Settings: []ch.Setting{
           ch.SettingInt("readonly", 1),
           ch.SettingInt("max_query_size", 1000000),

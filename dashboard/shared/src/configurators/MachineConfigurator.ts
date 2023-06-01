f1e173985434b618629c24daf596f8e006167277
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-06-01 22:57:24 +0200

    Revert "Wait till we loaded all machines before starting to emit values"
    This reverts commit 542be65bfdd3654039cf2213aaf1c1cecf313386.

--- dashboard/shared/src/configurators/MachineConfigurator.ts
+++ dashboard/shared/src/configurators/MachineConfigurator.ts
@@ -1,5 +1,5 @@
 import { deepEqual } from "fast-equals"
-import { distinctUntilChanged, Observable, switchMap,  withLatestFrom } from "rxjs"
+import { combineLatest, distinctUntilChanged, Observable, switchMap } from "rxjs"
 import { shallowRef } from "vue"
 import { PersistentStateManager } from "../PersistentStateManager"
 import { DataQuery, DataQueryConfigurator, DataQueryExecutorConfiguration, DataQueryFilter, toArray } from "../dataQuery"
@@ -20,13 +20,15 @@ export class MachineConfigurator implements DataQueryConfigurator, FilterConfigu
   readonly state = createComponentState()
   private readonly groupNameToItem = new Map<string, GroupedDimensionValue>()
 
+  private static readonly valueToGroup: { [key: string]: string } = getValueToGroup()
+
   constructor(serverConfigurator: ServerConfigurator, persistentStateManager: PersistentStateManager, filters: Array<FilterConfigurator> = [], readonly multiple: boolean = true) {
     const name = "machine"
     persistentStateManager.add(name, this.selected, it => toArray(it as never))
     const listObservable = createFilterObservable(serverConfigurator, filters)
       .pipe(
         switchMap(() => loadDimension(name, serverConfigurator, filters, this.state)),
-        updateComponentState(this.state)
+        updateComponentState(this.state),
       )
     listObservable.subscribe(data => {
       if (data == null) {
@@ -38,10 +40,14 @@ export class MachineConfigurator implements DataQueryConfigurator, FilterConfigu
     })
 
     // selected value may be a group name, so, we must re-execute query on machine list update
-    this.observable = listObservable.pipe(
-      withLatestFrom(refToObservable(this.selected, true)),
-      distinctUntilChanged(deepEqual)
+    this.observable = combineLatest([
+      refToObservable(this.selected, true),
+      listObservable,
+    ]).pipe(
+      distinctUntilChanged(deepEqual),
     )
+    // init groupNameToItem - if actual machine list is not yet loaded, but there is stored value for filter, use it to draw chart
+    this.groupValues(Object.keys(MachineConfigurator.valueToGroup))
   }
 
   createObservable(): Observable<unknown> {
@@ -98,6 +104,9 @@ export class MachineConfigurator implements DataQueryConfigurator, FilterConfigu
         }
         else if (value.startsWith("intellij-linux-performance-huge-aws-i")){
           groupName = "Linux EC2 C6id.metal (128 CPU Xeon, 256 GB)"
+        }
+        else {
+          groupName = MachineConfigurator.valueToGroup[value]
         }
 
         if (groupName == null) {
@@ -228,4 +237,104 @@ function prefix(words: Array<string>):string{
   while(words[0][i] && words.every(w => w[i] === words[0][i]))
     i++
   return words[0].slice(0, Math.max(0, i))
+}
+
+function getValueToGroup() {
+  // Mac mini Space Gray/3.0 GHz 6C/8GB/256GB
+  const macMini = "macMini 2018"
+  const macMiniIntel = "macMini Intel 3.2, 16GB"
+  // Mac Mini M1 Chip with 8‑Core CPU und 8‑Core GPU, SSD 256Gb, RAM 16Gb
+  const macMiniM1 = "macMini M1 2020"
+  const macMiniM1_16 = "macMini M1, 16GB"
+
+  // Core i7-3770 16Gb, Intel SSD 535
+  const win = "Windows Space i7-3770, 16Gb"
+
+  // old RAM	RAM	RAM type	CPU	CPU CLOCK	MotherBoard	HDDs
+
+  // 16384 Mb	16384 Mb	2xDDR3-12800 1600MHz 8Gb(8192Mb)	Core i7-3770	3400 Mhz	Intel DH77EB	240 Gb
+  const linux = "Linux Space i7-3770, 16Gb"
+
+  const blade = "linux-blade"
+
+  return {
+    "intellij-macos-hw-unit-1550": macMini,
+    "intellij-macos-hw-unit-1551": macMini,
+    "intellij-macos-hw-unit-1772": macMini,
+    "intellij-macos-hw-unit-1773": macMini,
+
+    "intellij-macos-hw-munit-716": macMiniIntel,
+    "intellij-macos-hw-munit-721": macMiniIntel,
+    "intellij-macos-hw-munit-722": macMiniIntel,
+    "intellij-macos-hw-munit-723": macMiniIntel,
+    "intellij-macos-hw-munit-724": macMiniIntel,
+
+    "intellij-macos-hw-munit-608": macMiniM1_16,
+    "intellij-macos-hw-munit-689": macMiniM1_16,
+    "intellij-macos-hw-munit-690": macMiniM1_16,
+    "intellij-macos-hw-munit-691": macMiniM1_16,
+    "intellij-macos-hw-munit-692": macMiniM1_16,
+    "intellij-macos-hw-munit-693": macMiniM1_16,
+    "intellij-macos-hw-munit-694": macMiniM1_16,
+    "intellij-macos-hw-munit-695": macMiniM1_16,
+    "intellij-macos-hw-munit-696": macMiniM1_16,
+    "intellij-macos-hw-munit-697": macMiniM1_16,
+    "intellij-macos-hw-munit-698": macMiniM1_16,
+
+    "intellij-macos-hw-unit-2204": macMiniM1,
+    "intellij-macos-hw-unit-2205": macMiniM1,
+    "intellij-macos-hw-unit-2206": macMiniM1,
+    "intellij-macos-hw-unit-2207": macMiniM1,
+
+    "intellij-macos-unit-2200-large-10298": macLarge,
+
+    "intellij-windows-hw-unit-498": win,
+    "intellij-windows-hw-unit-499": win,
+    "intellij-windows-hw-unit-449": win,
+    "intellij-windows-hw-unit-463": win,
+    "intellij-windows-hw-unit-493": win,
+    "intellij-windows-hw-unit-504": win,
+
+    "intellij-linux-hw-unit-449": linux,
+    "intellij-linux-hw-unit-499": linux,
+    "intellij-linux-hw-unit-450": linux,
+    "intellij-linux-hw-unit-484": linux,
+
+    // error in info table - only 16GB ram and not 32
+    "intellij-linux-hw-unit-493": linux,
+
+    "intellij-linux-hw-unit-504": linux,
+    "intellij-linux-hw-unit-531": linux,
+    "intellij-linux-hw-unit-534": linux,
+    "intellij-linux-hw-unit-556": linux,
+    "intellij-linux-hw-unit-558": linux,
+
+    "intellij-linux-hw-blade-023": blade,
+    "intellij-linux-hw-blade-024": blade,
+    "intellij-linux-hw-blade-025": blade,
+    "intellij-linux-hw-blade-026": blade,
+    "intellij-linux-hw-blade-027": blade,
+    "intellij-linux-hw-blade-028": blade,
+    "intellij-linux-hw-blade-029": blade,
+    "intellij-linux-hw-blade-030": blade,
+    "intellij-linux-hw-blade-031": blade,
+    "intellij-linux-hw-blade-032": blade,
+    "intellij-linux-hw-blade-033": blade,
+    "intellij-linux-hw-blade-034": blade,
+    "intellij-linux-hw-blade-035": blade,
+    "intellij-linux-hw-blade-036": blade,
+    "intellij-linux-hw-blade-037": blade,
+    "intellij-linux-hw-blade-038": blade,
+    "intellij-linux-hw-blade-039": blade,
+    "intellij-linux-hw-blade-040": blade,
+    "intellij-linux-hw-blade-041": blade,
+    "intellij-linux-hw-blade-042": blade,
+    "intellij-linux-hw-blade-043": blade,
+    "intellij-linux-hw-blade-044": blade,
+    "intellij-linux-hw-blade-045": blade,
+    "intellij-linux-hw-blade-046": blade,
+    "intellij-linux-hw-blade-047": blade,
+    "intellij-linux-hw-blade-048": blade,
+    "intellij-linux-hw-blade-049": blade,
+  }
 }

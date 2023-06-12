ecfd83f5bf71931dd2b440ec6a420076f56bff89
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-06-12 18:27:40 +0200

    chore: update clickhouse-backup

--- cmd/clickhouse-backup/main.go
+++ cmd/clickhouse-backup/main.go
@@ -1,134 +1,134 @@
 package main
 
 import (
-  "context"
-  "fmt"
-  "github.com/AlexAkulov/clickhouse-backup/pkg/backup"
-  "github.com/AlexAkulov/clickhouse-backup/pkg/status"
-  clickhousebackup "github.com/JetBrains/ij-perf-report-aggregator/pkg/clickhouse-backup"
-  "github.com/JetBrains/ij-perf-report-aggregator/pkg/util"
-  "github.com/develar/errors"
-  "github.com/nats-io/nats.go"
-  "go.deanishe.net/env"
-  "go.uber.org/zap"
-  "os"
-  "time"
+	"context"
+	"fmt"
+	"github.com/Altinity/clickhouse-backup/pkg/backup"
+	"github.com/Altinity/clickhouse-backup/pkg/status"
+	clickhousebackup "github.com/JetBrains/ij-perf-report-aggregator/pkg/clickhouse-backup"
+	"github.com/JetBrains/ij-perf-report-aggregator/pkg/util"
+	"github.com/develar/errors"
+	"github.com/nats-io/nats.go"
+	"go.deanishe.net/env"
+	"go.uber.org/zap"
+	"os"
+	"time"
 )
 
 func main() {
-  logger := util.CreateLogger()
-  defer func() {
-    _ = logger.Sync()
-  }()
+	logger := util.CreateLogger()
+	defer func() {
+		_ = logger.Sync()
+	}()
 
-  err := start("nats://"+env.Get("NATS", "nats:4222"), logger)
-  if err != nil {
-    logger.Fatal(fmt.Sprintf("%+v", err))
-  }
+	err := start("nats://"+env.Get("NATS", "nats:4222"), logger)
+	if err != nil {
+		logger.Fatal(fmt.Sprintf("%+v", err))
+	}
 }
 
 func start(natsUrl string, logger *zap.Logger) error {
-  taskContext, cancel := util.CreateCommandContext()
-  defer cancel()
+	taskContext, cancel := util.CreateCommandContext()
+	defer cancel()
 
-  if len(os.Getenv("KUBERNETES_SERVICE_HOST")) == 0 {
-    clickhousebackup.SetS3EnvForLocalRun()
-  }
+	if len(os.Getenv("KUBERNETES_SERVICE_HOST")) == 0 {
+		clickhousebackup.SetS3EnvForLocalRun()
+	}
 
-  backuper := clickhousebackup.CreateBackuper()
+	backuper := clickhousebackup.CreateBackuper()
 
-  if env.GetBool("DO_BACKUP") {
-    err := executeBackup(taskContext, backuper, true, logger)
-    return err
-  }
+	if env.GetBool("DO_BACKUP") {
+		err := executeBackup(taskContext, backuper, true, logger)
+		return err
+	}
 
-  logger.Info("started", zap.String("nats", natsUrl))
-  nc, err := nats.Connect(natsUrl)
-  if err != nil {
-    return errors.WithStack(err)
-  }
+	logger.Info("started", zap.String("nats", natsUrl))
+	nc, err := nats.Connect(natsUrl)
+	if err != nil {
+		return errors.WithStack(err)
+	}
 
-  sub, err := nc.SubscribeSync("db.backup")
-  if err != nil {
-    return errors.WithStack(err)
-  }
+	sub, err := nc.SubscribeSync("db.backup")
+	if err != nil {
+		return errors.WithStack(err)
+	}
 
-  lastBackupTime := time.Time{}
-  incrementalBackupCount := 0
-  isIncremental := true
-  for taskContext.Err() == nil {
-    _, err = sub.NextMsgWithContext(taskContext)
-    if err != nil {
-      contextError := taskContext.Err()
-      if contextError != nil {
-        logger.Info("cancelled", zap.NamedError("reason", contextError))
-        return nil
-      }
-      return errors.WithStack(contextError)
-    }
+	lastBackupTime := time.Time{}
+	incrementalBackupCount := 0
+	isIncremental := true
+	for taskContext.Err() == nil {
+		_, err = sub.NextMsgWithContext(taskContext)
+		if err != nil {
+			contextError := taskContext.Err()
+			if contextError != nil {
+				logger.Info("cancelled", zap.NamedError("reason", contextError))
+				return nil
+			}
+			return errors.WithStack(contextError)
+		}
 
-    if taskContext.Err() != nil {
-      return nil
-    }
+		if taskContext.Err() != nil {
+			return nil
+		}
 
-    if time.Since(lastBackupTime) < 4*time.Hour {
-      // do not create backups too often
-      logger.Info("backup request skipped", zap.String("reason", "time threshold"), zap.Time("lastBackupTime", lastBackupTime))
-      continue
-    }
+		if time.Since(lastBackupTime) < 4*time.Hour {
+			// do not create backups too often
+			logger.Info("backup request skipped", zap.String("reason", "time threshold"), zap.Time("lastBackupTime", lastBackupTime))
+			continue
+		}
 
-    logger.Info("backup requested")
-    err = executeBackup(taskContext, backuper, isIncremental, logger)
-    if err != nil {
-      logger.Error("cannot backup", zap.Error(err))
-    } else {
-      lastBackupTime = time.Now()
-      incrementalBackupCount++
-      if incrementalBackupCount > clickhousebackup.MaxIncrementalBackupCount {
-        incrementalBackupCount = 0
-        isIncremental = false
-      } else {
-        isIncremental = true
-      }
-    }
-  }
+		logger.Info("backup requested")
+		err = executeBackup(taskContext, backuper, isIncremental, logger)
+		if err != nil {
+			logger.Error("cannot backup", zap.Error(err))
+		} else {
+			lastBackupTime = time.Now()
+			incrementalBackupCount++
+			if incrementalBackupCount > clickhousebackup.MaxIncrementalBackupCount {
+				incrementalBackupCount = 0
+				isIncremental = false
+			} else {
+				isIncremental = true
+			}
+		}
+	}
 
-  return nil
+	return nil
 }
 
 func executeBackup(taskContext context.Context, backuper *backup.Backuper, isIncremental bool, logger *zap.Logger) error {
-  backupName := backup.NewBackupName()
-  logger = logger.With(zap.String("backup", backupName), zap.Bool("isIncremental", isIncremental))
+	backupName := backup.NewBackupName()
+	logger = logger.With(zap.String("backup", backupName), zap.Bool("isIncremental", isIncremental))
 
-  diffFromRemote := ""
-  if isIncremental {
-    remoteBackups, err := backuper.GetRemoteBackups(taskContext, true)
-    if err != nil {
-      logger.Error("cannot get remote backup list", zap.Error(err))
-    } else if len(remoteBackups) > 0 {
-      diffFromRemote = remoteBackups[len(remoteBackups)-1].BackupName
-    }
-  }
+	diffFromRemote := ""
+	if isIncremental {
+		remoteBackups, err := backuper.GetRemoteBackups(taskContext, true)
+		if err != nil {
+			logger.Error("cannot get remote backup list", zap.Error(err))
+		} else if len(remoteBackups) > 0 {
+			diffFromRemote = remoteBackups[len(remoteBackups)-1].BackupName
+		}
+	}
 
-  err := backuper.CreateBackup(backupName, "", nil, false, false, false, "unknown", status.NotFromAPI)
-  if err != nil {
-    return errors.WithStack(err)
-  }
+	err := backuper.CreateBackup(backupName, "", nil, false, false, false, false, "unknown", status.NotFromAPI)
+	if err != nil {
+		return errors.WithStack(err)
+	}
 
-  if taskContext.Err() != nil {
-    return nil
-  }
+	if taskContext.Err() != nil {
+		return nil
+	}
 
-  logger.Info("upload", zap.String("diffFromRemote", diffFromRemote))
-  err = backuper.Upload(backupName, "", diffFromRemote, "", nil, false, false, status.NotFromAPI)
-  if err != nil {
-    return err
-  }
+	logger.Info("upload", zap.String("diffFromRemote", diffFromRemote))
+	err = backuper.Upload(backupName, "", diffFromRemote, "", nil, false, false, status.NotFromAPI)
+	if err != nil {
+		return err
+	}
 
-  if taskContext.Err() != nil {
-    return nil
-  }
+	if taskContext.Err() != nil {
+		return nil
+	}
 
-  logger.Info("uploaded")
-  return nil
+	logger.Info("uploaded")
+	return nil
 }
--- pkg/clickhouse-backup/clickhouse-backup.go
+++ pkg/clickhouse-backup/clickhouse-backup.go
@@ -1,47 +1,47 @@
 package clickhouse_backup
 
 import (
-  "bytes"
-  "github.com/AlexAkulov/clickhouse-backup/pkg/backup"
-  "github.com/AlexAkulov/clickhouse-backup/pkg/config"
-  "github.com/JetBrains/ij-perf-report-aggregator/pkg/util"
-  "github.com/valyala/fastjson"
-  "log"
-  "os"
-  "os/exec"
+	"bytes"
+	"github.com/Altinity/clickhouse-backup/pkg/backup"
+	"github.com/Altinity/clickhouse-backup/pkg/config"
+	"github.com/JetBrains/ij-perf-report-aggregator/pkg/util"
+	"github.com/valyala/fastjson"
+	"log"
+	"os"
+	"os/exec"
 )
 
 // example: if data collected each 3 hours, will be 8 backup per day, so, upload full backup at least once a day
 const MaxIncrementalBackupCount = 4
 
 func CreateBackuper() *backup.Backuper {
-  backupConfig := config.DefaultConfig()
-  backupConfig.General.RemoteStorage = "s3"
-  backupConfig.General.BackupsToKeepRemote = MaxIncrementalBackupCount * 32
-  backupConfig.ClickHouse.Host = "127.0.0.1"
-  backupConfig.S3.AccessKey = util.GetEnvOrFileOrPanic("S3_ACCESS_KEY", "/etc/s3/accessKey")
-  backupConfig.S3.SecretKey = util.GetEnvOrFileOrPanic("S3_SECRET_KEY", "/etc/s3/secretKey")
-  backupConfig.S3.Bucket = util.GetEnvOrFileOrPanic("S3_BUCKET", "/etc/s3/bucket")
-  backupConfig.S3.Region = "eu-west-1"
-  backupConfig.S3.AllowMultipartDownload = true
-  backuper := backup.NewBackuper(backupConfig)
-  return backuper
+	backupConfig := config.DefaultConfig()
+	backupConfig.General.RemoteStorage = "s3"
+	backupConfig.General.BackupsToKeepRemote = MaxIncrementalBackupCount * 32
+	backupConfig.ClickHouse.Host = "127.0.0.1"
+	backupConfig.S3.AccessKey = util.GetEnvOrFileOrPanic("S3_ACCESS_KEY", "/etc/s3/accessKey")
+	backupConfig.S3.SecretKey = util.GetEnvOrFileOrPanic("S3_SECRET_KEY", "/etc/s3/secretKey")
+	backupConfig.S3.Bucket = util.GetEnvOrFileOrPanic("S3_BUCKET", "/etc/s3/bucket")
+	backupConfig.S3.Region = "eu-west-1"
+	backupConfig.S3.AllowMultipartDownload = true
+	backuper := backup.NewBackuper(backupConfig)
+	return backuper
 }
 
 func SetS3EnvForLocalRun() {
-  cmd := exec.Command("doppler", "secrets", "download", "--project", "s3", "--config", "prd", "--no-file")
-  stdout, err := cmd.Output()
-  if err != nil {
-    log.Println("failed to use doppler to retrieve credentials", err)
-  } else {
-    excludePrefix := []byte("DOPPLER_")
-    fastjson.MustParseBytes(stdout).GetObject().Visit(func(key []byte, v *fastjson.Value) {
-      if !bytes.HasPrefix(key, excludePrefix) {
-        err = os.Setenv(string(key), string(v.GetStringBytes()))
-        if err != nil {
-          log.Println("cannot set env", err)
-        }
-      }
-    })
-  }
+	cmd := exec.Command("doppler", "secrets", "download", "--project", "s3", "--config", "prd", "--no-file")
+	stdout, err := cmd.Output()
+	if err != nil {
+		log.Println("failed to use doppler to retrieve credentials", err)
+	} else {
+		excludePrefix := []byte("DOPPLER_")
+		fastjson.MustParseBytes(stdout).GetObject().Visit(func(key []byte, v *fastjson.Value) {
+			if !bytes.HasPrefix(key, excludePrefix) {
+				err = os.Setenv(string(key), string(v.GetStringBytes()))
+				if err != nil {
+					log.Println("cannot set env", err)
+				}
+			}
+		})
+	}
 }

42480917bc8dda9687ad19bdc84e2774afdd8c52
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-06-23 13:12:30 +0200

    Fix progress over time page

--- dashboard/new-dashboard/src/components/startup/IntelliJProgressOverTime.vue
+++ dashboard/new-dashboard/src/components/startup/IntelliJProgressOverTime.vue
@@ -14,7 +14,24 @@
   </StartupPage>
 </template>
 <script setup lang="ts">
+import { provide } from "vue"
+import { useRouter } from "vue-router"
+import { AggregationOperatorConfigurator } from "../../configurators/AggregationOperatorConfigurator"
+import { aggregationOperatorConfiguratorKey } from "../../shared/injectionKeys"
 import AggregationOperatorSelect from "../charts/AggregationOperatorSelect.vue"
 import BarChartCard from "../charts/BarChartCard.vue"
+import { PersistentStateManager } from "../common/PersistentStateManager"
 import StartupPage from "./StartupPage.vue"
+
+const persistentStateManager = new PersistentStateManager(
+  "ij-progress-over-time",
+  {
+    product: "IU",
+    project: "simple for IJ",
+    machine: "macMini M1, 16GB",
+    branch: "master",
+  },
+  useRouter()
+)
+provide(aggregationOperatorConfiguratorKey, new AggregationOperatorConfigurator(persistentStateManager))
 </script>

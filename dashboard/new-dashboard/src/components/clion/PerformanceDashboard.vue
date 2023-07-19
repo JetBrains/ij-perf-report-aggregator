0cddb23aa5e8f212f628a9e9158fd2ead2a92919
Alexander Karaev <Alexander.Karaev@jetbrains.com>
2023-07-19 16:34:21 +0300

    Update CLion dashboard

--- dashboard/new-dashboard/src/components/clion/PerformanceDashboard.vue
+++ dashboard/new-dashboard/src/components/clion/PerformanceDashboard.vue
@@ -10,57 +10,111 @@
       <div class="flex-1 min-w-0">
         <AggregationChart
           :configurators="averagesConfigurators"
-          :aggregated-measure="'processingSpeed#C++'"
-          :title="'Indexing C++ (kB/s)'"
-          :chart-color="'#219653'"
-          :value-unit="'counter'"
-        />
-      </div>
-      <div class="flex-1 min-w-0">
-        <AggregationChart
-          :configurators="averagesConfigurators"
-          :aggregated-measure="'processingSpeed#C++ Header'"
-          :title="'Indexing C++ Header (kB/s)'"
-          :chart-color="'#219653'"
-          :value-unit="'counter'"
-        />
-      </div>
-      <div class="flex-1 min-w-0">
-        <AggregationChart
-          :configurators="averagesConfigurators"
-          :aggregated-measure="'completion\_%'"
+          :aggregated-project="'clion/%'"
+          :aggregated-measure="'processingSpeed#%'"
           :is-like="true"
-          :title="'Completion'"
+          :title="'[CLion] Indexing speed (kB/s)'"
+          :chart-color="'#219653'"
+          :value-unit="'counter'"
+        />
+      </div>
+      <div class="flex-1 min-w-0">
+        <AggregationChart
+          :configurators="averagesConfigurators"
+          :aggregated-project="'clion/%hot%'"
+          :aggregated-measure="'completion#mean\_value'"
+          :is-like="true"
+          :title="'[CLion] Completion'"
+        />
+      </div>
+      <div class="flex-1 min-w-0">
+        <AggregationChart
+          :configurators="averagesConfigurators"
+          :aggregated-project="'clion/%'"
+          :aggregated-measure="'%#average_awt_delay'"
+          :is-like="true"
+          :title="'[CLion] UI responsiveness during typing'"
+          :chart-color="'#F2994A'"
+        />
+      </div>
+    </section>
+    <section class="flex gap-6">
+      <div class="flex-1 min-w-0">
+        <AggregationChart
+          :configurators="averagesConfigurators"
+          :aggregated-project="'radler/%'"
+          :aggregated-measure="'processingSpeed#%'"
+          :is-like="true"
+          :title="'[Radler] Indexing speed (kB/s)'"
+          :chart-color="'#219653'"
+          :value-unit="'counter'"
+        />
+      </div>
+      <div class="flex-1 min-w-0">
+        <AggregationChart
+          :configurators="averagesConfigurators"
+          :aggregated-project="'radler/%hot%'"
+          :aggregated-measure="'completion#mean\_value'"
+          :is-like="true"
+          :title="'[Radler] Completion'"
+        />
+      </div>
+      <div class="flex-1 min-w-0">
+        <AggregationChart
+          :configurators="averagesConfigurators"
+          :aggregated-project="'radler/%'"
+          :aggregated-measure="'%#average_awt_delay'"
+          :is-like="true"
+          :title="'[Radler] UI responsiveness during typing'"
+          :chart-color="'#F2994A'"
+        />
+      </div>
+    </section>
+    <section class="flex gap-x-6">
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Indexing (curl)"
+          :measure="['indexingTimeWithoutPauses']"
+          :projects="['clion/curl/indexing', 'radler/curl/indexing']"
+        />
+      </div>
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Scanning (curl)"
+          :measure="['scanningTimeWithoutPauses']"
+          :projects="['clion/curl/indexing', 'radler/curl/indexing']"
         />
       </div>
     </section>
     <section>
       <GroupProjectsChart
-        label="Indexing"
-        :measure="['indexing', 'indexingTimeWithoutPauses']"
-        :projects="['curl/indexing']"
+        label="Global Inspections (fmtlib)"
+        measure="globalInspections"
+        :projects="['clion/fmtlib/inspection', 'radler/fmtlib/inspection']"
       />
     </section>
-    <section>
-      <GroupProjectsChart
-        label="Number Of Indexed Files"
-        measure="numberOfIndexedFiles"
-        :projects="['curl/indexing']"
-      />
-    </section>
-    <section>
-      <GroupProjectsChart
-        label="Scanning"
-        :measure="['scanning', 'scanningTimeWithoutPauses']"
-        :projects="['curl/indexing']"
-      />
-    </section>
-    <section>
-      <GroupProjectsChart
-        label="Completion"
-        measure="completion"
-        :projects="['fmtlib/completion']"
-      />
+    <section class="flex gap-x-6">
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Completion, mean value (std::string, hot)"
+          measure="completion#mean_value"
+          :projects="['clion/fmtlib/completion/std.string (hot)', 'radler/fmtlib/completion/std.string (hot)']"
+        />
+      </div>
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Completion, mean value (std::shared_ptr<T>, hot)"
+          measure="completion#mean_value"
+          :projects="['clion/fmtlib/completion/std.shared_ptr (dep) (hot)', 'radler/fmtlib/completion/std.shared_ptr (dep) (hot)']"
+        />
+      </div>
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Completion, mean value (fmt::join<It>, hot)"
+          measure="completion#mean_value"
+          :projects="['clion/fmtlib/completion/fmt.join_view (dep) (hot)', 'radler/fmtlib/completion/fmt.join_view (dep) (hot)']"
+        />
+      </div>
     </section>
   </DashboardPage>
 </template>

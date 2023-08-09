7d15421bea8416a12eeb3a70d91c6e125453b6c5
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2023-08-09 19:47:04 +0200

    Refactoring: Implement data-driven generation of Kotlin dev dashboard charts
    - By extracting the group charts parameters into data structures, we can avoid a lot of duplicate elements in templates.
    - This will also make it easier to keep configurations in sync, for example the projects between the highlighting and local inspections dashboard (since they share the same projects), and the projects between all Kotlin dev performance dashboards and the K1 vs. K2 comparison dashboard.

---
+++ dashboard/new-dashboard/src/components/kotlin/dev/DashboardGroupCharts.vue
@@ -0,0 +1,33 @@
+<template>
+  <section
+    v-for="definition in definitions"
+    :key="definition.label"
+    class="flex gap-x-6"
+  >
+    <div class="flex-1 min-w-0">
+      <GroupProjectsChart
+        :label="`${definition.label} K1`"
+        :measure="definition.measure"
+        :projects="definition.projects.map((s) => `${s}_k1`)"
+      />
+    </div>
+    <div class="flex-1 min-w-0">
+      <GroupProjectsChart
+        :label="`${definition.label} K2`"
+        :measure="definition.measure"
+        :projects="definition.projects.map((s) => `${s}_k2`)"
+      />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import GroupProjectsChart from "../../charts/GroupProjectsChart.vue"
+import { ProjectsChartDefinition } from "./projects"
+
+interface Props {
+  definitions: ProjectsChartDefinition[]
+}
+
+defineProps<Props>()
+</script>
--- dashboard/new-dashboard/src/components/kotlin/dev/FindUsagesDashboard.vue
+++ dashboard/new-dashboard/src/components/kotlin/dev/FindUsagesDashboard.vue
@@ -27,133 +27,14 @@
         />
       </div>
     </section>
-    <section class="flex gap-x-6">
-      <div class="flex-1 min-w-0">
-        <GroupProjectsChart
-          label="Intellij findUsages mean value with Library cache K1"
-          measure="findUsages#mean_value"
-          :projects="[
-            'intellij_commit/findUsages/loadModuleEntity_with_library_cache_k1',
-            'intellij_commit/findUsages/setUp_with_library_cache_k1',
-            'intellij_commit/findUsages/SolutionModel_with_library_cache_k1',
-            'intellij_commit/findUsages/SqlBlock_with_library_cache_k1',
-            'intellij_commit/findUsages/UIAutomationInteractionModel_with_library_cache_k1',
-          ]"
-        />
-      </div>
-      <div class="flex-1 min-w-0">
-        <GroupProjectsChart
-          label="Intellij findUsages mean value with Library cache K2"
-          measure="findUsages#mean_value"
-          :projects="[
-            'intellij_commit/findUsages/loadModuleEntity_with_library_cache_k2',
-            'intellij_commit/findUsages/setUp_with_library_cache_k2',
-            'intellij_commit/findUsages/SolutionModel_with_library_cache_k2',
-            'intellij_commit/findUsages/SqlBlock_with_library_cache_k2',
-            'intellij_commit/findUsages/UIAutomationInteractionModel_with_library_cache_k2',
-          ]"
-        />
-      </div>
-    </section>
 
-    <section class="flex gap-x-6">
-      <div class="flex-1 min-w-0">
-        <GroupProjectsChart
-          label="'Kotlin lang' findUsages mean value with Library cache K1"
-          measure="findUsages#mean_value"
-          :projects="[
-            'kotlin_lang/findUsages/CommonParser_with_library_cache_k1',
-            'kotlin_lang/findUsages/DefaultArgumentStubGenerator_with_library_cache_k1',
-            'kotlin_lang/findUsages/FirErrors_with_library_cache_k1',
-            'kotlin_lang/findUsages/Flag_with_library_cache_k1',
-            'kotlin_lang/findUsages/ReferenceSymbolTable_with_library_cache_k1',
-          ]"
-        />
-      </div>
-      <div class="flex-1 min-w-0">
-        <GroupProjectsChart
-          label="'Kotlin lang' findUsages mean value with Library cache K2"
-          measure="findUsages#mean_value"
-          :projects="[
-            'kotlin_lang/findUsages/CommonParser_with_library_cache_k2',
-            'kotlin_lang/findUsages/DefaultArgumentStubGenerator_with_library_cache_k2',
-            'kotlin_lang/findUsages/FirErrors_with_library_cache_k2',
-            'kotlin_lang/findUsages/Flag_with_library_cache_k2',
-            'kotlin_lang/findUsages/ReferenceSymbolTable_with_library_cache_k2',
-          ]"
-        />
-      </div>
-    </section>
-    <section class="flex gap-x-6">
-      <div class="flex-1 min-w-0">
-        <GroupProjectsChart
-          label="'TBE' findUsages mean value with Library cache K1"
-          measure="findUsages#mean_value"
-          :projects="[
-            'toolbox_enterprise/findUsages/ErrorReport_with_library_cache_k1',
-            'toolbox_enterprise/findUsages/genUuid_with_library_cache_k1',
-            'toolbox_enterprise/findUsages/getAll_with_library_cache_k1',
-            'toolbox_enterprise/findUsages/getTempDirectory_with_library_cache_k1',
-            'toolbox_enterprise/findUsages/PrincipalContext_with_library_cache_k1',
-            'toolbox_enterprise/findUsages/RequestMapping_with_library_cache_k1',
-            'toolbox_enterprise/findUsages/RestController_with_library_cache_k1',
-            'toolbox_enterprise/findUsages/ROLE_ADMIN_with_library_cache_k1',
-          ]"
-        />
-      </div>
-      <div class="flex-1 min-w-0">
-        <GroupProjectsChart
-          label="'TBE' findUsages mean value with Library cache K2"
-          measure="findUsages#mean_value"
-          :projects="[
-            'toolbox_enterprise/findUsages/ErrorReport_with_library_cache_k2',
-            'toolbox_enterprise/findUsages/genUuid_with_library_cache_k2',
-            'toolbox_enterprise/findUsages/getAll_with_library_cache_k2',
-            'toolbox_enterprise/findUsages/getTempDirectory_with_library_cache_k2',
-            'toolbox_enterprise/findUsages/PrincipalContext_with_library_cache_k2',
-            'toolbox_enterprise/findUsages/RequestMapping_with_library_cache_k2',
-            'toolbox_enterprise/findUsages/RestController_with_library_cache_k2',
-            'toolbox_enterprise/findUsages/ROLE_ADMIN_with_library_cache_k2',
-          ]"
-        />
-      </div>
-    </section>
-    <section class="flex gap-x-6">
-      <div class="flex-1 min-w-0">
-        <GroupProjectsChart
-          label="'Android canary leak' findUsages mean value with Library cache K1"
-          measure="findUsages#mean_value"
-          :projects="[
-            'leak-canary-android/findUsages/BOOLEAN_with_library_cache_k1',
-            'leak-canary-android/findUsages/HeapGraph_with_library_cache_k1',
-            'leak-canary-android/findUsages/HeapObject_with_library_cache_k1',
-            'leak-canary-android/findUsages/HprofRecordTag_with_library_cache_k1',
-            'leak-canary-android/findUsages/INT_with_library_cache_k1',
-            'leak-canary-android/findUsages/PrimitiveType_with_library_cache_k1',
-          ]"
-        />
-      </div>
-      <div class="flex-1 min-w-0">
-        <GroupProjectsChart
-          label="'Android canary leak' findUsages mean value with Library cache K2"
-          measure="findUsages#mean_value"
-          :projects="[
-            'leak-canary-android/findUsages/BOOLEAN_with_library_cache_k2',
-            'leak-canary-android/findUsages/HeapGraph_with_library_cache_k2',
-            'leak-canary-android/findUsages/HeapObject_with_library_cache_k2',
-            'leak-canary-android/findUsages/HprofRecordTag_with_library_cache_k2',
-            'leak-canary-android/findUsages/INT_with_library_cache_k2',
-            'leak-canary-android/findUsages/PrimitiveType_with_library_cache_k2',
-          ]"
-        />
-      </div>
-    </section>
+    <DashboardGroupCharts :definitions="findUsagesCharts" />
   </DashboardPage>
-  >
 </template>
 
 <script setup lang="ts">
 import AggregationChart from "../../charts/AggregationChart.vue"
-import GroupProjectsChart from "../../charts/GroupProjectsChart.vue"
 import DashboardPage from "../../common/DashboardPage.vue"
+import DashboardGroupCharts from "./DashboardGroupCharts.vue"
+import { findUsagesCharts } from "./projects"
 </script>

d1e49c174e971fc0b1edea15380a8d41c653d099
Anna Koehler <anna.marchenko@jetbrains.com>
2023-08-25 13:44:08 +0200

    Fix Add a dashboard for startup metrics from FUS

---
+++ dashboard/new-dashboard/src/components/intelliJ/fus/FUSStartupDashboard.vue
@@ -0,0 +1,37 @@
+<template>
+  <DashboardPage
+    db-name="perfint"
+    table="idea"
+    persistent-id="fus_startup_dashboard"
+    initial-machine="Linux Munich i7-3770, 32 Gb"
+    :charts="charts"
+  >
+    <section>
+      <div>
+        <GroupProjectsChart
+          v-for="chart in charts"
+          :key="chart.definition.label"
+          :label="chart.definition.label"
+          :measure="chart.definition.measure"
+          :projects="chart.projects"
+        />
+      </div>
+    </section>
+  </DashboardPage>
+</template>
+
+<script setup lang="ts">
+import { ChartDefinition, combineCharts } from "../../charts/DashboardCharts"
+import GroupProjectsChart from "../../charts/GroupProjectsChart.vue"
+import DashboardPage from "../../common/DashboardPage.vue"
+
+const chartsDeclaration: ChartDefinition[] = [
+  {
+    labels: ["Code analysis execution time", "Startup total duration"],
+    measures: ["fus_code_analysis_execution_time", "fus_startup_totalDuration"],
+    projects: ["intellij-on-intellij_1/measureStartup"],
+  },
+]
+
+const charts = combineCharts(chartsDeclaration)
+</script>

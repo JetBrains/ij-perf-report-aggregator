28931dc6fb2dd8c24d2193317cf0bef078f5bf74
Anna Koehler <anna.marchenko@jetbrains.com>
2023-08-27 12:54:24 +0200

    Add indexing metrics from FUS to dashboard

--- dashboard/new-dashboard/src/components/intelliJ/fus/FUSDashboard.vue
+++ dashboard/new-dashboard/src/components/intelliJ/fus/FUSDashboard.vue
@@ -27,6 +27,11 @@ import DashboardPage from "../../common/DashboardPage.vue"
 
 const chartsDeclaration: ChartDefinition[] = [
   {
+    labels: ["Indexing time", "Scanning time"],
+    measures: ["fus_dumb_indexing_time", "fus_scanning_time"],
+    projects: ["intellij_sources/indexing"],
+  },
+  {
     labels: ["Completion time to show 90p", "Completion duration 90p"],
     measures: ["fus_time_to_show_90p", "fus_completion_duration_90p"],
     projects: ["grails/completion/groovy_file", "grails/completion/java_file", "keycloak_release_20/completion/QuarkusRuntimePomXml"],

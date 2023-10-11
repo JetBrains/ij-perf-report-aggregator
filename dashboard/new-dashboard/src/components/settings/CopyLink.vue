478dcd785cac8134de9c64d0d03718d383b60c74
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-10-11 18:58:58 +0200

    Fix eslint error

--- dashboard/new-dashboard/src/components/settings/CopyLink.vue
+++ dashboard/new-dashboard/src/components/settings/CopyLink.vue
@@ -10,20 +10,7 @@
     >
       <VTooltip theme="info">
         <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"
-            />
-          </svg>
+          <CopyIcon />
         </span>
         <template #popper><span class="text-sm">Copy link to dashboard with current date timeline.</span></template>
       </VTooltip>
@@ -32,21 +19,17 @@
   <div
     v-show="isToastVisible"
     id="toast-default"
-    class="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+    class="flex items-center p-2 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
     role="alert"
   >
-    <div class="ml-3 text-sm font-normal">Copied</div>
-    <button
-      type="button"
-      class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-      data-dismiss-target="#toast-default"
-    ></button>
+    <div class="text-sm font-normal">Copied</div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue"
 import { parseDuration, TimeRangeConfigurator } from "../../configurators/TimeRangeConfigurator"
+import CopyIcon from "../common/CopyIcon.vue"
 const props = defineProps<{
   timerangeConfigurator: TimeRangeConfigurator
 }>()

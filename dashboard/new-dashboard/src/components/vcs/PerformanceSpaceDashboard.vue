d01a1921a9657b7a5f3206a964ef187ead741319
Alexey Kryuchkov <alexey.kryuchkov@jetbrains.com>
2023-10-17 11:24:32 +0400

    Refresh, building graph, log reload metrics for vcs tests

--- dashboard/new-dashboard/src/components/vcs/PerformanceSpaceDashboard.vue
+++ dashboard/new-dashboard/src/components/vcs/PerformanceSpaceDashboard.vue
@@ -25,28 +25,51 @@ import DashboardPage from "../common/DashboardPage.vue"
 
 const spaceSpecific = "space/gitLogIndexing"
 const spaceSpecificSql = "space/gitLogIndexing-sql"
+const spaceCommit = "space/git-commit"
 
 const chartsDeclaration: ChartDefinition[] = [
+  //Indexing
   {
     labels: ["Indexing"],
     measures: [["vcs-log-indexing"]],
-    projects: [spaceSpecific, spaceSpecificSql],
+    projects: [spaceSpecific, spaceSpecificSql]
   },
   {
     labels: ["Number of collected commits"],
     measures: [["vcs-log-indexing#numberOfCommits"]],
-    projects: [spaceSpecific, spaceSpecificSql],
+    projects: [spaceSpecific, spaceSpecificSql]
+  },
+  {
+    labels: ["Real number of commits through git rev-list --count --all"],
+    measures: [["realNumberOfCommits"]],
+    projects: [spaceSpecific, spaceSpecificSql]
   },
   {
     labels: ["LoadingDetails - the time spent reading  batch of commits from git  (git log command)"],
     measures: [["LoadingDetails"]],
-    projects: [spaceSpecific, spaceSpecificSql],
+    projects: [spaceSpecific, spaceSpecificSql]
   },
+  //Commit
   {
     labels: ["Commit FUS duration"],
     measures: [["git-commit#fusCommitDuration"]],
-    projects: ["space/git-commit"],
+    projects: [spaceCommit]
   },
+  {
+    labels: ["refresh - time spent on reading latest commits from git and adding them to existing"],
+    measures: [["refresh"]],
+    projects: [spaceCommit]
+  },
+  {
+    labels: ["building graph - time spent on building commit graph(PermanentGraph)"],
+    measures: [["building graph"]],
+    projects: [spaceCommit]
+  },
+  {
+    labels: ["full log reload - time spent on building commit graph(PermanentGraph) + reading whole repository from git"],
+    measures: [["full log reload"]],
+    projects: [spaceCommit]
+  }
 ]
 
 const charts = combineCharts(chartsDeclaration)

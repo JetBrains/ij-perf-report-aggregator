94465da05d37af561b99c85adb3c42fb3d18a919
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2023-11-08 12:36:29 +0400

    [RubyMine] Extracted indexing dashboard

---
+++ dashboard/new-dashboard/src/components/rubymine/IndexingDashboard.vue
@@ -0,0 +1,57 @@
+<template>
+  <DashboardPage
+    v-slot="{ averagesConfigurators }"
+    db-name="perfint"
+    table="ruby"
+    persistent-id="rubymine_indexing_dashboard"
+    initial-machine="Linux EC2 C6id.8xlarge (32 vCPU Xeon, 64 GB)"
+  >
+    <section class="flex gap-6">
+      <div class="flex-1 min-w-0">
+        <AggregationChart
+          :configurators="averagesConfigurators"
+          :aggregated-measure="'processingSpeed#Ruby'"
+          :title="'Indexing Ruby (kB/s)'"
+          :chart-color="'#219653'"
+          :value-unit="'counter'"
+        />
+      </div>
+      <div class="flex-1 min-w-0">
+        <AggregationChart
+          :configurators="averagesConfigurators"
+          :aggregated-measure="'processingSpeed#JavaScript'"
+          :title="'Indexing JavaScript (kB/s)'"
+          :chart-color="'#219653'"
+          :value-unit="'counter'"
+        />
+      </div>
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Indexing"
+        :measure="['indexing', 'indexingTimeWithoutPauses']"
+        :projects="['diaspora-project-test/indexing', 'gem-rbs-collection-indexing-test/indexing', 'gitlab-project-test/indexing', 'redmine-project-test/indexing']"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Number Of Indexed Files"
+        measure="numberOfIndexedFiles"
+        :projects="['diaspora-project-test/indexing', 'gem-rbs-collection-indexing-test/indexing', 'gitlab-project-test/indexing', 'redmine-project-test/indexing']"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Scanning"
+        :measure="['scanning', 'scanningTimeWithoutPauses']"
+        :projects="['diaspora-project-test/indexing', 'gem-rbs-collection-indexing-test/indexing', 'gitlab-project-test/indexing', 'redmine-project-test/indexing']"
+      />
+    </section>
+  </DashboardPage>
+</template>
+
+<script setup lang="ts">
+import AggregationChart from "../charts/AggregationChart.vue"
+import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
+import DashboardPage from "../common/DashboardPage.vue"
+</script>
--- dashboard/new-dashboard/src/components/rubymine/PerformanceDashboard.vue
+++ dashboard/new-dashboard/src/components/rubymine/PerformanceDashboard.vue
@@ -4,28 +4,8 @@
     db-name="perfint"
     table="ruby"
     persistent-id="rubymine_dashboard"
-    initial-machine="Linux Munich i7-3770, 32 Gb"
+    initial-machine="Linux EC2 C6id.8xlarge (32 vCPU Xeon, 64 GB)"
   >
-    <section class="flex gap-6">
-      <div class="flex-1 min-w-0">
-        <AggregationChart
-          :configurators="averagesConfigurators"
-          :aggregated-measure="'processingSpeed#Ruby'"
-          :title="'Indexing Ruby (kB/s)'"
-          :chart-color="'#219653'"
-          :value-unit="'counter'"
-        />
-      </div>
-      <div class="flex-1 min-w-0">
-        <AggregationChart
-          :configurators="averagesConfigurators"
-          :aggregated-measure="'processingSpeed#JavaScript'"
-          :title="'Indexing JavaScript (kB/s)'"
-          :chart-color="'#219653'"
-          :value-unit="'counter'"
-        />
-      </div>
-    </section>
     <section class="flex gap-6">
       <div class="flex-1 min-w-0">
         <AggregationChart
@@ -43,27 +23,6 @@
           :chart-color="'#F2994A'"
         />
       </div>
-    </section>
-    <section>
-      <GroupProjectsChart
-        label="Indexing"
-        :measure="['indexing', 'indexingTimeWithoutPauses']"
-        :projects="['diaspora-project-test/indexing', 'gem-rbs-collection-indexing-test/indexing', 'gitlab-project-test/indexing', 'redmine-project-test/indexing']"
-      />
-    </section>
-    <section>
-      <GroupProjectsChart
-        label="Number Of Indexed Files"
-        measure="numberOfIndexedFiles"
-        :projects="['diaspora-project-test/indexing', 'gem-rbs-collection-indexing-test/indexing', 'gitlab-project-test/indexing', 'redmine-project-test/indexing']"
-      />
-    </section>
-    <section>
-      <GroupProjectsChart
-        label="Scanning"
-        :measure="['scanning', 'scanningTimeWithoutPauses']"
-        :projects="['diaspora-project-test/indexing', 'gem-rbs-collection-indexing-test/indexing', 'gitlab-project-test/indexing', 'redmine-project-test/indexing']"
-      />
     </section>
     <section>
       <GroupProjectsChart

ca0fc5d38f3e8ff6467184e3d7485e6d5eb1270c
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2023-11-08 16:47:43 +0400

    [RubyMine] Removed aggregated speed charts
    They are not bringing useful information, we have a processing time now and may watch it for signals of degradations

--- dashboard/new-dashboard/src/components/rubymine/IndexingDashboard.vue
+++ dashboard/new-dashboard/src/components/rubymine/IndexingDashboard.vue
@@ -1,31 +1,10 @@
 <template>
   <DashboardPage
-    v-slot="{ averagesConfigurators }"
     db-name="perfint"
     table="ruby"
     persistent-id="rubymine_indexing_dashboard"
     initial-machine="Linux EC2 C6id.8xlarge (32 vCPU Xeon, 64 GB)"
   >
-    <section class="flex gap-6">
-      <div class="flex-1 min-w-0">
-        <AggregationChart
-          :configurators="averagesConfigurators"
-          :aggregated-measure="'processingSpeed#Ruby'"
-          :title="'Indexing Ruby (kB/s)'"
-          :chart-color="'#219653'"
-          :value-unit="'counter'"
-        />
-      </div>
-      <div class="flex-1 min-w-0">
-        <AggregationChart
-          :configurators="averagesConfigurators"
-          :aggregated-measure="'processingSpeed#JavaScript'"
-          :title="'Indexing JavaScript (kB/s)'"
-          :chart-color="'#219653'"
-          :value-unit="'counter'"
-        />
-      </div>
-    </section>
     <section>
       <GroupProjectsChart
         label="Indexing Time"
@@ -94,7 +73,6 @@
 </template>
 
 <script setup lang="ts">
-import AggregationChart from "../charts/AggregationChart.vue"
 import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
 import DashboardPage from "../common/DashboardPage.vue"
 </script>

78448f187eaaf0f9322c386390806cea921a9cd9
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2023-11-14 16:21:54 +0400

    Clean up lexing/parsing metrics labels

--- dashboard/new-dashboard/src/shared/metricsMapping.ts
+++ dashboard/new-dashboard/src/shared/metricsMapping.ts
@@ -1,5 +1,11 @@
 export const METRICS_MAPPING: Record<string, string> = {
   "processingTime#": "",
+  "lexingSpeed#": "",
+  "parsingSpeed#": "",
+  "lexingTime#": "",
+  "parsingTime#": "",
+  "lexingSize#": "",
+  "parsingSize#": "",
   indexingTimeWithoutPauses: "indexing",
   scanningTimeWithoutPauses: "scanning",
 }

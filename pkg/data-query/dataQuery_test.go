a7c5c54d08a8c40d13e0ea12b0cc736c425cf935
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-11-16 18:00:47 +0100

    Add a new test that models the real request

--- pkg/data-query/dataQuery_test.go
+++ pkg/data-query/dataQuery_test.go
@@ -30,6 +30,25 @@ func TestAdvancedFilter(t *testing.T) {
   assert.Equal("select from test where (generated_time > subtractMonths(now(), 1)) order by generated_time", sql)
 }
 
+func TestQueryWithManyFields(t *testing.T) {
+  queries, err := readQuery([]byte(`
+{"fields":[{"n":"t","sql":"toUnixTimestamp(generated_time)*1000"},{"n":"measures","subName":"value"},{"n":"measures","subName":"name"},{"n":"measures","subName":"type"},"machine","tc_build_id","project","tc_installer_build_id","build_c1","build_c2","build_c3","branch"],"filters":[{"f":"project","v":"intellij_sources/vfsRefresh/with-1-thread(s)"},{"f":"branch","v":"akoehler/vfs-degradation-before-vk-ffd635a390d3%","o":"like"},{"f":"machine","v":"intellij-linux-performance-aws-%","o":"like"},{"f":"generated_time","q":">subtractMonths(now(),3)"},{"f":"triggeredBy","v":""},{"f":"build_c3","v":0,"o":"="},{"f":"measures.name","v":"vfs_initial_refresh"}],"order":"t"}
+`))
+  if err != nil {
+    t.Error(err)
+  }
+
+  //noinspection GoImportUsedAsName
+  assert := assert.New(t)
+  assert.NotEmpty(queries)
+
+  sql, _, err := buildSql(queries[0], "test")
+  if err != nil {
+    t.Error(err)
+  }
+  assert.Equal("select toUnixTimestamp(generated_time)*1000 as `t`, measures.value, measures.name, measures.type, machine, tc_build_id, project, tc_installer_build_id, build_c1, build_c2, build_c3, branch from test array join measures where (project = 'intellij_sources/vfsRefresh/with-1-thread(s)') and (branch like 'akoehler/vfs-degradation-before-vk-ffd635a390d3%') and (machine like 'intellij-linux-performance-aws-%') and (generated_time >subtractMonths(now(),3)) and (triggeredBy = '') and (build_c3=0) and (measures.name = 'vfs_initial_refresh') order by t", sql)
+}
+
 func TestOrInFilterQuery(t *testing.T) {
   queries, err := readQuery([]byte(`
 {

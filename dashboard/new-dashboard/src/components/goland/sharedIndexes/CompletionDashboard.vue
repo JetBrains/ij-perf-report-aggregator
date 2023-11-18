90a01e6e5f91e2189da38cc102a7a8688daf12cb
Lev Zagnetin <lev.zagnetin@jetbrains.com>
2023-11-18 02:09:03 +0200

    Try to generate charts from project names

--- dashboard/new-dashboard/src/components/goland/sharedIndexes/CompletionDashboard.vue
+++ dashboard/new-dashboard/src/components/goland/sharedIndexes/CompletionDashboard.vue
@@ -4,27 +4,18 @@
     table="goland"
     persistent-id="go_completion_dashboard"
     initial-machine="Linux EC2 C6id.8xlarge (32 vCPU Xeon, 64 GB)"
+    :project="projects"
   >
     <section>
-      <GroupProjectsChart
-        label="Completion (Empty Project)"
-        measure="completion"
-        :projects="['go-empty-project-bundled-sharedIndexes', 'go-empty-project-with-generated-sharedIndexes', 'go-empty-project-without-sharedIndexes']"
-      />
-    </section>
-    <section>
-      <GroupProjectsChart
-        label="Completion (Terraform)"
-        measure="completion"
-        :projects="['go-terraform-without-sharedIndexes', 'go-terraform-bundled-sharedIndexes', 'go-terraform-with-generated-sharedIndexes']"
-      />
-    </section>
-    <section>
-      <GroupProjectsChart
-        label="Completion (Kratos)"
-        measure="completion"
-        :projects="['go-kratos-without-sharedIndexes', 'go-kratos-bundled-sharedIndexes', 'go-kratos-with-generated-sharedIndexes']"
-      />
+      <div>
+        <GroupProjectsChart
+          v-for="project in projects"
+          :key="'Completion (' + project.pretty_name + ')'"
+          :label="'Completion (' + project.pretty_name + ')'"
+          measure="completion"
+          :projects="['go-'+ project.real_name +'-bundled-sharedIndexes', 'go-'+ project.real_name +'-with-generated-sharedIndexes', 'go-'+ project.real_name +'-without-sharedIndexes']"
+        />
+      </div>
     </section>
   </DashboardPage>
 </template>
@@ -32,4 +23,23 @@
 <script setup lang="ts">
 import GroupProjectsChart from "../../charts/GroupProjectsChart.vue"
 import DashboardPage from "../../common/DashboardPage.vue"
+interface project {
+  pretty_name: String
+  real_name: String
+}
+const projects: project[] = [
+  {
+    pretty_name: "Empty Project",
+    real_name: "empty-project"
+  },
+  {
+    pretty_name: "Terraform",
+    real_name: "terraform"
+  },
+  {
+    pretty_name: "Kratos",
+    real_name: "kratos"
+  }
+]
+
 </script>

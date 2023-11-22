bae1a37272fa9e2395622145d995189842c54a7d
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2023-11-22 13:57:30 +0100

    K1VsK2ComparisonDashboard: Add a non-dev version of the dashboard based on the `perfint` database
    - We want to compare the performance of the K1 vs. K2 plugins in the `master` branch based on data from performance test run on installers, which is only available in the `perfint` database.

---
+++ dashboard/new-dashboard/src/components/kotlin/dev/K1VsK2ComparisonDevDashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <K1VsK2ComparisonDashboardBase
+    db-name="perfintDev"
+    persistent-id="kotlinDev_k1VsK2Comparison"
+    initial-branch="kt-master"
+  />
+</template>
+
+<script setup lang="ts">
+import K1VsK2ComparisonDashboardBase from "../components/K1VsK2ComparisonDashboardBase.vue"
+</script>

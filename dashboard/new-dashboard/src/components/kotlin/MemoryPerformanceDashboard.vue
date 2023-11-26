d33016af4501972aafa1ae9a31c9d74046a0ab6d
roman.ivanitskii <roman.ivanitskii@jetbrains.com>
2023-11-26 12:13:37 +0200

    Add several metrics on memory dashboard

--- dashboard/new-dashboard/src/components/kotlin/MemoryPerformanceDashboard.vue
+++ dashboard/new-dashboard/src/components/kotlin/MemoryPerformanceDashboard.vue
@@ -5,25 +5,62 @@
     persistent-id="kotlin_dashboard"
     initial-machine="linux-blade-hetzner"
   >
+    <MeasureSelect
+      title="Metrics"
+      :selected-label="metricsSelectLabelFormat"
+      :configurator="measureConfigurator"
+    ></MeasureSelect>
     <Divider title="Completion" />
-    <MemoryDashboardGroupCharts :definitions="completionCharts" />
+    <MemoryDashboardGroupCharts
+      :metrics="measureConfigurator.selectedSafe"
+      :definitions="completionCharts"
+    />
     <Divider title="Code analysis" />
-    <MemoryDashboardGroupCharts :definitions="codeAnalysisCharts" />
-    <MemoryDashboardGroupCharts :definitions="highlightingCharts" />
+    <MemoryDashboardGroupCharts
+      :metrics="measureConfigurator.selectedSafe"
+      :definitions="codeAnalysisCharts"
+    />
+    <MemoryDashboardGroupCharts
+      :metrics="measureConfigurator.selectedSafe"
+      :definitions="highlightingCharts"
+    />
     <Divider title="Find usages" />
-    <MemoryDashboardGroupCharts :definitions="findUsagesCharts" />
+    <MemoryDashboardGroupCharts
+      :metrics="measureConfigurator.selectedSafe"
+      :definitions="findUsagesCharts"
+    />
     <Divider title="Debugger" />
-    <MemoryDashboardGroupCharts :definitions="evaluateExpressionChars" />
+    <MemoryDashboardGroupCharts
+      :metrics="measureConfigurator.selectedSafe"
+      :definitions="evaluateExpressionChars"
+    />
     <Divider title="Refactoring" />
-    <MemoryDashboardGroupCharts :definitions="refactoringCharts" />
+    <MemoryDashboardGroupCharts
+      :metrics="measureConfigurator.selectedSafe"
+      :definitions="refactoringCharts"
+    />
     <Divider title="Script" />
-    <MemoryDashboardGroupCharts :definitions="scriptCompletionCharts" />
-    <MemoryDashboardGroupCharts :definitions="highlightingScriptCharts" />
-    <MemoryDashboardGroupCharts :definitions="codeAnalysisScriptCharts" />
+    <MemoryDashboardGroupCharts
+      :metrics="measureConfigurator.selectedSafe"
+      :definitions="scriptCompletionCharts"
+    />
+    <MemoryDashboardGroupCharts
+      :metrics="measureConfigurator.selectedSafe"
+      :definitions="highlightingScriptCharts"
+    />
+    <MemoryDashboardGroupCharts
+      :metrics="measureConfigurator.selectedSafe"
+      :definitions="codeAnalysisScriptCharts"
+    />
   </DashboardPage>
 </template>
 
 <script setup lang="ts">
+import { provide } from "vue"
+import { SimpleMeasureConfigurator } from "../../configurators/SimpleMeasureConfigurator"
+import { simpleMeasureConfiguratorKey } from "../../shared/keys"
+import { metricsSelectLabelFormat } from "../../shared/labels"
+import MeasureSelect from "../charts/MeasureSelect.vue"
 import DashboardPage from "../common/DashboardPage.vue"
 import Divider from "../common/Divider.vue"
 import MemoryDashboardGroupCharts from "./MemoryDashboardGroupCharts.vue"
@@ -38,4 +75,8 @@ import {
   highlightingScriptCharts,
   codeAnalysisScriptCharts,
 } from "./projects"
+
+const measureConfigurator = new SimpleMeasureConfigurator("metrics", null)
+measureConfigurator.initData(["freedMemoryByGC", "JVM.diffHeapUsageMb_afterGc"])
+provide(simpleMeasureConfiguratorKey, measureConfigurator)
 </script>

a3a82bf007a86894cab5643627071453f6399383
Alexey <alexey.kryuchkov@jetbrains.com>
2023-12-19 13:21:27 +0400

    Space vcs dashboard moved on the new ui

--- dashboard/new-dashboard/src/components/vcs/PerformanceSpaceDashboard.vue
+++ dashboard/new-dashboard/src/components/vcs/PerformanceSpaceDashboard.vue
@@ -2,75 +2,93 @@
   <DashboardPage
     db-name="perfint"
     table="idea"
-    persistent-id="vcs_space_ultimate_dashboard"
+    persistent-id="vcs_idea_ultimate_dashboard"
     initial-machine="Linux EC2 C6id.8xlarge (32 vCPU Xeon, 64 GB)"
-    :charts="charts"
   >
-    <section>
-      <GroupProjectsChart
-        v-for="chart in charts"
-        :key="chart.definition.label"
-        :label="chart.definition.label"
-        :measure="chart.definition.measure"
-        :projects="chart.projects"
-      />
-    </section>
+    <div>
+      <Chip><a href="#index">Vcs indexing</a></Chip>
+      <Chip><a href="#commit">Commit</a></Chip>
+    </div>
+
+    <Accordion
+      :multiple="true"
+      :active-index="[0, 1]"
+    >
+      <AccordionTab header="Indexing">
+        <a name="index" />
+        <section>
+          <GroupProjectsChart
+            label="Indexing"
+            measure="vcs-log-indexing"
+            :projects="indexingProjects"
+          />
+        </section>
+        <section>
+          <GroupProjectsChart
+            label="Number of collected commits"
+            measure="vcs-log-indexing#numberOfCommits"
+            :projects="indexingProjects"
+          />
+        </section>
+        <section>
+          <GroupProjectsChart
+            label="Real number of collected commits through git rev-list --count --all"
+            measure="realNumberOfCommits"
+            :projects="indexingProjects"
+          />
+        </section>
+        <section>
+          <GroupProjectsChart
+            label="'vcs-log' directory size in gb"
+            measure="vcs-log-directory-size-gb"
+            :projects="indexingProjects"
+          />
+        </section>
+      </AccordionTab>
+
+      <AccordionTab header="Commit">
+        <a name="commit" />
+        <section>
+          <GroupProjectsChart
+            label="Commit FUS duration"
+            measure="git-commit#fusCommitDuration"
+            :projects="commitProjects"
+          />
+        </section>
+        <section>
+          <GroupProjectsChart
+            label="Refreshing VCS Log when repositories change (on commit, rebase, checkout branch, etc.)"
+            measure="vcs-log-refreshing"
+            :projects="commitProjects"
+          />
+        </section>
+        <section>
+          <GroupProjectsChart
+            label="Building a [com.intellij.vcs.log.graph.PermanentGraph] for the list of commits"
+            measure="vcs-log-building-graph"
+            :projects="commitProjects"
+          />
+        </section>
+        <section>
+          <GroupProjectsChart
+            label="Loading full VCS Log (all commits and references)"
+            measure="vcs-log-loading-full-log"
+            :projects="commitProjects"
+          />
+        </section>
+      </AccordionTab>
+    </Accordion>
   </DashboardPage>
 </template>
 
 <script setup lang="ts">
-import { ChartDefinition, combineCharts } from "../charts/DashboardCharts"
+import Accordion from "primevue/accordion"
+import Chip from "primevue/chip"
 import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
 import DashboardPage from "../common/DashboardPage.vue"
 
-const spaceSpecific = "space/gitLogIndexing"
-const spaceSpecificSql = "space/gitLogIndexing-sql"
-const spaceCommit = "space/git-commit"
+const spaceSpecificProject = "space/gitLogIndexing"
+const indexingProjects = [spaceSpecificProject, spaceSpecificProject + "-sql"]
 
-const chartsDeclaration: ChartDefinition[] = [
-  //Indexing
-  {
-    labels: ["Indexing"],
-    measures: [["vcs-log-indexing"]],
-    projects: [spaceSpecific, spaceSpecificSql],
-  },
-  {
-    labels: ["Number of collected commits"],
-    measures: [["vcs-log-indexing#numberOfCommits"]],
-    projects: [spaceSpecific, spaceSpecificSql],
-  },
-  {
-    labels: ["Real number of commits through git rev-list --count --all"],
-    measures: [["realNumberOfCommits"]],
-    projects: [spaceSpecific, spaceSpecificSql],
-  },
-  {
-    labels: ["'vcs-log' directory size in gb"],
-    measures: [["vcs-log-directory-size-gb"]],
-    projects: [spaceSpecific, spaceSpecificSql],
-  },
-  //Commit
-  {
-    labels: ["Commit FUS duration"],
-    measures: [["git-commit#fusCommitDuration"]],
-    projects: [spaceCommit],
-  },
-  {
-    labels: ["Refreshing VCS Log when repositories change (on commit, rebase, checkout branch, etc.)"],
-    measures: [["vcs-log-refreshing"]],
-    projects: [spaceCommit],
-  },
-  {
-    labels: ["Building a [com.intellij.vcs.log.graph.PermanentGraph] for the list of commits"],
-    measures: [["vcs-log-building-graph"]],
-    projects: [spaceCommit],
-  },
-  {
-    labels: ["Loading full VCS Log (all commits and references)"],
-    measures: [["vcs-log-loading-full-log"]],
-    projects: [spaceCommit],
-  },
-]
-
-const charts = combineCharts(chartsDeclaration)
+const commitProjects = ["space/git-commit"]
 </script>

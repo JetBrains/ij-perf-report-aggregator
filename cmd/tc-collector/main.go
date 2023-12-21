4db5a38ba271113d19fd0dd134fe5e3c0e6a7d94
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-12-21 16:46:00 +0100

    Fix JBR configurations

--- cmd/tc-collector/main.go
+++ cmd/tc-collector/main.go
@@ -99,8 +99,8 @@ func configureCollectFromTeamCity() error {
         }
       }
     case chunk.Database == "jbr":
-      jbrTypes := []string{"macOS12aarch64Metal", "macOS12aarch64OGL", "macOS12x64Metal", "macOS12x64OGL", "macOS13aarch64Metal", "macOS13aarch64OGL", "macOS13x64Metal",
-        "macOS13x64OGL", "Ubuntu2004x64", "Ubuntu2004x64OGL", "Ubuntu2204x64", "Ubuntu2204x64OGL", "Windows10x64", "Windows11x64"}
+      jbrTypes := []string{"MacOS12aarch64Metal", "MacOS12aarch64OGL", "MacOS12x86_64Metal", "MacOS12x86_64OGL", "MacOS13aarch64Metal", "MacOS13aarch64OGL", "MacOS13x86_64Metal",
+        "MacOS13x86_64OGL", "Ubuntu2004x86_64", "Ubuntu2004x86_64OGL", "Ubuntu2204x86_64", "Ubuntu2204x86_64OGL", "Windows10x86_64", "Windows11x86_64"}
       for _, configuration := range chunk.Configurations {
         for _, jbrType := range jbrTypes {
           buildConfigurationIds = append(buildConfigurationIds, configuration+"_"+jbrType)

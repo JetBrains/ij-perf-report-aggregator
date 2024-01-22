d56f989de67cacf180fb2afeabf82797893d04e1
Anna Koehler <anna.marchenko@jetbrains.com>
2024-01-22 13:55:54 +0100

    Refactor dashboards for SE, add perf tests for JBR 17 to the dashboard with Custom JBR

--- dashboard/new-dashboard/src/components/intelliJ/PerformanceSEDashboard.vue
+++ dashboard/new-dashboard/src/components/intelliJ/PerformanceSEDashboard.vue
@@ -25,66 +25,28 @@ import DashboardPage from "../common/DashboardPage.vue"
 
 const chartsDeclaration: ChartDefinition[] = [
   {
-    labels: ["Search Everywhere (insert whole word)", "SE Dialog Shown (insert whole word)", "SE Items Loaded (insert whole word)"],
-    measures: ["searchEverywhere", "searchEverywhere_dialog_shown", "searchEverywhere_items_loaded"],
-    projects: [
-      "community/go-to-action/SharedIndex",
-      "community/go-to-class/EditorImpl",
-      "community/go-to-class/SharedIndex",
-      "community/go-to-file/EditorImpl",
-      "community/go-to-file/SharedIndex",
-      "community/go-to-action/SharedIndex/insertingTheWholeWord",
-      "community/go-to-class/EditorImpl/insertingTheWholeWord",
-      "community/go-to-class/SharedIndex/insertingTheWholeWord",
-      "community/go-to-file/EditorImpl/insertingTheWholeWord",
-      "community/go-to-file/SharedIndex/insertingTheWholeWord",
-    ],
+    labels: ["Search Everywhere Class (slow typing)", "SE Dialog Shown Class (slow typing)"],
+    measures: ["searchEverywhere", "searchEverywhere_dialog_shown"],
+    projects: ["community/go-to-class/Kotlin/typingLetterByLetter", "community/go-to-class/Editor/typingLetterByLetter", "community/go-to-class/Runtime/typingLetterByLetter"],
   },
   {
-    labels: ["Search Everywhere Action (slow typing)", "SE Dialog Shown Action (slow typing)", "SE Items Loaded Action (slow typing)"],
-    measures: ["searchEverywhere", "searchEverywhere_dialog_shown", "searchEverywhere_items_loaded"],
-    projects: [
-      "community/go-to-action/Runtime",
-      "community/go-to-action/Runtime/typingLetterByLetter",
-      "community/go-to-action/Editor/typingLetterByLetter",
-      "community/go-to-action/Kotlin/typingLetterByLetter",
-      "community/go-to-action/Runtime/typingLetterByLetter",
-    ],
+    labels: ["Search Everywhere File (slow typing)", "SE Dialog Shown File (slow typing)"],
+    measures: ["searchEverywhere", "searchEverywhere_dialog_shown"],
+    projects: ["community/go-to-file/Editor/typingLetterByLetter", "community/go-to-file/Kotlin/typingLetterByLetter", "community/go-to-file/Runtime/typingLetterByLetter"],
   },
   {
-    labels: ["Search Everywhere Class (slow typing)", "SE Dialog Shown Class (slow typing)", "SE Items Loaded Class (slow typing)"],
-    measures: ["searchEverywhere", "searchEverywhere_dialog_shown", "searchEverywhere_items_loaded"],
-    projects: [
-      "community/go-to-class/Kotlin",
-      "community/go-to-class/Kotlin/typingLetterByLetter",
-      "community/go-to-class/Editor/typingLetterByLetter",
-      "community/go-to-class/Runtime/typingLetterByLetter",
-    ],
-  },
-  {
-    labels: ["Search Everywhere File (slow typing)", "SE Dialog Shown File (slow typing)", "SE Items Loaded File (slow typing)"],
-    measures: ["searchEverywhere", "searchEverywhere_dialog_shown", "searchEverywhere_items_loaded"],
-    projects: [
-      "community/go-to-file/properties",
-      "community/go-to-file/properties/typingLetterByLetter",
-      "community/go-to-file/Editor/typingLetterByLetter",
-      "community/go-to-file/Kotlin/typingLetterByLetter",
-      "community/go-to-file/Runtime/typingLetterByLetter",
-    ],
-  },
-  {
-    labels: ["Search Everywhere All (slow typing)", "SE Dialog Shown All (slow typing)", "SE Items Loaded All (slow typing)"],
-    measures: ["searchEverywhere", "searchEverywhere_dialog_shown", "searchEverywhere_items_loaded"],
+    labels: ["Search Everywhere All (slow typing)", "SE Dialog Shown All (slow typing)"],
+    measures: ["searchEverywhere", "searchEverywhere_dialog_shown"],
     projects: ["community/go-to-all/Editor/typingLetterByLetter", "community/go-to-all/Kotlin/typingLetterByLetter", "community/go-to-all/Runtime/typingLetterByLetter"],
   },
   {
-    labels: ["Search Everywhere Symbol (slow typing)", "SE Dialog Shown Symbol (slow typing)", "SE Items Loaded Symbol (slow typing)"],
-    measures: ["searchEverywhere", "searchEverywhere_dialog_shown", "searchEverywhere_items_loaded"],
+    labels: ["Search Everywhere Symbol (slow typing)", "SE Dialog Shown Symbol (slow typing)"],
+    measures: ["searchEverywhere", "searchEverywhere_dialog_shown"],
     projects: ["community/go-to-symbol/Editor/typingLetterByLetter", "community/go-to-symbol/Kotlin/typingLetterByLetter", "community/go-to-symbol/Runtime/typingLetterByLetter"],
   },
   {
-    labels: ["Search Everywhere Text (slow typing)", "SE Dialog Shown Text (slow typing)", "SE Items Loaded Text (slow typing)"],
-    measures: ["searchEverywhere", "searchEverywhere_dialog_shown", "searchEverywhere_items_loaded"],
+    labels: ["Search Everywhere Text (slow typing)", "SE Dialog Shown Text (slow typing)"],
+    measures: ["searchEverywhere", "searchEverywhere_dialog_shown"],
     projects: ["community/go-to-text/Editor/typingLetterByLetter", "community/go-to-text/Kotlin/typingLetterByLetter", "community/go-to-text/Runtime/typingLetterByLetter"],
   },
 ]

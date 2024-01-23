f746e189ba953647d85677a8411713caa7bb1d83
Vladislav Sholokhov <vladislav.sholokhov@jetbrains.com>
2024-01-23 11:31:45 +0200

    [webstorm] add webstorm next dashboard

---
+++ dashboard/new-dashboard/src/components/webstorm/PerformanceDashboardNEXT.vue
@@ -0,0 +1,213 @@
+<template>
+  <DashboardPage
+    db-name="perfint"
+    table="webstorm"
+    persistent-id="webstorm_dashboard_next"
+    initial-machine="linux-blade-hetzner"
+  >
+    <section>
+      <GroupProjectsChart
+        label="Code Vision (JSReferencesCodeVisionProvider)"
+        measure="JSReferencesCodeVisionProvider"
+        :projects="[
+          'aws_cdk/localInspection/logging',
+          'aws_cdk/localInspection/loggingNEXT',
+          'WEB_5976/localInspection/react_mui',
+          'WEB_5976/localInspection/react_muiNEXT',
+          'toh-pt6/localInspection/hero.service.ts',
+          'toh-pt6/localInspection/hero.service.tsNEXT',
+          'vue3-admin-vite/localInspection/index.vue',
+          'vue3-admin-vite/localInspection/index.vueNEXT',
+          'eslint-plugin-jest/localInspection/misc.ts',
+          'eslint-plugin-jest/localInspection/misc.tsNEXT',
+        ]"
+      />
+    </section>
+
+    <Divider title="TypeScript" />
+
+    <section class="flex gap-x-6 flex-col md:flex-row">
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Completion"
+          measure="completion"
+          :projects="['eslint-plugin-jest/completion/types', 'eslint-plugin-jest/completion/typesNEXT', 'novu/completion/everything', 'novu/completion/everythingNEXT']"
+        />
+      </div>
+
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Local inspections"
+          measure="localInspections"
+          :projects="[
+            'aws_cdk/localInspection/logging',
+            'aws_cdk/localInspection/loggingNEXT',
+            'eslint-plugin-jest/localInspection/misc.ts',
+            'eslint-plugin-jest/localInspection/misc.tsNEXT',
+            'novu/localInspection/init.ts',
+            'novu/localInspection/init.tsNEXT',
+          ]"
+        />
+      </div>
+
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Typing"
+          measure="typing"
+          :projects="['eslint-plugin-jest/typing', 'eslint-plugin-jest/typingNEXT', 'novu/typing']"
+        />
+      </div>
+    </section>
+
+    <Divider title="JavaScript" />
+
+    <section class="flex gap-x-6 flex-col md:flex-row">
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Completion"
+          measure="completion"
+          :projects="['axios/completion/functions', 'axios/completion/functionsNEXT']"
+        />
+      </div>
+
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Local inspections"
+          measure="localInspections"
+          :projects="['axios/localInspection/utils.js', 'axios/localInspection/utils.jsNEXT']"
+        />
+      </div>
+
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Typing"
+          measure="typing"
+          :projects="['axios/typing', 'axios/typingNEXT']"
+        />
+      </div>
+    </section>
+
+    <Divider title="Angular" />
+
+    <section class="flex gap-x-6 flex-col md:flex-row">
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Completion"
+          measure="completion"
+          :projects="['toh-pt6/completion/attribute', 'toh-pt6/completion/attributeNEXT', 'toh-pt6/completion/component', 'toh-pt6/completion/componentNEXT']"
+        />
+      </div>
+
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Local inspections"
+          measure="localInspections"
+          :projects="[
+            'toh-pt6/localInspection/hero.service.ts',
+            'toh-pt6/localInspection/hero.service.tsNEXT',
+            'toh-pt6/localInspection/heroes.component.html',
+            'toh-pt6/localInspection/heroes.component.htmlNEXT',
+          ]"
+        />
+      </div>
+
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Typing"
+          measure="typing"
+          :projects="['toh-pt6/typing', 'toh-pt6/typingNEXT']"
+        />
+      </div>
+    </section>
+
+    <Divider title="React" />
+
+    <section class="flex gap-x-6 flex-col md:flex-row">
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Completion"
+          measure="completion"
+          :projects="[
+            'react-todo-js/completion/attribute',
+            'react-todo-js/completion/attributeNEXT',
+            'react-todo-js/completion/component',
+            'react-todo-js/completion/componentNEXT',
+            'vkui/completion/component',
+            'vkui/completion/componentNEXT',
+            'ring-ui/completion/component',
+            'ring-ui/completion/componentNEXT',
+          ]"
+        />
+      </div>
+
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Local inspections"
+          measure="localInspections"
+          :projects="[
+            'react-todo-js/localInspection/App.js',
+            'react-todo-js/localInspection/App.jsNEXT',
+            'WEB_5976/localInspection/react_mui',
+            'WEB_5976/localInspection/react_muiNEXT',
+          ]"
+        />
+      </div>
+
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Typing"
+          measure="typing"
+          :projects="['react-todo-js/typing', 'react-todo-js/typingNEXT']"
+        />
+      </div>
+    </section>
+
+    <Divider title="Vue" />
+
+    <section class="flex gap-x-6 flex-col md:flex-row">
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Completion"
+          measure="completion"
+          :projects="[
+            'vue-template/completion/attribute',
+            'vue-template/completion/attributeNEXT',
+            'vue-template/completion/component',
+            'vue-template/completion/componentNEXT',
+            'vue3-admin-vite/completion/component',
+            'vue3-admin-vite/completion/componentNEXT',
+            'vue3-admin-vite/completion/attribute',
+            'vue3-admin-vite/completion/attributeNEXT',
+          ]"
+        />
+      </div>
+
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Local inspections"
+          measure="localInspections"
+          :projects="[
+            'vue-template/localInspection/HelloWorld.vue',
+            'vue-template/localInspection/HelloWorld.vueNEXT',
+            'vue3-admin-vite/localInspection/index.vue',
+            'vue3-admin-vite/localInspection/index.vueNEXT',
+          ]"
+        />
+      </div>
+
+      <div class="flex-1 min-w-0">
+        <GroupProjectsChart
+          label="Typing"
+          measure="typing"
+          :projects="['vue-template/typing', 'vue-template/typingNEXT']"
+        />
+      </div>
+    </section>
+  </DashboardPage>
+</template>
+
+<script setup lang="ts">
+import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
+import DashboardPage from "../common/DashboardPage.vue"
+import Divider from "../common/Divider.vue"
+</script>

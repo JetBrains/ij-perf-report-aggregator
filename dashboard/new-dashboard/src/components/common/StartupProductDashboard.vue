778514e04b79d03d09943f33ad5326187d9e85eb
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-02-19 18:41:22 +0100

    Hide additional metrics on product startup

--- dashboard/new-dashboard/src/components/common/StartupProductDashboard.vue
+++ dashboard/new-dashboard/src/components/common/StartupProductDashboard.vue
@@ -33,72 +33,76 @@
             :configurators="configurators"
           />
         </section>
+        <Accordion :lazy="true">
+          <AccordionTab header="Additional metrics">
+            <Divider label="Bootstrap" />
+            <section class="grid grid-cols-2 gap-x-6">
+              <LineChart
+                :measures="['appInit_d', 'app initialization.end']"
+                title="App Initialization"
+                :configurators="configurators"
+                tooltip-trigger="axis"
+              />
+              <LineChart
+                :measures="['bootstrap_d']"
+                title="Bootstrap"
+                :configurators="configurators"
+                :with-measure-name="true"
+              />
+            </section>
 
-        <Divider label="Bootstrap" />
-        <section class="grid grid-cols-2 gap-x-6">
-          <LineChart
-            :measures="['appInit_d', 'app initialization.end']"
-            title="App Initialization"
-            :configurators="configurators"
-            tooltip-trigger="axis"
-          />
-          <LineChart
-            :measures="['bootstrap_d']"
-            title="Bootstrap"
-            :configurators="configurators"
-            :with-measure-name="true"
-          />
-        </section>
+            <section class="grid grid-cols-2 gap-x-6">
+              <LineChart
+                :measures="['classLoadingPreparedCount', 'classLoadingLoadedCount']"
+                title="Class Loading (Count)"
+                :configurators="configurators"
+                tooltip-trigger="axis"
+              />
+              <LineChart
+                :configurators="configurators"
+                :measures="['editorRestoring']"
+                title="Editor restoring"
+                :with-measure-name="true"
+              />
+            </section>
 
-        <section class="grid grid-cols-2 gap-x-6">
-          <LineChart
-            :measures="['classLoadingPreparedCount', 'classLoadingLoadedCount']"
-            title="Class Loading (Count)"
-            :configurators="configurators"
-            tooltip-trigger="axis"
-          />
-          <LineChart
-            :configurators="configurators"
-            :measures="['editorRestoring']"
-            title="Editor restoring"
-            :with-measure-name="true"
-          />
-        </section>
-
-        <span v-if="highlightingPasses">
-          <Divider label="Highlighting Passes" />
-          <span v-if="showAllPasses">
+            <span v-if="highlightingPasses">
+              <Divider label="Highlighting Passes" />
+              <span v-if="showAllPasses">
+                <LineChart
+                  title="Highlighting Passes"
+                  :measures="highlightingPasses"
+                  :configurators="configurators"
+                />
+              </span>
+              <LineChart
+                title="Code Analysis"
+                :measures="['metrics.codeAnalysisDaemon/fusExecutionTime', 'metrics.runDaemon/executionTime']"
+                :configurators="configurators"
+                tooltip-trigger="axis"
+              />
+            </span>
+            <slot :configurators="configurators"></slot>
+            <Divider label="Notifications" />
             <LineChart
-              title="Highlighting Passes"
-              :measures="highlightingPasses"
+              title="Notifications"
+              :measures="['metrics.notifications/number']"
+              :skip-zero-values="false"
               :configurators="configurators"
+              :with-measure-name="true"
             />
-          </span>
-          <LineChart
-            title="Code Analysis"
-            :measures="['metrics.codeAnalysisDaemon/fusExecutionTime', 'metrics.runDaemon/executionTime']"
-            :configurators="configurators"
-            tooltip-trigger="axis"
-          />
-        </span>
-        <slot :configurators="configurators"></slot>
-        <Divider label="Notifications" />
-        <LineChart
-          title="Notifications"
-          :measures="['metrics.notifications/number']"
-          :skip-zero-values="false"
-          :configurators="configurators"
-          :with-measure-name="true"
-        />
 
-        <Divider label="Exit" />
-        <LineChart
-          title="Exit Metrics"
-          :measures="['metrics.exitMetrics/application.exit', 'metrics.exitMetrics/saveSettingsOnExit', 'metrics.exitMetrics/disposeProjects']"
-          :configurators="configurators"
-          tooltip-trigger="axis"
-        />
+            <Divider label="Exit" />
+            <LineChart
+              title="Exit Metrics"
+              :measures="['metrics.exitMetrics/application.exit', 'metrics.exitMetrics/saveSettingsOnExit', 'metrics.exitMetrics/disposeProjects']"
+              :configurators="configurators"
+              tooltip-trigger="axis"
+            />
+          </AccordionTab>
+        </Accordion>
       </div>
+
       <InfoSidebar />
     </main>
   </div>

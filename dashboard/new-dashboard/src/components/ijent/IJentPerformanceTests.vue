29e531c6606367f9ba24b49f189520cdf6d0ae65
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2024-02-22 17:47:54 +0200

    [ijent] AT-753 Dashboard for benchmarks and perf tests of IJent

---
+++ dashboard/new-dashboard/src/components/ijent/IJentPerformanceTests.vue
@@ -0,0 +1,70 @@
+<template>
+  <DashboardPage
+    db-name="perfint"
+    table="idea"
+    persistent-id="ijent_performance_dashboard"
+    initial-machine="linux-blade-hetzner"
+    :charts="charts"
+  >
+    <template #configurator>
+      <MeasureSelect
+        :configurator="testConfigurator"
+        title="Test"
+      >
+        <template #icon>
+          <ChartBarIcon class="w-4 h-4 text-gray-500" />
+        </template>
+      </MeasureSelect>
+    </template>
+    <section>
+      <GroupProjectsChart
+        v-for="chart in charts"
+        :key="chart.definition.label"
+        :label="chart.definition.label"
+        :measure="chart.definition.measure"
+        :projects="chart.projects"
+      />
+    </section>
+  </DashboardPage>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue"
+import { SimpleMeasureConfigurator } from "../../configurators/SimpleMeasureConfigurator"
+import { ChartDefinition, combineCharts } from "../charts/DashboardCharts"
+import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
+import MeasureSelect from "../charts/MeasureSelect.vue"
+import DashboardPage from "../common/DashboardPage.vue"
+
+const metricsDeclaration = [
+  "AWTEventQueue.dispatchTimeTotal",
+  "gcPause",
+  "gcPauseCount",
+  "fullGCPause",
+  "freedMemoryByGC",
+  "totalHeapUsedMax",
+
+  "JVM.GC.collectionTimesMs",
+  "JVM.GC.collections",
+  "JVM.maxHeapMegabytes",
+  "JVM.maxThreadCount",
+  "JVM.totalCpuTimeMs",
+]
+
+// TODO: add future projects here
+const projects = [""]
+
+const testConfigurator = new SimpleMeasureConfigurator("project", null)
+testConfigurator.initData(projects)
+
+const charts = computed(() => {
+  const chartsDeclaration: ChartDefinition[] = metricsDeclaration.map((metric) => {
+    return {
+      labels: [metric],
+      measures: [metric],
+      projects: testConfigurator.selected.value ?? [],
+    }
+  })
+  return combineCharts(chartsDeclaration)
+})
+</script>

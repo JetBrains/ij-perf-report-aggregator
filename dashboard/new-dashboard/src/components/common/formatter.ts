95621f64580ba8fb0a235437a1492a7cf91671c6
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-02-26 16:43:56 +0100

    Don't round values less 1 ms to 0 or 1.

--- dashboard/new-dashboard/src/components/common/formatter.ts
+++ dashboard/new-dashboard/src/components/common/formatter.ts
@@ -35,6 +35,10 @@ export const durationAxisPointerFormatter = (valueInMs: number, type: string = "
   if (type === "counter" || type == "c") {
     return valueInMs.toLocaleString()
   }
+  //humanizer doesn't handle values less than 1ms properly and just round them to either 0 or 1ms
+  if (valueInMs < 1) {
+    return valueInMs.toFixed(2) + "ms"
+  }
   const humanizer = humanizeDuration.humanizer(durationFormatOptions)
   return humanizer(valueInMs)
 }

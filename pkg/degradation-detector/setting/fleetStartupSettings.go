3ea4bad4ee2a883dbe8f9af550d399454bb4e59e
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-03-25 11:46:28 +0100

    Add a new channel for Fleet degradations

--- pkg/degradation-detector/setting/fleetStartupSettings.go
+++ pkg/degradation-detector/setting/fleetStartupSettings.go
@@ -6,28 +6,30 @@ import (
 
 func GenerateFleetStartupSettings() []detector.FleetStartupSettings {
   settings := make([]detector.FleetStartupSettings, 0, 100)
+  machines := []string{"intellij-linux-hw-munit-%", "intellij-windows-hw-munit-%", "intellij-macos-hw-munit-%"}
   mainSettings := detector.FleetStartupSettings{
-    Branch:  "master",
-    Machine: "intellij-linux-hw-munit-%",
+    Branch: "master",
   }
   slackSettings := detector.SlackSettings{
-    Channel:     "fleet-performance-hackathon-feb24",
+    Channel:     "fleet-performance-tests-notifications",
     ProductLink: "fleet",
   }
   metrics := []string{"editor appeared", "time to edit", "terminal ready", "file tree rendered", "highlighting done", "window appeared"}
 
-  for _, metric := range metrics {
-    settings = append(settings, detector.FleetStartupSettings{
-      Branch:  mainSettings.Branch,
-      Machine: mainSettings.Machine,
-      Metric:  metric + ".end",
-      AnalysisSettings: detector.AnalysisSettings{
-        MinimumSegmentLength:      7,
-        MedianDifferenceThreshold: 5,
-        EffectSizeThreshold:       0.5,
-      },
-      SlackSettings: slackSettings,
-    })
+  for _, machine := range machines {
+    for _, metric := range metrics {
+      settings = append(settings, detector.FleetStartupSettings{
+        Branch:  mainSettings.Branch,
+        Machine: machine,
+        Metric:  metric + ".end",
+        AnalysisSettings: detector.AnalysisSettings{
+          MinimumSegmentLength:      7,
+          MedianDifferenceThreshold: 5,
+          EffectSizeThreshold:       0.5,
+        },
+        SlackSettings: slackSettings,
+      })
+    }
   }
   return settings
 }

a0554d3e620cf3ee83f0aadea5c2477c25678c86
vlad20012 <beskvlad@gmail.com>
2024-04-08 21:30:52 +0200

    [Rust] Update Rust dashboard

--- dashboard/new-dashboard/src/components/rust/PerformanceDashboardRustRover.vue
+++ dashboard/new-dashboard/src/components/rust/PerformanceDashboardRustRover.vue
@@ -1,43 +1,40 @@
 <template>
   <DashboardPage
-    v-slot="{ averagesConfigurators }"
     db-name="perfint"
     table="rust"
     persistent-id="rust_plugin_dashboard"
     initial-machine="Linux EC2 C6id.8xlarge (32 vCPU Xeon, 64 GB)"
+    release-configurator="EAP / Release"
   >
-    <section class="flex gap-6">
-      <div class="w-1/2">
-        <AggregationChart
-          :configurators="averagesConfigurators"
-          :aggregated-measure="'processingSpeedAvg#Rust'"
-          :title="'Indexing Rust (kB/s)'"
-          :chart-color="'#219653'"
-          :value-unit="'counter'"
-        />
-      </div>
-    </section>
-    <section class="flex gap-x-6">
-      <div class="flex-1 min-w-0">
-        <GroupProjectsChart
-          label="Indexing"
-          :measure="['indexingTimeWithoutPauses']"
-          :projects="['indexing/rustling', 'indexing/yew']"
-        />
-      </div>
-      <div class="flex-1 min-w-0">
-        <GroupProjectsChart
-          label="Scanning"
-          :measure="['scanningTimeWithoutPauses']"
-          :projects="['indexing/rustling', 'indexing/yew']"
-        />
-      </div>
-    </section>
     <section>
       <GroupProjectsChart
-        label="Global Inspection execution time"
+        label="Local Inspections (on file open, metric 'firstCodeAnalysis')"
+        measure="firstCodeAnalysis"
+        :projects="rustLocalInspectionCases"
+      />
+    </section>
+
+    <section>
+      <GroupProjectsChart
+        label="Local Inspections (on typing top-level, metric 'typingCodeAnalyzing#mean_value')"
+        measure="typingCodeAnalyzing#mean_value"
+        :projects="rustLocalInspectionCases.map((testCase) => `${testCase}-top-level-typing`)"
+      />
+    </section>
+
+    <section>
+      <GroupProjectsChart
+        label="Local Inspections (on typing stmt in function, metric 'typingCodeAnalyzing#mean_value')"
+        measure="typingCodeAnalyzing#mean_value"
+        :projects="rustLocalInspectionCases"
+      />
+    </section>
+
+    <section>
+      <GroupProjectsChart
+        label="Global Inspection execution time (metric 'globalInspections')"
         measure="globalInspections"
-        :projects="globalInspectionProjects.map((project) => `global-inspection/${project}-inspection`)"
+        :projects="rustGlobalInspectionProjects.map((project) => `global-inspection/${project}-inspection`)"
       />
     </section>
 
@@ -55,324 +52,11 @@
         :projects="['find-usages/yew', 'find-usages/wasm']"
       />
     </section>
-
-    <section>
-      <GroupProjectsChart
-        label="Local Inspections (on file open, metric 'firstCodeAnalysis')"
-        measure="firstCodeAnalysis"
-        :projects="localInspectionCases"
-      />
-    </section>
-
-    <section>
-      <GroupProjectsChart
-        label="Local Inspections (on typing top-level, metric 'typingCodeAnalyzing#mean_value')"
-        measure="typingCodeAnalyzing#mean_value"
-        :projects="localInspectionCases.map((testCase) => `${testCase}-top-level-typing`)"
-      />
-    </section>
-
-    <section>
-      <GroupProjectsChart
-        label="Local Inspections (on typing stmt, metric 'typingCodeAnalyzing#mean_value')"
-        measure="typingCodeAnalyzing#mean_value"
-        :projects="localInspectionCases"
-      />
-    </section>
-    <section>
-      <GroupProjectsChart
-        label="Cargo Sync (metric 'cargo_sync_execution_time')"
-        measure="cargo_sync_execution_time"
-        :projects="[
-          'arrow-rs/local-inspection/cast',
-          'arrow-rs/local-inspection/comparison',
-          'arrow-rs/local-inspection/parse',
-          'cargo/local-inspection/config/build_script.rs',
-          'cargo/local-inspection/fingerprint/mod.rs',
-          'cargo/local-inspection/testsuite/build.rs',
-          'cargo/local-inspection/testsuite/build_script.rs',
-          'cargo/local-inspection/testsuite/git.rs',
-          'cargo/local-inspection/testsuite/metadata.rs',
-          'cargo/local-inspection/testsuite/test.rs',
-          'cargo/local-inspection/toml/mod.rs',
-          'clap/local-inspection/derives/subcommand.rs',
-          'clap/local-inspection/parser/parser.rs',
-          'clap/local-inspection/toml/mod.rs',
-          'completion/arrow-rs',
-          'completion/vec',
-          'deno/local-inspection/integration/run_tests.rs',
-          'diesel/local-inspection/connection/mod.rs',
-          'diesel/local-inspection/macros/mod.rs',
-          'diesel/local-inspection/src/sql_function.rs',
-          'diesel/local-inspection/src/table.rs',
-          'diesel/local-inspection/type_impls/tuples.rs',
-          'find-usages/wasm',
-          'find-usages/yew',
-          'global-inspection/arrowRs-inspection',
-          'global-inspection/arti-inspection',
-          'global-inspection/bat-inspection',
-          'global-inspection/bevy-inspection',
-          'global-inspection/cargo-inspection',
-          'global-inspection/chrono-inspection',
-          'global-inspection/clap-inspection',
-          'global-inspection/deno-inspection',
-          'global-inspection/diem-inspection',
-          'global-inspection/fd-inspection',
-          'global-inspection/fuel-inspection',
-          'global-inspection/hyperfine-inspection',
-          'global-inspection/meiliSearch-inspection',
-          'global-inspection/nalgebra-inspection',
-          'global-inspection/rand-inspection',
-          'global-inspection/ripgrep-inspection',
-          'global-inspection/ruffle-inspection',
-          'global-inspection/solana-inspection',
-          'global-inspection/spotify-inspection',
-          'global-inspection/tokio-inspection',
-          'global-inspection/turbo-inspection',
-          'global-inspection/yew-inspection',
-          'indexing/rustling',
-          'indexing/yew',
-          'mySql/local-inspection/conn/mod.rs',
-          'mySql/local-inspection/io/mod.rs',
-          'mySql/local-inspection/opts/mod.rs',
-          'mySql/local-inspection/pool/mod.rs',
-          'mySql/local-inspection/src/query.rs',
-          'rust-only-inspection/arrowRs-inspection',
-          'rust-only-inspection/arti-inspection',
-          'rust-only-inspection/bat-inspection',
-          'rust-only-inspection/bevy-inspection',
-          'rust-only-inspection/cargo-inspection',
-          'rust-only-inspection/chrono-inspection',
-          'rust-only-inspection/clap-inspection',
-          'rust-only-inspection/deno-inspection',
-          'rust-only-inspection/diem-inspection',
-          'rust-only-inspection/fd-inspection',
-          'rust-only-inspection/fuel-inspection',
-          'rust-only-inspection/hyperfine-inspection',
-          'rust-only-inspection/lemmy-inspection',
-          'rust-only-inspection/meiliSearch-inspection',
-          'rust-only-inspection/nalgebra-inspection',
-          'rust-only-inspection/rand-inspection',
-          'rust-only-inspection/ripgrep-inspection',
-          'rust-only-inspection/ruffle-inspection',
-          'rust-only-inspection/solana-inspection',
-          'rust-only-inspection/spotify-inspection',
-          'rust-only-inspection/tokio-inspection',
-          'rust-only-inspection/turbo-inspection',
-          'rust-only-inspection/veloren-inspection',
-          'rust-only-inspection/yew-inspection',
-          'solana/progress_map.rs',
-        ]"
-      />
-    </section>
-    <section>
-      <GroupProjectsChart
-        label="Rust macro expansion time"
-        measure="rust_macro_expansion_execution_time"
-        :projects="[
-          'arrow-rs/local-inspection/cast',
-          'arrow-rs/local-inspection/comparison',
-          'arrow-rs/local-inspection/parse',
-          'cargo/local-inspection/config/build_script.rs',
-          'cargo/local-inspection/fingerprint/mod.rs',
-          'cargo/local-inspection/testsuite/build.rs',
-          'cargo/local-inspection/testsuite/build_script.rs',
-          'cargo/local-inspection/testsuite/git.rs',
-          'cargo/local-inspection/testsuite/metadata.rs',
-          'cargo/local-inspection/testsuite/test.rs',
-          'cargo/local-inspection/toml/mod.rs',
-          'clap/local-inspection/derives/subcommand.rs',
-          'clap/local-inspection/parser/parser.rs',
-          'clap/local-inspection/toml/mod.rs',
-          'completion/arrow-rs',
-          'completion/vec',
-          'deno/local-inspection/integration/run_tests.rs',
-          'diesel/local-inspection/connection/mod.rs',
-          'diesel/local-inspection/macros/mod.rs',
-          'diesel/local-inspection/src/sql_function.rs',
-          'diesel/local-inspection/src/table.rs',
-          'diesel/local-inspection/type_impls/tuples.rs',
-          'find-usages/wasm',
-          'find-usages/yew',
-          'indexing/rustling',
-          'indexing/yew',
-          'mySql/local-inspection/conn/mod.rs',
-          'mySql/local-inspection/io/mod.rs',
-          'mySql/local-inspection/opts/mod.rs',
-          'mySql/local-inspection/pool/mod.rs',
-          'mySql/local-inspection/src/query.rs',
-          'solana/progress_map.rs',
-        ]"
-      />
-    </section>
-    <section>
-      <GroupProjectsChart
-        label="Rust DefMaps execution time"
-        measure="rust_def_maps_execution_time"
-        :projects="[
-          'arrow-rs/local-inspection/cast',
-          'arrow-rs/local-inspection/comparison',
-          'arrow-rs/local-inspection/parse',
-          'cargo/local-inspection/config/build_script.rs',
-          'cargo/local-inspection/fingerprint/mod.rs',
-          'cargo/local-inspection/testsuite/build.rs',
-          'cargo/local-inspection/testsuite/build_script.rs',
-          'cargo/local-inspection/testsuite/git.rs',
-          'cargo/local-inspection/testsuite/metadata.rs',
-          'cargo/local-inspection/testsuite/test.rs',
-          'cargo/local-inspection/toml/mod.rs',
-          'clap/local-inspection/derives/subcommand.rs',
-          'clap/local-inspection/parser/parser.rs',
-          'clap/local-inspection/toml/mod.rs',
-          'completion/arrow-rs',
-          'completion/vec',
-          'deno/local-inspection/integration/run_tests.rs',
-          'diesel/local-inspection/connection/mod.rs',
-          'diesel/local-inspection/macros/mod.rs',
-          'diesel/local-inspection/src/sql_function.rs',
-          'diesel/local-inspection/src/table.rs',
-          'diesel/local-inspection/type_impls/tuples.rs',
-          'find-usages/wasm',
-          'find-usages/yew',
-          'global-inspection/arrowRs-inspection',
-          'global-inspection/arti-inspection',
-          'global-inspection/bat-inspection',
-          'global-inspection/bevy-inspection',
-          'global-inspection/cargo-inspection',
-          'global-inspection/chrono-inspection',
-          'global-inspection/clap-inspection',
-          'global-inspection/deno-inspection',
-          'global-inspection/diem-inspection',
-          'global-inspection/fd-inspection',
-          'global-inspection/fuel-inspection',
-          'global-inspection/hyperfine-inspection',
-          'global-inspection/meiliSearch-inspection',
-          'global-inspection/nalgebra-inspection',
-          'global-inspection/rand-inspection',
-          'global-inspection/ripgrep-inspection',
-          'global-inspection/ruffle-inspection',
-          'global-inspection/solana-inspection',
-          'global-inspection/spotify-inspection',
-          'global-inspection/tokio-inspection',
-          'global-inspection/turbo-inspection',
-          'global-inspection/yew-inspection',
-          'indexing/rustling',
-          'indexing/yew',
-          'mySql/local-inspection/conn/mod.rs',
-          'mySql/local-inspection/io/mod.rs',
-          'mySql/local-inspection/opts/mod.rs',
-          'mySql/local-inspection/pool/mod.rs',
-          'mySql/local-inspection/src/query.rs',
-          'rust-only-inspection/arrowRs-inspection',
-          'rust-only-inspection/arti-inspection',
-          'rust-only-inspection/bat-inspection',
-          'rust-only-inspection/bevy-inspection',
-          'rust-only-inspection/cargo-inspection',
-          'rust-only-inspection/chrono-inspection',
-          'rust-only-inspection/clap-inspection',
-          'rust-only-inspection/deno-inspection',
-          'rust-only-inspection/diem-inspection',
-          'rust-only-inspection/fd-inspection',
-          'rust-only-inspection/fuel-inspection',
-          'rust-only-inspection/hyperfine-inspection',
-          'rust-only-inspection/lemmy-inspection',
-          'rust-only-inspection/meiliSearch-inspection',
-          'rust-only-inspection/nalgebra-inspection',
-          'rust-only-inspection/rand-inspection',
-          'rust-only-inspection/ripgrep-inspection',
-          'rust-only-inspection/ruffle-inspection',
-          'rust-only-inspection/solana-inspection',
-          'rust-only-inspection/spotify-inspection',
-          'rust-only-inspection/tokio-inspection',
-          'rust-only-inspection/turbo-inspection',
-          'rust-only-inspection/veloren-inspection',
-          'rust-only-inspection/yew-inspection',
-          'solana/progress_map.rs',
-        ]"
-      />
-    </section>
   </DashboardPage>
 </template>
 
 <script setup lang="ts">
-import AggregationChart from "../charts/AggregationChart.vue"
 import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
 import DashboardPage from "../common/DashboardPage.vue"
-
-const globalInspectionProjects = [
-  "arrowRs",
-  "arti",
-  "bat",
-  "bevy",
-  "cargo",
-  "chrono",
-  "clap",
-  "deno",
-  "diem",
-  "fd",
-  "fuel",
-  "hyperfine",
-  "hello-world",
-  "meiliSearch",
-  "nalgebra",
-  "rand",
-  "ripgrep",
-  "ruffle",
-  "solana",
-  "spotify",
-  "tokio",
-  "turbo",
-  "yew",
-]
-
-const localInspectionCases = [
-  "arrow-rs/local-inspection/parse",
-  "arrow-rs/local-inspection/cast",
-  "arrow-rs/local-inspection/comparison",
-  "bat/local-inspection/src/bin/bat/clap_app.rs",
-  "bat/local-inspection/src/diff.rs",
-  "bevy/local-inspection/crates/bevy_pbr/src/light.rs",
-  "bevy/local-inspection/crates/bevy_pbr/src/render/light.rs",
-  "bevy/local-inspection/crates/bevy_render/macros/src/as_bind_group.rs",
-  "fd/local-inspection/src/cli.rs",
-  "fuel/local-inspection/crates/fuel-core/src/executor.rs",
-  "cargo/local-inspection/testsuite/build.rs",
-  "cargo/local-inspection/testsuite/build_script.rs",
-  "cargo/local-inspection/testsuite/metadata.rs",
-  "cargo/local-inspection/testsuite/test.rs",
-  "cargo/local-inspection/testsuite/git.rs",
-  "cargo/local-inspection/config/mod.rs",
-  "cargo/local-inspection/fingerprint/mod.rs",
-  "cargo/local-inspection/toml/mod.rs",
-  "chrono/local-inspection/src/naive/date.rs",
-  "chrono/local-inspection/src/duration.rs",
-  "clap/local-inspection/toml/mod.rs",
-  "clap/local-inspection/derives/subcommand.rs",
-  "clap/local-inspection/parser/parser.rs",
-  "deno/local-inspection/integration/run_tests.rs",
-  "deno/local-inspection/args/flags.rs",
-  "diesel/local-inspection/src/table.rs",
-  "diesel/local-inspection/connection/mod.rs",
-  "diesel/local-inspection/macros/mod.rs",
-  "diesel/local-inspection/type_impls/tuples.rs",
-  "diesel/local-inspection/src/sql_function.rs",
-  "hyperfine/local-inspection/src/command.rs",
-  "hyperfine/local-inspection/src/export/markup.rs",
-  "lemmy/local-inspection/comment_report_view.rs",
-  "nalgebra/local-inspection/src/base/matrix.rs",
-  "nalgebra/local-inspection/src/base/alias_slice.rs",
-  "nalgebra/local-inspection/src/linalg/cholesky.rs",
-  "mySql/local-inspection/conn/mod.rs",
-  "mySql/local-inspection/opts/mod.rs",
-  "mySql/local-inspection/pool/mod.rs",
-  "mySql/local-inspection/io/mod.rs",
-  "mySql/local-inspection/src/query.rs",
-  "mySql/local-inspection/routines/helpers.rs",
-  "rustAnalyzer/local-inspection/crates/ide-db/src/generated/lints.rs",
-  "rustAnalyzer/local-inspection/crates/hir-ty/src/chalk_db.rs",
-  "solana/local-inspection/progress_map.rs",
-  "tokio/local-inspection/tokio/src/net/udp.rs",
-  "tokio/local-inspection/tokio/tests/rt_common.rs",
-  "tokio/local-inspection/tokio/src/time/interval.rs",
-]
+import { rustLocalInspectionCases, rustGlobalInspectionProjects } from "./RustTestCases"
 </script>

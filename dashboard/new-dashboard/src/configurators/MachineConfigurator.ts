77c818c6c2dd1c368e816315c7c5aee8addc519a
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-05-17 14:48:26 +0200

    Add proper Linux icon

--- dashboard/new-dashboard/src/configurators/MachineConfigurator.ts
+++ dashboard/new-dashboard/src/configurators/MachineConfigurator.ts
@@ -165,7 +165,7 @@ export class MachineConfigurator implements DataQueryConfigurator, FilterConfigu
 
   private getIcons(groupName: string): string {
     if (groupName.toLowerCase().startsWith("linux")) {
-      return "pi pi-desktop"
+      return "fa-brands fa-linux"
     } else if (groupName.toLowerCase().startsWith("mac")) {
       return "pi pi-apple"
     } else {

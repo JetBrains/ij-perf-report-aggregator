8c12e8cbd3e86a911b3c14a6285f851336de6e90
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-06-06 16:00:01 +0200

    Fix eslint warning

--- dashboard/new-dashboard/src/components/aia/aia.ts
+++ dashboard/new-dashboard/src/components/aia/aia.ts
@@ -2,5 +2,5 @@ export const aiaModels = ["NativeLLaMA", "JBAICloudWithoutControl", "JBAICloudCo
 export const aiaLanguages = ["css", "csharp", "cpp", "go", "html", "javascript", "java", "kotlin", "php", "python", "ruby", "rust", "scala", "terraform"]
 
 export function getAllProjects(prefix: string) {
-  return aiaLanguages.map((project) => aiaModels.map((model) => prefix + "_" + project + "_" + model)).flat()
+  return aiaLanguages.flatMap((project) => aiaModels.map((model) => prefix + "_" + project + "_" + model))
 }

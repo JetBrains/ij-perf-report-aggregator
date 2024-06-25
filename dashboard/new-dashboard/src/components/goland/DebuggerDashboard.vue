61840a5c108c5005b72cdebfcc0fe4a8f0d3b745
Daniil Maslov <daniil.maslov@jetbrains.com>
2024-06-25 16:30:38 +0200

    [goland] add debugger tab

---
+++ dashboard/new-dashboard/src/components/goland/DebuggerDashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <DashboardPage
+    db-name="perfint"
+    table="goland"
+    persistent-id="goland_debugger_dashboard"
+    initial-machine="linux-blade-hetzner"
+  >
+    <section>
+      <GroupProjectsChart
+        label="Run Debug Configuration"
+        measure="debugRunConfiguration"
+        :projects="['runDebugConfiguration/debug']"
+      />
+      <GroupProjectsChart
+        label="Debug Step Into"
+        measure="debugStep_into"
+        :projects="['runDebugConfiguration/debug']"
+      />
+    </section>
+  </DashboardPage>
+</template>
+
+<script setup lang="ts">
+import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
+import DashboardPage from "../common/DashboardPage.vue"
+</script>

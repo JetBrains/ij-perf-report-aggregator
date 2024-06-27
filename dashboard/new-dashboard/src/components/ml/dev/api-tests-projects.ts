227bddb5a7619efc98841d4aeda4cfdcc777bd2e
Roman.Ivanitskii <roman.ivanitskii@jetbrains.com>
2024-06-27 15:44:40 +0200

    Add labels

--- dashboard/new-dashboard/src/components/ml/dev/api-tests-projects.ts
+++ dashboard/new-dashboard/src/components/ml/dev/api-tests-projects.ts
@@ -2,8 +2,8 @@ import { ChartDefinition } from "../../charts/DashboardCharts"
 import { computed, ComputedRef } from "vue"
 const machine = "Linux EC2 c5.xlarge (4 vCPU, 8 GB)"
 const tests = ["inEditorCodeGeneration_function", "inEditorCodeGeneration_all", "getAvailableProfiles"]
-const defaultMetrics = ["success_rate"]
-const timingMetrics = ["response_time#mean_value"]
+const defaultMetric = "success_rate"
+const timingMetric = "response_time#mean_value"
 const nameSuggestTests = [
   "java_name_suggest_PsiAnnotation",
   "java_name_suggest_PsiClass",
@@ -16,27 +16,27 @@ export function toSeparateCharts(tests: string[]): ComputedRef<ChartDefinition[]
   return computed(() => tests.map((value) => toChartDefinition(value)))
 }
 
-export function toOneCharts(label: string, tests: string[], metrics: string[]): ComputedRef<ChartDefinition[]> {
-  return computed(() => [toCombineChartDefinition(label, tests, metrics)])
+export function toOneCharts(label: string, tests: string[], metric: string): ComputedRef<ChartDefinition[]> {
+  return computed(() => [toCombineChartDefinition(label, tests, metric)])
 }
 
 export const testProjects = toSeparateCharts(tests)
-export const nameSuggestProjects = toOneCharts("Name suggest", nameSuggestTests, defaultMetrics)
-export const timingProjects = toOneCharts("Mean response time", [...nameSuggestTests, ...tests], timingMetrics)
+export const nameSuggestProjects = toOneCharts("Name suggest", nameSuggestTests, defaultMetric)
+export const timingProjects = toOneCharts("Responses", [...nameSuggestTests, ...tests], timingMetric)
 function toChartDefinition(test: string): ChartDefinition {
   return {
-    labels: [test],
+    labels: [test + " (" + defaultMetric + ")"],
     machines: [machine],
-    measures: defaultMetrics,
+    measures: [defaultMetric],
     projects: [test],
   }
 }
 
-function toCombineChartDefinition(label: string, tests: string[], metrics: string[]): ChartDefinition {
+function toCombineChartDefinition(label: string, tests: string[], metric: string): ChartDefinition {
   return {
-    labels: [label],
+    labels: [label + " (" + metric + ")"],
     machines: [machine],
-    measures: metrics,
+    measures: metric,
     projects: tests,
   }
 }

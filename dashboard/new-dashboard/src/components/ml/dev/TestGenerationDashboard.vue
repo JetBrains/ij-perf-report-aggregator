1ce0104f605729ed6e7cdafbe98993db577f9461
eugene.nizienko <eugene.nizienko@jetbrains.com>
2024-07-16 11:39:33 +0200

    Removing prod charts from test generation dashboard

--- dashboard/new-dashboard/src/components/ml/dev/TestGenerationDashboard.vue
+++ dashboard/new-dashboard/src/components/ml/dev/TestGenerationDashboard.vue
@@ -6,12 +6,12 @@
     persistent-id="test-generation"
     :with-installer="false"
   >
-    <ApiTestsDashboardGroupCharts :definitions="testGenerationPassedProjects" />
-    <ApiTestsDashboardGroupCharts :definitions="testGenerationCompilationProjects" />
+    <ApiTestGenerationDashboardGroupCharts :definitions="testGenerationPassedProjects" />
+    <ApiTestGenerationDashboardGroupCharts :definitions="testGenerationCompilationProjects" />
   </DashboardPage>
 </template>
 <script setup lang="ts">
 import DashboardPage from "../../common/DashboardPage.vue"
-import ApiTestsDashboardGroupCharts from "./components/ApiTestsDashboardGroupCharts.vue"
+import ApiTestGenerationDashboardGroupCharts from "./components/ApiTestsDashboardGroupCharts.vue"
 import { testGenerationCompilationProjects, testGenerationPassedProjects } from "./api-tests-projects"
 </script>
---
+++ dashboard/new-dashboard/src/components/ml/dev/components/ApiTestGenerationDashboardGroupCharts.vue
@@ -0,0 +1,35 @@
+<template>
+  <section
+    v-for="definition in definitions"
+    :key="definition.labels[0]"
+    class="flex gap-x-6"
+  >
+    <div class="flex-1 min-w-0">
+      <GroupProjectsChart
+        :label="`stage: ${definition.labels[0]}`"
+        :measure="definition.measures as string[] | string"
+        :projects="definition.projects.map((s) => `stage: ${s}`)"
+        :machines="definition.machines"
+      />
+    </div>
+    <!--<div class="flex-1 min-w-0">-->
+    <!--  <GroupProjectsChart-->
+    <!--    :label="`prod: ${definition.labels[0]}`"-->
+    <!--    :measure="definition.measures as string[] | string"-->
+    <!--    :projects="definition.projects.map((s) => `prod: ${s}`)"-->
+    <!--    :machines="definition.machines"-->
+    <!--  />-->
+    <!--</div>-->
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ChartDefinition } from "../../../charts/DashboardCharts"
+import GroupProjectsChart from "../../../charts/GroupProjectsChart.vue"
+
+interface Props {
+  definitions: ChartDefinition[]
+}
+
+defineProps<Props>()
+</script>

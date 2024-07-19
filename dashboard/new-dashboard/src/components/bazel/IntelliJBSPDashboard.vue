92483a574eead3bbee5ee41aca1cd84aadfa9c0d
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2024-07-19 08:24:11 +0400

    [IntellijBSP] small update;
    add one metric bsp.used.after.indexing.mb
    add project Hirschgarten
    rename bsp.used.at.exit.mb to bsp.used.after.sync.mb
    reorder metrics

--- dashboard/new-dashboard/src/components/bazel/IntelliJBSPDashboard.vue
+++ dashboard/new-dashboard/src/components/bazel/IntelliJBSPDashboard.vue
@@ -25,17 +25,18 @@ import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
 import DashboardPage from "../common/DashboardPage.vue"
 
 const metricsDeclaration = [
-  "add.bsp.fetched.jdks.ms",
-  "apply.changes.on.workspace.model.ms",
-  "bsp.max.used.memory.mb",
   "bsp.sync.project.ms",
-  "bsp.used.at.exit.mb",
-  "calculate.all.unique.jdk.infos.ms",
+  "bsp.used.after.sync.mb",
+  "bsp.used.after.indexing.mb",
+  "bsp.max.used.memory.mb",
   "collect.project.details.ms",
-  "create.target.id.to.module.entities.map.ms",
-  "load.modules.ms",
+  "apply.changes.on.workspace.model.ms",
   "replacebysource.in.apply.on.workspace.model.ms",
   "replaceprojectmodel.in.apply.on.workspace.model.ms",
+  "add.bsp.fetched.jdks.ms",
+  "create.target.id.to.module.entities.map.ms",
+  "load.modules.ms",
+  "calculate.all.unique.jdk.infos.ms",
 ]
 
 const chartsDeclaration: ChartDefinition[] = metricsDeclaration.map((metric) => {
@@ -46,12 +47,13 @@ const chartsDeclaration: ChartDefinition[] = metricsDeclaration.map((metric) => 
       "Bazel (ij)",
       "Bazel-BSP (ij)",
       "Datalore (ij)",
+      "gRPC-Java (ij)",
+      "Hirschgarten (ij)",
       "Synthetic 1 project (ij)",
       "Synthetic 1000 project (ij)",
       "Synthetic 10000 project (ij)",
       "Synthetic 20000 project (ij)",
       "Synthetic 5000 project (ij)",
-      "gRPC-Java (ij)",
     ],
   }
 })

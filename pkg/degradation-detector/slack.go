5971be4bdac90f85696c0d2f8abac2f52280ffe2
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-07-29 17:28:45 +0200

    Add mapping for Windows machines for slack notifications

--- pkg/degradation-detector/slack.go
+++ pkg/degradation-detector/slack.go
@@ -177,6 +177,8 @@ func getMachineGroup(pattern string) string {
   switch pattern {
   case "intellij-linux-performance-aws-%":
     return "Linux EC2 C6id.8xlarge (32 vCPU Xeon, 64 GB)"
+  case "intellij-windows-performance-aws-%":
+    return "Windows EC2 C6id.4xlarge (16 vCPU Xeon, 32 GB)"
   case "intellij-linux-hw-hetzner%":
     return "linux-blade-hetzner"
   case "intellij-linux-%-hetzner-%":

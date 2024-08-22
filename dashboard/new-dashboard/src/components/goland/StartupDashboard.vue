e0881c68209bf449d6a37ae7780a1401e6cf3d31
Daniil Maslov <daniil.maslov@jetbrains.com>
2024-08-22 17:51:55 +0200

    [goland] update startup dashboard

--- dashboard/new-dashboard/src/components/goland/StartupDashboard.vue
+++ dashboard/new-dashboard/src/components/goland/StartupDashboard.vue
@@ -1,41 +1,22 @@
 <template>
   <StartupProductDashboard
     product="GO"
-    default-project="kratos"
-    initial-machine="Linux Munich i7-13700, 64 Gb"
+    default-project="opentelemetry-collector-contrib"
+    initial-machine="Mac Mini M2 Pro (10 vCPU, 32 GB)"
   >
     <template #default="{ configurators }">
-      <Divider label="go list" />
-      <LineChart
-        title="Execution time: Updating Go modules dependencies"
-        :measures="['metrics.progressMetric/Progress: Updating Go modules dependencies']"
-        :configurators="configurators"
-      />
-      <LineChart
-        title="Execution time: go list -m -json"
-        :measures="['metrics.progressMetric/Subtask: go list -m -json']"
-        :configurators="configurators"
-      />
-      <LineChart
-        title="Median value: go list -m -json"
-        :measures="['metrics.progressMetric/Subtask: go list -m -json#mean_value']"
-        :configurators="configurators"
-      />
-      <LineChart
-        title="Number of go list -m -json runs"
-        :measures="['metrics.progressMetric/Subtask: go list -m -json#count']"
-        :configurators="configurators"
-      />
       <Divider label="Warm Startup" />
       <LineChart
-        title="Startup Completed"
-        :measures="['startUpCompleted']"
+        title="Updating Go modules dependencies & go list count"
+        :measures="['metrics.progressMetric/Progress: Updating Go modules dependencies', 'metrics.progressMetric/Subtask: go list -m -json#count']"
         :configurators="configurators"
+        tooltip-trigger="axis"
       />
       <LineChart
-        title="Time from IDE start till File Analysis Finished"
-        :measures="['metrics.totalOpeningTime/timeFromAppStartTillAnalysisFinished']"
+        title="Startup and File Analysis"
+        :measures="['startUpCompleted', 'metrics.totalOpeningTime/timeFromAppStartTillAnalysisFinished']"
         :configurators="configurators"
+        tooltip-trigger="axis"
       />
     </template>
   </StartupProductDashboard>

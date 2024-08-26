c502f58962365a025380a9fae2e80b2c614c24eb
Alexander Karaev <alexander.karaev@jetbrains.com>
2024-08-26 12:12:37 +0300

    [clion] Add tabs to memory dashboard

--- dashboard/new-dashboard/src/components/clion/CLionVsRadlerMemoryUsageChart.vue
+++ dashboard/new-dashboard/src/components/clion/CLionVsRadlerMemoryUsageChart.vue
@@ -1,31 +1,68 @@
 <template>
-  <section class="flex gap-x-6 flex-col md:flex-row">
-    <div class="flex-1 min-w-0">
-      <section>
-        <GroupProjectsChart
-          :label="radlerLabel"
-          :measure="[backendMeasure, frontendMeasure]"
-          :projects="[radlerProject]"
-          :legend-formatter="legendFormatter"
-        />
-      </section>
+  <TabGroup
+    v-slot="{ selectedIndex }"
+    :default-index="1"
+  >
+    <div class="border-b border-gray-200">
+      <TabList class="-mb-px flex space-x-8">
+        <Tab
+          v-for="item in tabDescription"
+          :key="item"
+          v-slot="{ selected }"
+          as="template"
+        >
+          <button
+            :class="[
+              selected ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+              'whitespace-nowrap py-1 px-1 border-b-2 font-medium text-sm',
+            ]"
+          >
+            {{ item }}
+          </button>
+        </Tab>
+      </TabList>
     </div>
 
-    <div class="flex-1 min-w-0">
-      <section>
-        <GroupProjectsChart
-          :label="clionLabel"
-          :measure="[frontendMeasure]"
-          :projects="[clionProject]"
-          :legend-formatter="legendFormatter"
-        />
-      </section>
-    </div>
-  </section>
+    <TabPanels>
+      <TabPanel
+        v-for="item in tabDescription"
+        :key="item"
+        class="p-3"
+        :unmount="false"
+      >
+        <section class="flex gap-x-6 flex-col md:flex-row">
+          <div class="flex-1 min-w-0">
+            <section>
+              <GroupProjectsChart
+                :key="`radler-${selectedIndex}`"
+                :label="`[Radler] ${props.label}, Mb`"
+                :measure="[backendMeasures[selectedIndex], frontendMeasures[selectedIndex]]"
+                :projects="[radlerProject]"
+                :legend-formatter="legendFormatter"
+              />
+            </section>
+          </div>
+
+          <div class="flex-1 min-w-0">
+            <section>
+              <GroupProjectsChart
+                :key="`clion-${selectedIndex}`"
+                :label="`[CLion] ${props.label}, Mb`"
+                :measure="[frontendMeasures[selectedIndex]]"
+                :projects="[clionProject]"
+                :legend-formatter="legendFormatter"
+              />
+            </section>
+          </div>
+        </section>
+      </TabPanel>
+    </TabPanels>
+  </TabGroup>
 </template>
 
 <script setup lang="ts">
 import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/vue"
 
 const props = defineProps<{
   label: string
@@ -33,17 +70,26 @@ const props = defineProps<{
   project: string
 }>()
 
+const getAllMeasures = (prefix: string) => [`${prefix}/beforeGC`, prefix, `${prefix}/idle`]
+const tabDescription = ["Before GC", "After GC", "After GC (idle)"]
 const clionProject = `clion/${props.project}`
 const radlerProject = `radler/${props.project}`
-const clionLabel = `[CLion] ${props.label}, Mb`
-const radlerLabel = `[Radler] ${props.label}, Mb`
-const frontendMeasure = `JVM.heapUsageMb/${props.measure}`
-const backendMeasure = `rd.memory.allocatedManagedMemoryMb/${props.measure}`
+const backendMeasurePrefix = `rd.memory.allocatedManagedMemoryMb/${props.measure}`
+const frontendMeasurePrefix = `JVM.heapUsageMb/${props.measure}`
 
-const legendFormatter = function (name: string) {
+const backendMeasures = getAllMeasures(backendMeasurePrefix)
+const frontendMeasures = getAllMeasures(frontendMeasurePrefix)
+
+const legendFormatter = (name: string) => {
+  const lengthDescComparer = (a: string, b: string) => b.length - a.length
+
+  name = name.replace(clionProject, "JVM (Frontend)") // HACK: remove this line when CLion Classic gets more than one memory metric
+  for (const frontendMeasure of frontendMeasures.toSorted(lengthDescComparer)) {
+    name = name.replace(frontendMeasure, "JVM (Frontend)")
+  }
+  for (const backendMeasure of backendMeasures.toSorted(lengthDescComparer)) {
+    name = name.replace(backendMeasure, ".NET (Backend)")
+  }
   return name
-    .replace(clionProject, "JVM (Frontend)") // HACK: remove this line when CLion Classic gets more than one memory metric
-    .replace(frontendMeasure, "JVM (Frontend)")
-    .replace(backendMeasure, ".NET (Backend)")
 }
 </script>

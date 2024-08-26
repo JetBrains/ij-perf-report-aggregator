1d8dd451cc7d654949d56e7cdf1dd6aabe8052a8
Daniil Maslov <daniil.maslov@jetbrains.com>
2024-08-26 11:38:34 +0200

    [goland] fix typo

--- dashboard/new-dashboard/src/components/goland/InspectionsDashboard.vue
+++ dashboard/new-dashboard/src/components/goland/InspectionsDashboard.vue
@@ -9,7 +9,7 @@
       <GroupProjectsChart
         label="Local inspection execution time"
         measure="localInspections"
-        :projects="['istio/localInspection/asdc.go', 'minotaur/localInspection/server.go']"
+        :projects="['istio/localInspection/adsc.go', 'minotaur/localInspection/server.go']"
       />
     </section>
     <section>

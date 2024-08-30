348a61548bc36311970385357b6da11e98bfa965
Eugene Nizienko <eugene.nizienko@jetbrains.com>
2024-08-30 16:28:32 +0200

    [llm test generation] removed the old model chart; added credit metrics

--- dashboard/new-dashboard/src/components/ml/dev/TestGenerationDashboard.vue
+++ dashboard/new-dashboard/src/components/ml/dev/TestGenerationDashboard.vue
@@ -9,10 +9,18 @@
     <ApiTestGenerationDashboardGroupCharts :definitions="testGenerationPsiValidProjects" />
     <ApiTestGenerationDashboardGroupCharts :definitions="testGenerationCompilationProjects" />
     <ApiTestGenerationDashboardGroupCharts :definitions="testGenerationPassedProjects" />
+    <ApiTestGenerationDashboardGroupCharts :definitions="testGenerationCreditAvgProjects" />
+    <ApiTestGenerationDashboardGroupCharts :definitions="testGenerationQuotaLeftProjects" />
   </DashboardPage>
 </template>
 <script setup lang="ts">
 import DashboardPage from "../../common/DashboardPage.vue"
 import ApiTestGenerationDashboardGroupCharts from "./components/ApiTestGenerationDashboardGroupCharts.vue"
-import { testGenerationCompilationProjects, testGenerationPassedProjects, testGenerationPsiValidProjects } from "./api-tests-projects"
+import {
+  testGenerationCompilationProjects,
+  testGenerationCreditAvgProjects,
+  testGenerationPassedProjects,
+  testGenerationPsiValidProjects,
+  testGenerationQuotaLeftProjects,
+} from "./api-tests-projects"
 </script>
--- dashboard/new-dashboard/src/components/ml/dev/api-tests-projects.ts
+++ dashboard/new-dashboard/src/components/ml/dev/api-tests-projects.ts
@@ -1,5 +1,6 @@
 import { ChartDefinition } from "../../charts/DashboardCharts"
 import { computed, ComputedRef } from "vue"
+
 const machine = "Linux EC2 c5.xlarge (4 vCPU, 8 GB)"
 const tests = ["inEditorCodeGeneration_function", "inEditorCodeGeneration_all", "getAvailableProfiles", "documentation_generation_calculator_sum"]
 const defaultMetric = "success_rate"
@@ -11,13 +12,8 @@ const nameSuggestTests = [
   "java_name_suggest_PsiField",
   "java_name_suggest_PsiMethod",
 ]
-const testGenerationTests = [
-  "test_generation_add_tests: openai-gpt-4",
-  "test_generation_new_tests: openai-gpt-4",
-  "test_generation: default",
-  "test_generation_add_tests: gpt-4o",
-  "test_generation_new_tests: gpt-4o",
-]
+const testGenerationTests = ["test_generation_add_tests: default", "test_generation_new_tests: default"]
+
 export function toSeparateCharts(tests: string[]): ComputedRef<ChartDefinition[]> {
   return computed(() => tests.map((value) => toChartDefinition(value)))
 }
@@ -32,6 +28,9 @@ export const timingProjects = toOneCharts("Responses", [...nameSuggestTests, ...
 export const testGenerationPassedProjects = toOneCharts("Test generation", testGenerationTests, "Passed Rate")
 export const testGenerationCompilationProjects = toOneCharts("Test generation", testGenerationTests, "Compilation Rate")
 export const testGenerationPsiValidProjects = toOneCharts("Test generation", testGenerationTests, "Psi Valid Rate")
+export const testGenerationCreditAvgProjects = toOneCharts("Test generation", testGenerationTests, "Credit Cost Avg")
+export const testGenerationQuotaLeftProjects = toOneCharts("Test generation", testGenerationTests, "QuotaLeft")
+
 function toChartDefinition(test: string): ChartDefinition {
   return {
     labels: [test + " (" + defaultMetric + ")"],

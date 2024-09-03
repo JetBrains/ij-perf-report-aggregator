24668ea918f333a45b6ed035a8e798950c0ebe3b
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-09-03 17:52:55 +0200

    Support collection of Fleet histogram metrics
    The DB scheme is changed since we might have multiple metrics per file now. It's now identical to perfintDev

--- db-schema/fleet/measure.sql
+++ db-schema/fleet/measure.sql
@@ -1,15 +1,43 @@
-create table measure
+-- Description: Table to store the measurements of the test runs
+create table fleet.measure_new
 (
   `machine`               LowCardinality(String) CODEC (ZSTD(20)),
-  `generated_time`        DateTime CODEC (Gorilla, ZSTD(20)),
+  `build_time`            DateTime CODEC (Delta(4), ZSTD(20)),
+  `generated_time`        DateTime CODEC (Delta(4), ZSTD(20)),
   `project`               LowCardinality(String) CODEC (ZSTD(20)),
-  `tc_build_id`           UInt32 CODEC (Gorilla, ZSTD(20)),
+  `tc_build_id`           UInt32 CODEC (DoubleDelta, ZSTD(20)),
   `branch`                LowCardinality(String) CODEC (ZSTD(20)),
-  `triggeredBy`           LowCardinality(String) CODEC (ZSTD(20)),
+  `tc_build_type`         LowCardinality(String) CODEC (ZSTD(20)),
 
-  `name` LowCardinality(String) CODEC(ZSTD(20)),
-  `value` UInt64 CODEC(Gorilla, ZSTD(20))
+  `measures.name`         Array(LowCardinality(String)) CODEC (ZSTD(20)),
+  `measures.value`        Array(Float64) CODEC (Gorilla, ZSTD(20)),
+  `measures.type`         Array(LowCardinality(String)) CODEC (ZSTD(20)),
+
+  `triggeredBy`           LowCardinality(String) CODEC (ZSTD(20))
 )
   engine = MergeTree
-    partition by toYYYYMM(generated_time)
-    order by (branch, machine, project, generated_time)
+    partition by (toYYYYMM(generated_time))
+    order by (branch, project, machine, build_time, generated_time);
+
+-- Copy data to new DB
+INSERT INTO fleet.measure_new
+SELECT
+  machine,
+  generated_time AS build_time,
+  generated_time,
+  project,
+  tc_build_id,
+  branch,
+  '' AS tc_build_type,
+  groupArray(name) AS `measures.name`,
+  groupArray(CAST(value AS Float64) / 1000000) AS `measures.value`,
+  arrayWithConstant(length(groupArray(name)),'d') AS `measures.type`,
+  triggeredBy
+FROM fleet.measure
+GROUP BY
+  machine,
+  generated_time,
+  project,
+  tc_build_id,
+  branch,
+  triggeredBy;

4df58cabda3000049aa5c06d2ae3cbe52cfa8e6c
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-09-03 13:43:38 +0200

    Don't specify machine since we want to match all machines

--- dashboard/new-dashboard/src/components/ml/dev/api-tests-projects.ts
+++ dashboard/new-dashboard/src/components/ml/dev/api-tests-projects.ts
@@ -1,7 +1,6 @@
 import { ChartDefinition } from "../../charts/DashboardCharts"
 import { computed, ComputedRef } from "vue"
 
-const machine = "Linux EC2 c5.xlarge (4 vCPU, 8 GB)"
 const tests = ["inEditorCodeGeneration_function", "inEditorCodeGeneration_all", "getAvailableProfiles", "documentation_generation_calculator_sum"]
 const defaultMetric = "success_rate"
 const timingMetric = "response_time#mean_value"
@@ -34,7 +33,6 @@ export const testGenerationQuotaLeftProjects = toOneCharts("Test generation", te
 function toChartDefinition(test: string): ChartDefinition {
   return {
     labels: [test + " (" + defaultMetric + ")"],
-    machines: [machine],
     measures: [defaultMetric],
     projects: [test],
   }
@@ -43,7 +41,6 @@ function toChartDefinition(test: string): ChartDefinition {
 function toCombineChartDefinition(label: string, tests: string[], metric: string): ChartDefinition {
   return {
     labels: [label + " (" + metric + ")"],
-    machines: [machine],
     measures: [metric],
     projects: tests,
   }

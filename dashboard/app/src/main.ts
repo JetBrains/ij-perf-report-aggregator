eb5350534278fe645e4e66e953b84af93e58c4d5
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-09-17 17:40:00 +0200

    Switch to default theme

--- dashboard/app/src/main.ts
+++ dashboard/app/src/main.ts
@@ -11,18 +11,22 @@ import { createAndConfigureRouter } from "./route"
 // get rid of color.png
 // avoid tiff/svg/other deprecated stuff in a final build
 import "./primeicons.css"
-import "../prime-theme/themes/aura/aura-light/blue/theme.scss"
-
-import "new-dashboard/src/primevue-theme/select.css"
-import "new-dashboard/src/primevue-theme/select-panel.css"
-import "new-dashboard/src/primevue-theme/misc.css"
+import { MyPreset } from "./theme"
 
 async function initApp() {
   const app = createApp(App)
   const router = createAndConfigureRouter()
   const pinia = createPinia()
   app.use(router)
-  app.use(PrimeVue)
+  app.use(PrimeVue, {
+    theme: {
+      preset: MyPreset,
+      cssLayer: {
+        name: "primevue",
+        order: "tailwind-base, primevue, tailwind-utilities",
+      },
+    },
+  })
   app.use(ToastService)
   app.use(pinia)
   app.directive("tooltip", Tooltip)
---
+++ dashboard/app/src/theme.ts
@@ -0,0 +1,20 @@
+import { definePreset } from "@primevue/themes"
+import Aura from "@primevue/themes/aura"
+
+export const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: "{blue.50}",
+      100: "{blue.100}",
+      200: "{blue.200}",
+      300: "{blue.300}",
+      400: "{blue.400}",
+      500: "{blue.500}",
+      600: "{blue.600}",
+      700: "{blue.700}",
+      800: "{blue.800}",
+      900: "{blue.900}",
+      950: "{blue.950}",
+    },
+  },
+})

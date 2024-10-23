adc31c1036036ef55b427db1aba4b5ffa632ae45
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-10-23 13:09:59 +0200

    Add #firstElementShown for split tests in Ruby

--- dashboard/new-dashboard/src/components/rubymine/PerformanceDashboard.vue
+++ dashboard/new-dashboard/src/components/rubymine/PerformanceDashboard.vue
@@ -186,6 +186,54 @@
     </section>
     <section>
       <GroupProjectsWithClientChart
+        label="Completion First Element (Diaspora)"
+        measure="completion#firstElementShown"
+        :projects="[
+          'diaspora-project-test/completion/routes',
+          'diaspora-project-test/completion/exceptions',
+          'diaspora-project-test/completion/localization',
+          'diaspora-project-test/completion/constant',
+          'diaspora-project-test/completion/exceptions-prefix',
+          'diaspora-project-test/completion/method',
+          'diaspora-project-test/completion/qualified',
+        ]"
+        :aliases="['Routes', 'Exceptions', 'I18n#t', 'Constant', 'Exceptions (prefix)', 'Method', 'Qualified']"
+      />
+    </section>
+    <section>
+      <GroupProjectsWithClientChart
+        label="Completion First Element (GitLab)"
+        measure="completion#firstElementShown"
+        :projects="[
+          'gitlab-project-test/completion/routes',
+          'gitlab-project-test/completion/exceptions',
+          'gitlab-project-test/completion/localization',
+          'gitlab-project-test/completion/constant',
+          'gitlab-project-test/completion/exceptions-prefix',
+          'gitlab-project-test/completion/method',
+          'gitlab-project-test/completion/qualified',
+        ]"
+        :aliases="['Routes', 'Exceptions', 'I18n#t', 'Constant', 'Exceptions (prefix)', 'Method', 'Qualified']"
+      />
+    </section>
+    <section>
+      <GroupProjectsWithClientChart
+        label="Completion First Element (Redmine)"
+        measure="completion#firstElementShown"
+        :projects="[
+          'redmine-project-test/completion/routes',
+          'redmine-project-test/completion/exceptions',
+          'redmine-project-test/completion/localization',
+          'redmine-project-test/completion/constant',
+          'redmine-project-test/completion/exceptions-prefix',
+          'redmine-project-test/completion/method',
+          'redmine-project-test/completion/qualified',
+        ]"
+        :aliases="['Routes', 'Exceptions', 'I18n#t', 'Constant', 'Exceptions (prefix)', 'Method', 'Qualified']"
+      />
+    </section>
+    <section>
+      <GroupProjectsWithClientChart
         label="Typing: Average AWT Delay"
         measure="test#average_awt_delay"
         :projects="[

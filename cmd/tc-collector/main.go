38b2f0b499af17877b8d9a1b1b3d1cc50e280c0a
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-11-27 18:33:45 +0100

    JBR switched to usual TC configuration and now have groups per project

--- cmd/tc-collector/main.go
+++ cmd/tc-collector/main.go
@@ -100,21 +100,6 @@ func configureCollectFromTeamCity() error {
 					}
 				}
 			}
-		case chunk.Database == "jbr":
-			jbrTypes := []string{
-				"macOS13aarch64", "macOS13aarch64Metal", "macOS13aarch64OGL",
-				"macOS13x86_64", "macOS13x86_64Metal", "macOS13x86_64OGL",
-				"macOS14x86_64", "macOS14x86_64Metal", "macOS14x86_64OGL",
-				"macOS15aarch64mini", "macOS15aarch64miniMetal", "macOS15aarch64miniOGL",
-				"macOS15x86_64mini", "macOS15x86_64miniMetal", "macOS15x86_64miniOGL",
-				"Ubuntu2004x86_64", "Ubuntu2004x86_64OGL",
-				"Windows10x86_64",
-			}
-			for _, configuration := range chunk.Configurations {
-				for _, jbrType := range jbrTypes {
-					buildConfigurationIds = append(buildConfigurationIds, configuration+"_"+jbrType)
-				}
-			}
 		default:
 			for _, configuration := range chunk.Configurations {
 				collector := &Collector{

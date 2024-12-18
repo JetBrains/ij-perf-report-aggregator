ba7091765fba81ba29dd5b145da1fdcd3a373916
Eugene Morozov <eugene.morozov@jetbrains.com>
2024-12-18 14:57:17 +0100

    AT-693 Add vfsRefreshAfterMassModify metric to VFS Refresh after Mass Changes chart

--- dashboard/new-dashboard/src/components/intelliJ/PerformanceDashboard.vue
+++ dashboard/new-dashboard/src/components/intelliJ/PerformanceDashboard.vue
@@ -55,7 +55,7 @@ const chartsDeclaration: ChartDefinition[] = [
   },
   {
     labels: ["VFS Refresh after Mass Changes"],
-    measures: [["vfsRefreshAfterMassCreate", "vfsRefreshAfterMassDelete"]],
+    measures: [["vfsRefreshAfterMassCreate", "vfsRefreshAfterMassModify", "vfsRefreshAfterMassDelete"]],
     projects: ["empty_project/vfs-mass-update-txt", "empty_project/vfs-mass-update-java", "empty_project/vfs-mass-update-kt"],
     aliases: ["txt", "java", "kotlin"],
   },

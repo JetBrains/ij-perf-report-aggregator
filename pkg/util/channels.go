e7d32a00131f8d1d63a3d223174b849820ab8eb1
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2025-01-24 11:41:22 +0100

    Fix panic: send on closed channel

--- pkg/util/channels.go
+++ pkg/util/channels.go
@@ -1,14 +1,20 @@
 package util
 
+import "sync"
+
 func Broadcast[T any](input <-chan T, outputs ...chan T) {
+	var wg sync.WaitGroup
 	go func() {
 		for value := range input {
 			for _, out := range outputs {
+				wg.Add(1)
 				go func() {
+					defer wg.Done()
 					out <- value
 				}()
 			}
 		}
+		wg.Wait()
 		// Close all output channels when input is exhausted
 		for _, out := range outputs {
 			close(out)

482507e0b4fdf457e29a31c8bc94bc8c563a54de
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2025-01-25 11:39:09 +0100

    The issue is resolved, disable additional logging

--- pkg/degradation-detector/inferDegradations.go
+++ pkg/degradation-detector/inferDegradations.go
@@ -1,7 +1,5 @@
 package degradation_detector
 
-import "log/slog"
-
 type DegradationWithSettings struct {
 	Details  Degradation
 	Settings Settings
@@ -11,7 +9,6 @@ func InferDegradations(data <-chan QueryResultWithSettings) <-chan DegradationWi
 	degradationChan := make(chan DegradationWithSettings, 100)
 	go func() {
 		for datum := range data {
-			slog.Info("infer degradations", "settings", datum.Settings)
 			for _, degradation := range detectDegradations(datum.values, datum.builds, datum.timestamps, datum.Settings) {
 				degradationChan <- DegradationWithSettings{Details: degradation, Settings: datum.Settings}
 			}

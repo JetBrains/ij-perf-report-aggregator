aa142e03774002c1f9b6387436abaf09017787b8
Litemn <Litemn@users.noreply.github.com>
2025-02-26 14:42:06 +0000

    [junie] [junie] Update AIACodeGeneration.vue dashboard changes from the task: "214"

--- dashboard/new-dashboard/src/components/aia/aia.ts
+++ dashboard/new-dashboard/src/components/aia/aia.ts
@@ -1,5 +1,5 @@
 export const aiaModels = ["NativeLLaMA", "JBAICloudWithoutControl", "JBAICloudControl", ""]
-export const aiaLanguages = ["css", "csharp", "cpp", "go", "html", "javascript", "java", "kotlin", "php", "python", "ruby", "rust", "scala", "terraform"]
+export const aiaLanguages = ["css", "csharp", "cpp", "html", "javascript", "idea", "kotlin", "php", "python", "ruby", "rust", "scala", "terraform"]
 
 export function getAllProjects(prefix: string) {
   return aiaLanguages.flatMap((project) => aiaModels.map((model) => prefix + "_" + project + "_" + model))

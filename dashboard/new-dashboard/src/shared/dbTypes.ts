f6c6e92cac05c7e787faa851d35e337ffb1ae0d5
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2025-04-26 22:16:34 +0200

    Fix prettier

--- dashboard/new-dashboard/src/shared/dbTypes.ts
+++ dashboard/new-dashboard/src/shared/dbTypes.ts
@@ -53,7 +53,13 @@ export const dbTypeStore = defineStore("dbTypeStore", () => {
   }
 
   function isModeSupported(): boolean {
-    return dbType.value == DBType.INTELLIJ || dbType.value == DBType.INTELLIJ_DEV || dbType.value == DBType.PERF_UNIT_TESTS || dbType.value == DBType.DIOGEN || dbType.value == DBType.TOOLBOX
+    return (
+      dbType.value == DBType.INTELLIJ ||
+      dbType.value == DBType.INTELLIJ_DEV ||
+      dbType.value == DBType.PERF_UNIT_TESTS ||
+      dbType.value == DBType.DIOGEN ||
+      dbType.value == DBType.TOOLBOX
+    )
   }
 
   return { dbType, setDbType, isStartup, isIJStartup, isModeSupported }

7498cdc81096433145a4e3698bcc180bf00d096a
Alexander.Danilov <alexander.danilov@jetbrains.com>
2025-05-07 12:53:01 +0200

    Add Chat/Context chart

--- dashboard/new-dashboard/src/components/ml/dev/LLMDashboard.vue
+++ dashboard/new-dashboard/src/components/ml/dev/LLMDashboard.vue
@@ -27,9 +27,32 @@
         :projects="['gradle-calculator_CodeGenerationPerformanceTest/generate code']"
       ></GroupProjectsChart>
     </section>
+    <section>
+      <GroupProjectsChart
+        label="Chat/Context"
+        :measure="[
+          'SimpleCompletableMessage::Collecting context…',
+          'SimpleCompletableMessage::Generating answer…',
+          'retrieveContextAsync.Chat Submit(userInput=&quot;test&quot;, chatSession=ChatRetrievalSession(&quot;New Chat&quot;), chatRetrieversType=SLOW).time.max',
+          'retrieveContextAsync.Initialize chat.time.max',
+          'retrieveContextAsync.[Deprecated] Chat Retrieval(userInput=&quot;test&quot;, chatSession=ChatRetrievalSession(&quot;New Chat&quot;), chatRetrieversType=ALL).time.max',
+          'computeContext.PsiFileSearchRetriever.[Deprecated] Chat Retrieval(userInput=&quot;test&quot;, chatSession=ChatRetrievalSession(&quot;New Chat&quot;), chatRetrieversType=ALL).time.max',
+        ]"
+        :legend-formatter="chatContextMetricsLegendFormatter"
+        :projects="['kotlinx_coroutines_k2_dev_ContextPerformanceTest/basic context performance test']"
+      ></GroupProjectsChart>
+    </section>
   </DashboardPage>
 </template>
 <script setup lang="ts">
 import GroupProjectsChart from "../../charts/GroupProjectsChart.vue"
 import DashboardPage from "../../common/DashboardPage.vue"
+
+const chatContextMetricsLegendFormatter = (name: string) => {
+  if (name.startsWith("retrieveContextAsync.Chat Submit")) return "retrieveContext.Chat Submit"
+  else if (name.startsWith("retrieveContextAsync.Initialize chat")) return "retrieveContext.Initialize chat"
+  else if (name.startsWith("retrieveContextAsync.[Deprecated] Chat Retrieval")) return "retrieveContext.Chat Retrieval[Deprecated]"
+  else if (name.startsWith("computeContext.PsiFileSearchRetriever")) return "PsiFileSearchRetriever"
+  else return name.replace("SimpleCompletableMessage::", "")
+}
 </script>

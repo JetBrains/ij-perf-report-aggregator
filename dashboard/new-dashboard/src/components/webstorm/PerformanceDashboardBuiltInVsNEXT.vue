ddb5b9e6d207c0adc7cc2f5a8d2183a9be86d5e7
Aleksandra Poluianova <aleksandra.poluianova@jetbrains.com>
2025-07-04 14:47:56 +0200

    Corrected measure for Finding Usages

--- dashboard/new-dashboard/src/components/webstorm/PerformanceDashboardBuiltInVsNEXT.vue
+++ dashboard/new-dashboard/src/components/webstorm/PerformanceDashboardBuiltInVsNEXT.vue
@@ -171,7 +171,7 @@ const groups = [
   },
   {
     label: "Find Usages",
-    measure: "find_Usages_Duration",
+    measure: "findUsages",
     projects: ["pancake/Model.tsxFindUsages", "backstage/types.tsFindUsages"],
   },
 ]

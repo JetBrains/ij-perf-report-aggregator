8a36fcbaf5880aa2782dc931cf8c9b72ca4a871c
Anna Koehler <anna.marchenko@jetbrains.com>
2025-07-24 07:38:55 +0200

    Adjust dashboard for WSl

--- dashboard/new-dashboard/src/components/intelliJ/PerformanceWSLDashboard.vue
+++ dashboard/new-dashboard/src/components/intelliJ/PerformanceWSLDashboard.vue
@@ -42,74 +42,9 @@ const chartsDeclaration: ChartDefinition[] = [
     projects: ["community/indexing", "spring-pet-clinic-gradle/indexing", "spring-pet-clinic-maven/indexing"],
   },
   {
-    labels: ["Rebuild of Community"],
+    labels: ["Rebuild"],
     measures: ["build_compilation_duration"],
     projects: ["community/rebuild"],
-  },
-  {
-    labels: ["Eslint indexing WSL/Windows"],
-    measures: ["indexingTimeWithoutPauses"],
-    projects: ["indexing-eslint/wsl", "indexing-eslint/windows"],
-  },
-  {
-    labels: ["Eslint scanning WSL/Windows"],
-    measures: ["scanningTimeWithoutPauses"],
-    projects: ["indexing-eslint/wsl", "indexing-eslint/windows"],
-  },
-  {
-    labels: ["Eslint number of indexed files WSL/Windows"],
-    measures: ["numberOfIndexedFiles"],
-    projects: ["indexing-eslint/wsl", "indexing-eslint/windows"],
-  },
-  {
-    labels: ["Design Patterns indexing WSL/Windows"],
-    measures: ["indexingTimeWithoutPauses"],
-    projects: ["indexing-java/wsl", "indexing-java/windows"],
-  },
-  {
-    labels: ["Design Patterns scanning WSL/Windows"],
-    measures: ["scanningTimeWithoutPauses"],
-    projects: ["indexing-java/wsl", "indexing-java/windows"],
-  },
-  {
-    labels: ["Design Patterns number of indexed files WSL/Windows"],
-    measures: ["numberOfIndexedFiles"],
-    projects: ["indexing-java/wsl", "indexing-java/windows"],
-  },
-  {
-    labels: ["Community indexing WSL/Windows"],
-    measures: ["indexingTimeWithoutPauses"],
-    projects: ["indexing-sources-intellij-community-master/wsl", "indexing-sources-intellij-community-master/windows"],
-  },
-  {
-    labels: ["Community scanning WSL/Windows"],
-    measures: ["scanningTimeWithoutPauses"],
-    projects: ["indexing-sources-intellij-community-master/wsl", "indexing-sources-intellij-community-master/windows"],
-  },
-  {
-    labels: ["Community number of indexed files WSL/Windows"],
-    measures: ["numberOfIndexedFiles"],
-    projects: ["indexing-sources-intellij-community-master/wsl", "indexing-sources-intellij-community-master/windows"],
-  },
-  {
-    labels: ["Indexing on WSL"],
-    measures: ["indexingTimeWithoutPauses"],
-    projects: ["wsl-java/indexing", "wsl-spring-pet-clinic-gradle/indexing", "wsl-spring-pet-clinic-maven/indexing", "wsl-empty_project/indexing", "wsl-grails/indexing"],
-  },
-  {
-    labels: ["Scanning on WSL"],
-    measures: ["scanningTimeWithoutPauses"],
-    projects: ["wsl-java/indexing", "wsl-spring-pet-clinic-gradle/indexing", "wsl-spring-pet-clinic-maven/indexing", "wsl-empty_project/indexing", "wsl-grails/indexing"],
-  },
-  {
-    labels: ["Number of indexed files on WSL"],
-    measures: ["numberOfIndexedFiles"],
-    projects: ["wsl-java/indexing", "wsl-spring-pet-clinic-gradle/indexing", "wsl-spring-pet-clinic-maven/indexing", "wsl-empty_project/indexing", "wsl-grails/indexing"],
-  },
-  {
-    labels: ["Rebuild on WSL"],
-    measures: ["build_compilation_duration"],
-    projects: ["wsl-empty_project/rebuild", "wsl-java/rebuild", "wsl-grails/rebuild"],
   },
 ]
 

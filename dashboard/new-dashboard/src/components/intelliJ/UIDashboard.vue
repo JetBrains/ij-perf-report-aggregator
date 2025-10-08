7676e02bfd6d2ed2e896a32a2f84722328c57cdd
Anna Koehler <anna.marchenko@jetbrains.com>
2025-10-08 21:05:17 +0200

    Dashboard with UI metrics has been updated: the cases with new SE have been added, charts have been split into cold and warm SE

--- dashboard/new-dashboard/src/components/intelliJ/UIDashboard.vue
+++ dashboard/new-dashboard/src/components/intelliJ/UIDashboard.vue
@@ -131,7 +131,7 @@ const chartsDeclaration: ChartDefinition[] = [
     ],
   },
   {
-    labels: ["Search Everywhere Action (slow typing)", "SE First Element Added Action (slow typing)"],
+    labels: ["Cold Search Everywhere Action (slow typing)", "Cold SE First Element Added Action (slow typing)"],
     measures: ["searchEverywhere", "searchEverywhere_first_elements_added"],
     projects: [
       "community/go-to-action/Kotlin/typingLetterByLetter",
@@ -139,22 +139,39 @@ const chartsDeclaration: ChartDefinition[] = [
       "community/go-to-action/Runtime/typingLetterByLetter",
       "community/go-to-action-finished-embeddings/Runtime/typingLetterByLetter",
       "java/go-to-action/Runtime/typingLetterByLetter",
+
+      "intellij_commit/go-to-action/Kotlin/typingLetterByLetter",
+      "intellij_commit/go-to-action/Editor/typingLetterByLetter",
+      "intellij_commit/go-to-action/Runtime/typingLetterByLetter",
+      "intellij_commit/go-to-action-finished-embeddings/Runtime/typingLetterByLetter",
+
+      "intellij_commit/new-se-go-to-action/Kotlin/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-action/Editor/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-action/Runtime/typingLetterByLetter",
+    ],
+  },
+  {
+    labels: ["Warm Search Everywhere Action (slow typing)", "Warm SE First Element Added Action (slow typing)"],
+    measures: ["searchEverywhere", "searchEverywhere_first_elements_added"],
+    projects: [
       "community/go-to-action-with-warmup/Kotlin/typingLetterByLetter",
       "community/go-to-action-with-warmup/Editor/typingLetterByLetter",
       "community/go-to-action-with-warmup/Runtime/typingLetterByLetter",
       "community/go-to-action-with-warmup-finished-embeddings/Runtime/typingLetterByLetter",
       "java/go-to-action-with-warmup/Runtime/typingLetterByLetter",
-      "intellij_commit/go-to-action/Kotlin/typingLetterByLetter",
-      "intellij_commit/go-to-action/Editor/typingLetterByLetter",
-      "intellij_commit/go-to-action/Runtime/typingLetterByLetter",
+
       "intellij_commit/go-to-action-with-warmup/Kotlin/typingLetterByLetter",
       "intellij_commit/go-to-action-with-warmup/Editor/typingLetterByLetter",
       "intellij_commit/go-to-action-with-warmup/Runtime/typingLetterByLetter",
       "intellij_commit/go-to-action-with-warmup-finished-embeddings/Runtime/typingLetterByLetter",
+
+      "intellij_commit/new-se-go-to-action-with-warmup/Kotlin/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-action-with-warmup/Editor/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-action-with-warmup/Runtime/typingLetterByLetter",
     ],
   },
   {
-    labels: ["Search Everywhere Class (slow typing)", "SE First Element Added Class (slow typing)"],
+    labels: ["Cold Search Everywhere Class (slow typing)", "Cold SE First Element Added Class (slow typing)"],
     measures: ["searchEverywhere", "searchEverywhere_first_elements_added"],
     projects: [
       "community/go-to-class/Kotlin/typingLetterByLetter",
@@ -162,22 +179,39 @@ const chartsDeclaration: ChartDefinition[] = [
       "community/go-to-class/Runtime/typingLetterByLetter",
       "community/go-to-class-finished-embeddings/Runtime/typingLetterByLetter",
       "java/go-to-class/Runtime/typingLetterByLetter",
+
+      "intellij_commit/go-to-class/Kotlin/typingLetterByLetter",
+      "intellij_commit/go-to-class/Editor/typingLetterByLetter",
+      "intellij_commit/go-to-class/Runtime/typingLetterByLetter",
+      "intellij_commit/go-to-class-finished-embeddings/Runtime/typingLetterByLetter",
+
+      "intellij_commit/new-se-go-to-class/Kotlin/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-class/Editor/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-class/Runtime/typingLetterByLetter",
+    ],
+  },
+  {
+    labels: ["Warm Search Everywhere Class (slow typing)", "Warm SE First Element Added Class (slow typing)"],
+    measures: ["searchEverywhere", "searchEverywhere_first_elements_added"],
+    projects: [
       "community/go-to-class-with-warmup/Kotlin/typingLetterByLetter",
       "community/go-to-class-with-warmup/Editor/typingLetterByLetter",
       "community/go-to-class-with-warmup/Runtime/typingLetterByLetter",
       "community/go-to-class-with-warmup-finished-embeddings/Runtime/typingLetterByLetter",
       "java/go-to-class-with-warmup/Runtime/typingLetterByLetter",
-      "intellij_commit/go-to-class/Kotlin/typingLetterByLetter",
-      "intellij_commit/go-to-class/Editor/typingLetterByLetter",
-      "intellij_commit/go-to-class/Runtime/typingLetterByLetter",
+
       "intellij_commit/go-to-class-with-warmup/Kotlin/typingLetterByLetter",
       "intellij_commit/go-to-class-with-warmup/Editor/typingLetterByLetter",
       "intellij_commit/go-to-class-with-warmup/Runtime/typingLetterByLetter",
       "intellij_commit/go-to-class-with-warmup-finished-embeddings/Runtime/typingLetterByLetter",
+
+      "intellij_commit/new-se-go-to-class-with-warmup/Kotlin/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-class-with-warmup/Editor/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-class-with-warmup/Runtime/typingLetterByLetter",
     ],
   },
   {
-    labels: ["Search Everywhere File (slow typing)", "SE First Element Added File(slow typing)"],
+    labels: ["Cold Search Everywhere File (slow typing)", "Cold SE First Element Added File(slow typing)"],
     measures: ["searchEverywhere", "searchEverywhere_first_elements_added"],
     projects: [
       "community/go-to-file/Editor/typingLetterByLetter",
@@ -185,22 +219,39 @@ const chartsDeclaration: ChartDefinition[] = [
       "community/go-to-file/Runtime/typingLetterByLetter",
       "community/go-to-file-finished-embeddings/Runtime/typingLetterByLetter",
       "java/go-to-file/Runtime/typingLetterByLetter",
+
+      "intellij_commit/go-to-file/Kotlin/typingLetterByLetter",
+      "intellij_commit/go-to-file/Editor/typingLetterByLetter",
+      "intellij_commit/go-to-file/Runtime/typingLetterByLetter",
+      "intellij_commit/go-to-file-finished-embeddings/Runtime/typingLetterByLetter",
+
+      "intellij_commit/new-se-go-to-file/Kotlin/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-file/Editor/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-file/Runtime/typingLetterByLetter",
+    ],
+  },
+  {
+    labels: ["Warm Search Everywhere File (slow typing)", "Warm SE First Element Added File(slow typing)"],
+    measures: ["searchEverywhere", "searchEverywhere_first_elements_added"],
+    projects: [
       "community/go-to-file-with-warmup/Editor/typingLetterByLetter",
       "community/go-to-file-with-warmup/Kotlin/typingLetterByLetter",
       "community/go-to-file-with-warmup/Runtime/typingLetterByLetter",
       "community/go-to-file-with-warmup-finished-embeddings/Runtime/typingLetterByLetter",
       "java/go-to-file-with-warmup/Runtime/typingLetterByLetter",
-      "intellij_commit/go-to-file/Kotlin/typingLetterByLetter",
-      "intellij_commit/go-to-file/Editor/typingLetterByLetter",
-      "intellij_commit/go-to-file/Runtime/typingLetterByLetter",
+
       "intellij_commit/go-to-file-with-warmup/Kotlin/typingLetterByLetter",
       "intellij_commit/go-to-file-with-warmup/Editor/typingLetterByLetter",
       "intellij_commit/go-to-file-with-warmup/Runtime/typingLetterByLetter",
       "intellij_commit/go-to-file-with-warmup-finished-embeddings/Runtime/typingLetterByLetter",
+
+      "intellij_commit/new-se-go-to-file-with-warmup/Kotlin/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-file-with-warmup/Editor/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-file-with-warmup/Runtime/typingLetterByLetter",
     ],
   },
   {
-    labels: ["Search Everywhere All (slow typing)", "SE First Element Added All(slow typing)"],
+    labels: ["Cold Search Everywhere All (slow typing)", "Cold SE First Element Added All(slow typing)"],
     measures: ["searchEverywhere", "searchEverywhere_first_elements_added"],
     projects: [
       "community/go-to-all/Editor/typingLetterByLetter",
@@ -208,22 +259,39 @@ const chartsDeclaration: ChartDefinition[] = [
       "community/go-to-all/Runtime/typingLetterByLetter",
       "community/go-to-all-finished-embeddings/Runtime/typingLetterByLetter",
       "java/go-to-all/Runtime/typingLetterByLetter",
+
+      "intellij_commit/go-to-all/Kotlin/typingLetterByLetter",
+      "intellij_commit/go-to-all/Editor/typingLetterByLetter",
+      "intellij_commit/go-to-all/Runtime/typingLetterByLetter",
+      "intellij_commit/go-to-all-finished-embeddings/Runtime/typingLetterByLetter",
+
+      "intellij_commit/new-se-go-to-all/Kotlin/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-all/Editor/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-all/Runtime/typingLetterByLetter",
+    ],
+  },
+  {
+    labels: ["Warm Search Everywhere All (slow typing)", "Warm SE First Element Added All(slow typing)"],
+    measures: ["searchEverywhere", "searchEverywhere_first_elements_added"],
+    projects: [
       "community/go-to-all-with-warmup/Editor/typingLetterByLetter",
       "community/go-to-all-with-warmup/Kotlin/typingLetterByLetter",
       "community/go-to-all-with-warmup/Runtime/typingLetterByLetter",
       "community/go-to-all-with-warmup-finished-embeddings/Runtime/typingLetterByLetter",
       "java/go-to-all-with-warmup/Runtime/typingLetterByLetter",
-      "intellij_commit/go-to-all/Kotlin/typingLetterByLetter",
-      "intellij_commit/go-to-all/Editor/typingLetterByLetter",
-      "intellij_commit/go-to-all/Runtime/typingLetterByLetter",
+
       "intellij_commit/go-to-all-with-warmup/Kotlin/typingLetterByLetter",
       "intellij_commit/go-to-all-with-warmup/Editor/typingLetterByLetter",
       "intellij_commit/go-to-all-with-warmup/Runtime/typingLetterByLetter",
       "intellij_commit/go-to-all-with-warmup-finished-embeddings/Runtime/typingLetterByLetter",
+
+      "intellij_commit/new-se-go-to-all-with-warmup/Kotlin/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-all-with-warmup/Editor/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-all-with-warmup/Runtime/typingLetterByLetter",
     ],
   },
   {
-    labels: ["Search Everywhere Symbol (slow typing)", "SE First Element Symbol (slow typing)"],
+    labels: ["Cold Search Everywhere Symbol (slow typing)", "Cold SE First Element Symbol (slow typing)"],
     measures: ["searchEverywhere", "searchEverywhere_first_elements_added"],
     projects: [
       "community/go-to-symbol/Editor/typingLetterByLetter",
@@ -231,22 +299,39 @@ const chartsDeclaration: ChartDefinition[] = [
       "community/go-to-symbol/Runtime/typingLetterByLetter",
       "community/go-to-symbol-finished-embeddings/Runtime/typingLetterByLetter",
       "java/go-to-symbol/Runtime/typingLetterByLetter",
+
+      "intellij_commit/go-to-symbol/Kotlin/typingLetterByLetter",
+      "intellij_commit/go-to-symbol/Editor/typingLetterByLetter",
+      "intellij_commit/go-to-symbol/Runtime/typingLetterByLetter",
+      "intellij_commit/go-to-symbol-finished-embeddings/Runtime/typingLetterByLetter",
+
+      "intellij_commit/new-se-go-to-symbol/Kotlin/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-symbol/Editor/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-symbol/Runtime/typingLetterByLetter",
+    ],
+  },
+  {
+    labels: ["Warm Search Everywhere Symbol (slow typing)", "Warm SE First Element Symbol (slow typing)"],
+    measures: ["searchEverywhere", "searchEverywhere_first_elements_added"],
+    projects: [
       "community/go-to-symbol-with-warmup/Editor/typingLetterByLetter",
       "community/go-to-symbol-with-warmup/Kotlin/typingLetterByLetter",
       "community/go-to-symbol-with-warmup/Runtime/typingLetterByLetter",
       "community/go-to-symbol-with-warmup-finished-embeddings/Runtime/typingLetterByLetter",
       "java/go-to-symbol-with-warmup/Runtime/typingLetterByLetter",
-      "intellij_commit/go-to-symbol/Kotlin/typingLetterByLetter",
-      "intellij_commit/go-to-symbol/Editor/typingLetterByLetter",
-      "intellij_commit/go-to-symbol/Runtime/typingLetterByLetter",
+
       "intellij_commit/go-to-symbol-with-warmup/Kotlin/typingLetterByLetter",
       "intellij_commit/go-to-symbol-with-warmup/Editor/typingLetterByLetter",
       "intellij_commit/go-to-symbol-with-warmup/Runtime/typingLetterByLetter",
       "intellij_commit/go-to-symbol-with-warmup-finished-embeddings/Runtime/typingLetterByLetter",
+
+      "intellij_commit/new-se-go-to-symbol-with-warmup/Kotlin/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-symbol-with-warmup/Editor/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-symbol-with-warmup/Runtime/typingLetterByLetter",
     ],
   },
   {
-    labels: ["Search Everywhere Text (slow typing)", "SE First Element Text (slow typing)"],
+    labels: ["Cold Search Everywhere Text (slow typing)", "Cold SE First Element Text (slow typing)"],
     measures: ["searchEverywhere", "searchEverywhere_first_elements_added"],
     projects: [
       "community/go-to-text/Editor/typingLetterByLetter",
@@ -254,18 +339,35 @@ const chartsDeclaration: ChartDefinition[] = [
       "community/go-to-text/Runtime/typingLetterByLetter",
       "community/go-to-text-finished-embeddings/Runtime/typingLetterByLetter",
       "java/go-to-text/Runtime/typingLetterByLetter",
+
+      "intellij_commit/go-to-text/Kotlin/typingLetterByLetter",
+      "intellij_commit/go-to-text/Editor/typingLetterByLetter",
+      "intellij_commit/go-to-text/Runtime/typingLetterByLetter",
+      "intellij_commit/go-to-text-finished-embeddings/Runtime/typingLetterByLetter",
+
+      "intellij_commit/new-se-go-to-text/Kotlin/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-text/Editor/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-text/Runtime/typingLetterByLetter",
+    ],
+  },
+  {
+    labels: ["Warm Search Everywhere Text (slow typing)", "Warm SE First Element Text (slow typing)"],
+    measures: ["searchEverywhere", "searchEverywhere_first_elements_added"],
+    projects: [
       "community/go-to-text-with-warmup/Editor/typingLetterByLetter",
       "community/go-to-text-with-warmup/Kotlin/typingLetterByLetter",
       "community/go-to-text-with-warmup/Runtime/typingLetterByLetter",
       "community/go-to-text-with-warmup-finished-embeddings/Runtime/typingLetterByLetter",
       "java/go-to-text-with-warmup/Runtime/typingLetterByLetter",
-      "intellij_commit/go-to-text/Kotlin/typingLetterByLetter",
-      "intellij_commit/go-to-text/Editor/typingLetterByLetter",
-      "intellij_commit/go-to-text/Runtime/typingLetterByLetter",
+
       "intellij_commit/go-to-text-with-warmup/Kotlin/typingLetterByLetter",
       "intellij_commit/go-to-text-with-warmup/Editor/typingLetterByLetter",
       "intellij_commit/go-to-text-with-warmup/Runtime/typingLetterByLetter",
       "intellij_commit/go-to-text-with-warmup-finished-embeddings/Runtime/typingLetterByLetter",
+
+      "intellij_commit/new-se-go-to-text-with-warmup/Kotlin/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-text-with-warmup/Editor/typingLetterByLetter",
+      "intellij_commit/new-se-go-to-text-with-warmup/Runtime/typingLetterByLetter",
     ],
   },
 ]

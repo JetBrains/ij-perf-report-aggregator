10260adabaced42edcfc0540254055c7e71ae6bf
bartlomiej.filipiuk <bartlomiej.filipiuk@jetbrains.com>
2025-11-10 10:21:26 +0100

    CPP-47093 Added lagging metrics during completion and navigation tests

--- dashboard/new-dashboard/src/components/clion/CLionLaggingLatencyDashboard.vue
+++ dashboard/new-dashboard/src/components/clion/CLionLaggingLatencyDashboard.vue
@@ -4,13 +4,28 @@
     table="clion"
     persistent-id="clion_lagging_latency_dashboard"
     initial-machine="Linux EC2 C6id.8xlarge (32 vCPU Xeon, 64 GB)"
-    :charts="laggingCharts"
     :with-installer="false"
   >
     <section>
       <Divider title="Lagging during indexing" />
       <GroupProjectsChart
-        v-for="chart in laggingCharts"
+        v-for="chart in laggingIndexingChartsCombined"
+        :key="chart.definition.label"
+        :label="chart.definition.label"
+        :measure="chart.definition.measure"
+        :projects="chart.projects"
+      />
+      <Divider title="Lagging during completion" />
+      <GroupProjectsChart
+        v-for="chart in laggingCompletionChartsCombined"
+        :key="chart.definition.label"
+        :label="chart.definition.label"
+        :measure="chart.definition.measure"
+        :projects="chart.projects"
+      />
+      <Divider title="Lagging during navigation" />
+      <GroupProjectsChart
+        v-for="chart in laggingNavigationChartsCombined"
         :key="chart.definition.label"
         :label="chart.definition.label"
         :measure="chart.definition.measure"
@@ -41,5 +56,57 @@ const laggingIndexingCharts: ChartDefinition[] = [
   },
 ]
 
-const laggingCharts = combineCharts(laggingIndexingCharts)
+const laggingCompletionCharts: ChartDefinition[] = [
+  {
+    labels: ["Lagging during completion"],
+    measures: [["ui.lagging#average", "ui.lagging#max", "ui.lagging#sum"]],
+    projects: [
+      "radler/fmtlib/completion/fmt.join_view (dep) (hot)",
+      "radler/fmtlib/completion/std.shared_ptr (dep) (hot)",
+      "radler/fmtlib/completion/std.string (cold)",
+      "radler/fmtlib/completion/std.string (hot)",
+    ],
+    aliases: ["fmt.join_view", "std.shared_ptr (dep) (hot)", "std.string (cold)", "std.string (hot)"],
+  },
+  {
+    labels: ["Lags count during completion"],
+    measures: ["ui.lagging#count"],
+    projects: [
+      "radler/fmtlib/completion/fmt.join_view (dep) (hot)",
+      "radler/fmtlib/completion/std.shared_ptr (dep) (hot)",
+      "radler/fmtlib/completion/std.string (cold)",
+      "radler/fmtlib/completion/std.string (hot)",
+    ],
+    aliases: ["fmt.join_view", "std.shared_ptr (dep) (hot)", "std.string (cold)", "std.string (hot)"],
+  },
+]
+
+const laggingNavigationCharts: ChartDefinition[] = [
+  {
+    labels: ["Lagging during navigation"],
+    measures: [["ui.lagging#average", "ui.lagging#max", "ui.lagging#sum"]],
+    projects: [
+      "radler/luau/findUsages/class template (DenseHashTable)",
+      "radler/luau/findUsages/enumerable (LuauOpcode)",
+      "radler/luau/gotoDeclaration/time.h",
+      "radler/luau/gotoDeclaration/TypeChecker.getScopes",
+    ],
+    aliases: ["class template (DenseHashTable)", "enumerable (LuauOpcode)", "time.h", "TypeChecker.getScopes"],
+  },
+  {
+    labels: ["Lags count during navigation"],
+    measures: ["ui.lagging#count"],
+    projects: [
+      "radler/luau/findUsages/class template (DenseHashTable)",
+      "radler/luau/findUsages/enumerable (LuauOpcode)",
+      "radler/luau/gotoDeclaration/time.h",
+      "radler/luau/gotoDeclaration/TypeChecker.getScopes",
+    ],
+    aliases: ["class template (DenseHashTable)", "enumerable (LuauOpcode)", "time.h", "TypeChecker.getScopes"],
+  },
+]
+
+const laggingIndexingChartsCombined = combineCharts(laggingIndexingCharts)
+const laggingCompletionChartsCombined = combineCharts(laggingCompletionCharts)
+const laggingNavigationChartsCombined = combineCharts(laggingNavigationCharts)
 </script>

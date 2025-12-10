ff12842aa2f72e9df70886fefb149e07a5e2036b
Yury Suponeu <yury.suponeu@jetbrains.com>
2025-12-10 17:19:47 +0100

    [kmt] KQA-2108 add kmt alerts settings skeleton

---
+++ pkg/degradation-detector/setting/kmtSettings.go
@@ -0,0 +1,56 @@
+package setting
+
+import (
+	"log/slog"
+	"net/http"
+
+	detector "github.com/JetBrains/ij-perf-report-aggregator/pkg/degradation-detector"
+)
+
+func GenerateKotlinMultiplatformToolingSettings(backendUrl string, client *http.Client) []detector.PerformanceSettings {
+	baseSettings := detector.PerformanceSettings{
+		Db:    "perfintDev",
+		Table: "kmt",
+		BaseSettings: detector.BaseSettings{
+			Branch:  "master",
+			Machine: "cidr.appcode.performance%",
+		},
+	}
+	tests, err := detector.FetchAllTests(backendUrl, client, baseSettings)
+	settings := make([]detector.PerformanceSettings, 0, 100)
+	if err != nil {
+		slog.Error("error while getting tests", "error", err)
+		return settings
+	}
+	for _, test := range tests {
+		metrics := []string{
+			"Create KMP Run Configurations",
+			"Progress: Generating Xcode files…",
+			"globalInspections",
+			"SourceKitDiagnosticsPass#mean_value",
+			"SourceKitSemanticHighlightingPass#mean_value",
+			"completion",
+			"XCodeBuild",
+			"IosAppStartup",
+			"IosAppStartupDebug",
+			"KmpIosConfigurationRun",
+		}
+		for _, metric := range metrics {
+			settings = append(settings, detector.PerformanceSettings{
+				Db:      baseSettings.Db,
+				Table:   baseSettings.Table,
+				Project: test,
+				BaseSettings: detector.BaseSettings{
+					Branch:  baseSettings.Branch,
+					Machine: baseSettings.Machine,
+					Metric:  metric,
+					SlackSettings: detector.SlackSettings{
+						Channel:     "kmt-infrastructure-alerts",
+						ProductLink: "kmt",
+					},
+				},
+			})
+		}
+	}
+	return settings
+}

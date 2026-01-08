1479dd7d17c02a6bad9403920780d3c794f6c7b6
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2026-01-08 10:58:17 +0100

    make degradation detector parallelized

--- pkg/degradation-detector/inferDegradations.go
+++ pkg/degradation-detector/inferDegradations.go
@@ -1,5 +1,12 @@
 package degradation_detector
 
+import (
+	"runtime"
+	"sync"
+
+	"github.com/alitto/pond"
+)
+
 type DegradationWithSettings struct {
 	Details  Degradation
 	Settings Settings
@@ -8,11 +15,19 @@ type DegradationWithSettings struct {
 func InferDegradations(data <-chan QueryResultWithSettings) <-chan DegradationWithSettings {
 	degradationChan := make(chan DegradationWithSettings, 100)
 	go func() {
+		var wg sync.WaitGroup
+		pool := pond.New(runtime.NumCPU(), 1000)
+		defer pool.StopAndWait()
 		for datum := range data {
-			for _, degradation := range detectDegradations(datum.values, datum.builds, datum.timestamps, datum.Settings) {
-				degradationChan <- DegradationWithSettings{Details: degradation, Settings: datum.Settings}
-			}
+			wg.Add(1)
+			pool.Submit(func() {
+				defer wg.Done()
+				for _, degradation := range detectDegradations(datum.values, datum.builds, datum.timestamps, datum.Settings) {
+					degradationChan <- DegradationWithSettings{Details: degradation, Settings: datum.Settings}
+				}
+			})
 		}
+		wg.Wait()
 		close(degradationChan)
 	}()
 	return degradationChan

b209a0c6dfe9537b5f9f0eab68c056cea35eb34b
maksim.semenov <maksim.semenov@jetbrains.com>
2026-03-09 17:01:17 +0100

    Fix formatting in `DebuggerDashboard.vue` and clean up `RustTestCases.ts`

--- dashboard/new-dashboard/src/components/rust/DebuggerDashboard.vue
+++ dashboard/new-dashboard/src/components/rust/DebuggerDashboard.vue
@@ -4,40 +4,41 @@
     table="rust"
     persistent-id="rust_debugger_dashboard"
     initial-machine="Linux EC2 C6id.8xlarge (32 vCPU Xeon, 64 GB)"
-    :with-installer="false">
+    :with-installer="false"
+  >
     <section>
       <GroupProjectsChart
         label="Launch Debug"
         measure="debugRunConfiguration"
-        :projects=rustDebuggerTestCases
+        :projects="rustDebuggerTestCases"
       />
     </section>
     <section>
       <GroupProjectsChart
         label="Step Into"
         measure="debugStep_into"
-        :projects=rustDebuggerTestCases
+        :projects="rustDebuggerTestCases"
       />
     </section>
     <section>
       <GroupProjectsChart
         label="Step Out"
         measure="debugStep_out"
-        :projects=rustDebuggerTestCases
+        :projects="rustDebuggerTestCases"
       />
     </section>
     <section>
       <GroupProjectsChart
         label="Step Over"
         measure="debugStep_over"
-        :projects=rustDebuggerTestCases
+        :projects="rustDebuggerTestCases"
       />
     </section>
     <section>
       <GroupProjectsChart
         label="Evaluate Expression"
         measure="evaluateExpression"
-        :projects=rustDebuggerTestCases
+        :projects="rustDebuggerTestCases"
       />
     </section>
   </DashboardPage>

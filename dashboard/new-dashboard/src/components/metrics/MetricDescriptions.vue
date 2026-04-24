715919ef5fb6abd9cc0cac75048e60ab02a2b9da
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2026-04-24 15:07:51 +0200

    Add notion of main metrics to MetricDescriptions.vue

--- dashboard/new-dashboard/src/components/metrics/MetricDescriptions.vue
+++ dashboard/new-dashboard/src/components/metrics/MetricDescriptions.vue
@@ -4,7 +4,7 @@
     :value="metricsLists"
     striped-rows
     paginator
-    :rows="30"
+    :rows="100"
     :table-style="{ 'min-width': '50rem' }"
     class="p-datatable-sm"
     filter-display="row"
@@ -13,6 +13,7 @@
       field="name"
       header="Name"
       :sortable="true"
+      :show-filter-menu="false"
     >
       <template #filter="{ filterModel, filterCallback }">
         <InputText
@@ -22,6 +23,43 @@
           placeholder="Search by name"
           @input="filterCallback()"
         />
+      </template>
+    </Column>
+    <Column
+      field="isMain"
+      header="Main"
+      :sortable="true"
+      :show-filter-menu="false"
+      :style="{ width: '9rem' }"
+    >
+      <template #body="slotProps">
+        <span
+          v-if="slotProps.data.isMain"
+          class="inline-block rounded bg-blue-100 px-2 py-0.5 text-xs font-medium text-blue-800"
+        >
+          Main
+        </span>
+      </template>
+      <template #filter="{ filterModel, filterCallback }">
+        <div class="flex items-center gap-2">
+          <Checkbox
+            input-id="main-metrics-filter"
+            :model-value="filterModel.value === true"
+            binary
+            @update:model-value="
+              (v) => {
+                filterModel.value = v ? true : null
+                filterCallback()
+              }
+            "
+          />
+          <label
+            for="main-metrics-filter"
+            class="text-xs text-gray-600"
+          >
+            Only main
+          </label>
+        </div>
       </template>
     </Column>
     <Column
@@ -50,28 +88,42 @@
 import { FilterMatchMode } from "@primevue/core/api"
 import { ref } from "vue"
 import { metricsDescription } from "../../shared/metricsDescription"
+import { MAIN_METRICS } from "../../util/mainMetrics"
+
+const mainMetricsSet = new Set(MAIN_METRICS)
+
+function isMainMetric(name: string): boolean {
+  if (mainMetricsSet.has(name)) return true
+  const hashIdx = name.indexOf("#")
+  return hashIdx > 0 && mainMetricsSet.has(name.slice(0, hashIdx))
+}
 
 const filters = ref({
   name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+  isMain: { value: null, matchMode: FilterMatchMode.EQUALS },
 })
 
 interface Metric {
   name: string
   description: string
   url?: string
+  isMain: boolean
 }
 const metricsLists: Metric[] = []
 for (const [name, value] of metricsDescription) {
+  const isMain = isMainMetric(name)
   if (typeof value === "string") {
     metricsLists.push({
       name,
       description: value,
+      isMain,
     })
   } else {
     metricsLists.push({
       name,
       description: value.description,
       url: value.url,
+      isMain,
     })
   }
 }

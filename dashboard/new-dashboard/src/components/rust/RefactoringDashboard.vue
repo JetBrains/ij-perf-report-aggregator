30795683ae6bf2dad2177cb1cd5ea61f8328c2f0
maksim.semenov <maksim.semenov@jetbrains.com>
2026-05-12 13:42:36 +0200

    Add "RustRover Refactoring Dashboard"

---
+++ dashboard/new-dashboard/src/components/rust/RefactoringDashboard.vue
@@ -0,0 +1,58 @@
+<template>
+  <DashboardPage
+    db-name="perfintDev"
+    table="rust"
+    persistent-id="rust_refactoring_dashboard"
+    initial-machine="Linux EC2 C6id.8xlarge (32 vCPU Xeon, 64 GB)"
+    :with-installer="false"
+  >
+    <section>
+      <GroupProjectsChart
+        label="Rename Low Use Function"
+        measure="performInlineRename#mean_value"
+        :projects="rustRefactoringTestCases.renameLowUseFn"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Rename High Use Function"
+        measure="performInlineRename#mean_value"
+        :projects="rustRefactoringTestCases.renameHighUseFn"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Rename Low Use Struct"
+        measure="performInlineRename#mean_value"
+        :projects="rustRefactoringTestCases.renameLowUseStruct"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Rename High Use Struct"
+        measure="performInlineRename#mean_value"
+        :projects="rustRefactoringTestCases.renameHighUseStruct"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Optimize Imports Small File"
+        measure="execute_editor_optimizeimports"
+        :projects="rustRefactoringTestCases.optimizeImportsSmallFile"
+      />
+    </section>
+    <section>
+      <GroupProjectsChart
+        label="Optimize Imports Big File"
+        measure="execute_editor_optimizeimports"
+        :projects="rustRefactoringTestCases.optimizeImportsBigFile"
+      />
+    </section>
+  </DashboardPage>
+</template>
+
+<script setup lang="ts">
+import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
+import DashboardPage from "../common/DashboardPage.vue"
+import { rustRefactoringTestCases } from "./RustTestCases"
+</script>

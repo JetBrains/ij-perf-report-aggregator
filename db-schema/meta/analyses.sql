f73911cda310eab29029a98a1173645dbc5abcc6
Alexander.Danilov <danilovalex.d@gmail.com>
2026-05-19 15:57:38 +0200

    LLM Analysis: guard inpput from llm to db

--- db-schema/meta/analyses.sql
+++ db-schema/meta/analyses.sql
@@ -18,7 +18,7 @@ CREATE TABLE analyses
   yt_issue_id           VARCHAR(20),
   state                 VARCHAR(50)  NOT NULL DEFAULT 'in_progress',
   llm_guilty_commits    VARCHAR(40)[],
-  llm_comment           TEXT,
+  llm_comment           VARCHAR(8000),
   total_cost_usd        NUMERIC(10, 4)
 );
 

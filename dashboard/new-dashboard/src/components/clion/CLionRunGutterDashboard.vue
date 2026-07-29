95340b8146a6b39cda9f0957e1f5ce47540b41f0
Maksim Banaev <Maxim.Banaev@jetbrains.com>
2026-07-29 18:24:50 +0200

    CPP-51450 Get rid of Classic vs Radler comparison in Performance Tests Dashboard

--- dashboard/new-dashboard/src/components/clion/CLionRunGutterDashboard.vue
+++ dashboard/new-dashboard/src/components/clion/CLionRunGutterDashboard.vue
@@ -10,77 +10,8 @@
 
     <section class="flex gap-x-6 flex-col md:flex-row">
       <div class="flex-1 min-w-0">
-        <CLionVsRadlerGroupProjectsChart
-          label="Time to show test gutter (luau, AstQuery.test.cpp)"
-          measure="waitFirstTestGutter"
-          project="luau/checkLocalTestConfig/AstQuery.test.cpp.marks"
-        />
-      </div>
-
-      <div class="flex-1 min-w-0">
-        <CLionVsRadlerGroupProjectsChart
-          label="Time to show test gutter (luau, Linter.test.cpp)"
-          measure="waitFirstTestGutter"
-          project="luau/checkLocalTestConfig/Linter.test.cpp.marks"
-        />
-      </div>
-    </section>
-
-    <section class="flex gap-x-6 flex-col md:flex-row">
-      <div class="flex-1 min-w-0">
-        <CLionVsRadlerGroupProjectsChart
-          label="Time to show test gutter (luau, Repl.test.cpp)"
-          measure="waitFirstTestGutter"
-          project="luau/checkLocalTestConfig/Repl.test.cpp.marks"
-        />
-      </div>
-
-      <div class="flex-1 min-w-0">
-        <CLionVsRadlerGroupProjectsChart
-          label="Time to show test gutter (luau, TypeInfer.unionTypes.test.cpp)"
-          measure="waitFirstTestGutter"
-          project="luau/checkLocalTestConfig/TypeInfer.unionTypes.test.cpp.marks"
-        />
-      </div>
-    </section>
-
-    <section class="flex gap-x-6 flex-col md:flex-row">
-      <div class="flex-1 min-w-0">
-        <CLionVsRadlerGroupProjectsChart
-          label="Time to show test gutter (openCV, test_houghlines.cpp)"
-          measure="waitFirstTestGutter"
-          project="opencv/checkLocalTestConfig/test.houghlines.cpp.marks"
-        />
-      </div>
-
-      <div class="flex-1 min-w-0">
-        <CLionVsRadlerGroupProjectsChart
-          label="Time to show test gutter (openCV, test_kalman.cpp)"
-          measure="waitFirstTestGutter"
-          project="opencv/checkLocalTestConfig/test.kalman.cpp.marks"
-        />
-      </div>
-    </section>
-
-    <section class="flex gap-x-6 flex-col md:flex-row">
-      <div class="flex-1 min-w-0">
         <GroupProjectsChart
-          label="[CLion] Time to show test gutter (all tests)"
-          measure="waitFirstTestGutter"
-          :projects="[
-            'clion/luau/checkLocalTestConfig/AstQuery.test.cpp.marks',
-            'clion/luau/checkLocalTestConfig/Linter.test.cpp.marks',
-            'clion/luau/checkLocalTestConfig/Repl.test.cpp.marks',
-            'clion/luau/checkLocalTestConfig/TypeInfer.unionTypes.test.cpp.marks',
-            'clion/opencv/checkLocalTestConfig/test.houghlines.cpp.marks',
-            'clion/opencv/checkLocalTestConfig/test.kalman.cpp.marks',
-          ]"
-        />
-      </div>
-
-      <div class="flex-1 min-w-0">
-        <GroupProjectsChart
-          label="[Radler] Time to show test gutter (all tests)"
+          label="Time to show test gutter (all tests)"
           measure="waitFirstTestGutter"
           :projects="[
             'radler/luau/checkLocalTestConfig/AstQuery.test.cpp.marks',
@@ -98,24 +29,28 @@
 
     <section class="flex gap-x-6 flex-col md:flex-row">
       <div class="flex-1 min-w-0">
-        <CLionVsRadlerGroupProjectsChart
+        <GroupProjectsChart
           label="Time to Show Context menu in Project View for a single file"
           measure="fus_popup_latency_ms"
-          project="llvm/showPopupMenu/SingleSelection"
+          :projects="['radler/llvm/showPopupMenu/SingleSelection']"
         />
       </div>
+    </section>
+    <section class="flex gap-x-6 flex-col md:flex-row">
       <div class="flex-1 min-w-0">
-        <CLionVsRadlerGroupProjectsChart
+        <GroupProjectsChart
           label="Time to Show Context menu in Project View for multiple files"
           measure="fus_popup_latency_ms"
-          project="llvm/showPopupMenu/MultipleSelection"
+          :projects="['radler/llvm/showPopupMenu/MultipleSelection']"
         />
       </div>
+    </section>
+    <section class="flex gap-x-6 flex-col md:flex-row">
       <div class="flex-1 min-w-0">
-        <CLionVsRadlerGroupProjectsChart
+        <GroupProjectsChart
           label="Time to Show Context menu in Project View for multiple files during indexing"
           measure="fus_popup_latency_ms"
-          project="llvm/showPopupMenu/MultipleSelectionDuringIndexing"
+          :projects="['radler/llvm/showPopupMenu/MultipleSelectionDuringIndexing']"
         />
       </div>
     </section>
@@ -128,5 +63,4 @@
 import GroupProjectsChart from "../charts/GroupProjectsChart.vue"
 import DashboardPage from "../common/DashboardPage.vue"
 import Divider from "../common/Divider.vue"
-import CLionVsRadlerGroupProjectsChart from "./CLionVsRadlerGroupProjectsChart.vue"
 </script>

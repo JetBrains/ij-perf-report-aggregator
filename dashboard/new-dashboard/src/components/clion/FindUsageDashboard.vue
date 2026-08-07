4c6666eb9e37e261d19d6bd3ef54b421ffcd6954
Maksim Banaev <Maxim.Banaev@jetbrains.com>
2026-08-07 14:43:50 +0200

    clion: drop classic in Find Usage dashboard

--- dashboard/new-dashboard/src/components/clion/FindUsageDashboard.vue
+++ dashboard/new-dashboard/src/components/clion/FindUsageDashboard.vue
@@ -11,16 +11,16 @@
         <GroupProjectsChart
           label="Total Execution Time: Find Usages (luau, enumerable)"
           :measure="['%syncAction FindUsages', 'findUsagesInToolWindow']"
-          :projects="['clion/luau/findUsages/enumerable (LuauOpcode)', 'radler/luau/findUsages/enumerable (LuauOpcode)']"
-          :aliases="['clion/luau/findUsages/enumerable (LuauOpcode)', 'radler/luau/findUsages/enumerable (LuauOpcode)']"
+          :projects="['radler/luau/findUsages/enumerable (LuauOpcode)']"
+          :aliases="['radler/luau/findUsages/enumerable (LuauOpcode)']"
         />
       </div>
       <div class="flex-1 min-w-0">
         <GroupProjectsChart
           label="Total Execution Time: Find Usages (luau, enumerator)"
           :measure="['%syncAction FindUsages', 'findUsagesInToolWindow']"
-          :projects="['clion/luau/findUsages/enumerator (LOP_NOP)', 'radler/luau/findUsages/enumerator (LOP_NOP)']"
-          :aliases="['clion/luau/findUsages/enumerator (LOP_NOP)', 'radler/luau/findUsages/enumerator (LOP_NOP)']"
+          :projects="['radler/luau/findUsages/enumerator (LOP_NOP)']"
+          :aliases="['radler/luau/findUsages/enumerator (LOP_NOP)']"
         />
       </div>
     </section>
@@ -29,16 +29,16 @@
         <GroupProjectsChart
           label="Total Execution Time: Find Usages (luau, class template)"
           :measure="['%syncAction FindUsages', 'findUsagesInToolWindow']"
-          :projects="['clion/luau/findUsages/class template (DenseHashTable)', 'radler/luau/findUsages/class template (DenseHashTable)']"
-          :aliases="['clion/luau/findUsages/class template (DenseHashTable)', 'radler/luau/findUsages/class template (DenseHashTable)']"
+          :projects="['radler/luau/findUsages/class template (DenseHashTable)']"
+          :aliases="['radler/luau/findUsages/class template (DenseHashTable)']"
         />
       </div>
       <div class="flex-1 min-w-0">
         <GroupProjectsChart
           label="Total Execution Time: Find Usages (luau, macro)"
           :measure="['%syncAction FindUsages', 'findUsagesInToolWindow']"
-          :projects="['clion/luau/findUsages/macro (LUAU_ASSERT)', 'radler/luau/findUsages/macro (LUAU_ASSERT)']"
-          :aliases="['clion/luau/findUsages/macro (LUAU_ASSERT)', 'radler/luau/findUsages/macro (LUAU_ASSERT)']"
+          :projects="['radler/luau/findUsages/macro (LUAU_ASSERT)']"
+          :aliases="['radler/luau/findUsages/macro (LUAU_ASSERT)']"
         />
       </div>
     </section>
@@ -47,31 +47,31 @@
         <GroupProjectsChart
           label="Total Execution Time: Find Usages (cmake, class)"
           :measure="['%syncAction FindUsages', 'findUsagesInToolWindow']"
-          :projects="['clion/cmake/findUsages/class (cmCTestResourceAllocator)', 'radler/cmake/findUsages/class (cmCTestResourceAllocator)']"
-          :aliases="['clion/cmake/findUsages/class (cmCTestResourceAllocator)', 'radler/cmake/findUsages/class (cmCTestResourceAllocator)']"
+          :projects="['radler/cmake/findUsages/class (cmCTestResourceAllocator)']"
+          :aliases="['radler/cmake/findUsages/class (cmCTestResourceAllocator)']"
         />
       </div>
       <div class="flex-1 min-w-0">
         <GroupProjectsChart
           label="Total Execution Time: Find Usages (cmake, macro)"
           :measure="['%syncAction FindUsages', 'findUsagesInToolWindow']"
-          :projects="['clion/cmake/findUsages/macro (SAFEDIV)', 'radler/cmake/findUsages/macro (SAFEDIV)']"
-          :aliases="['clion/cmake/findUsages/macro (SAFEDIV)', 'radler/cmake/findUsages/macro (SAFEDIV)']"
+          :projects="['radler/cmake/findUsages/macro (SAFEDIV)']"
+          :aliases="['radler/cmake/findUsages/macro (SAFEDIV)']"
         />
       </div>
       <div class="flex-1 min-w-0">
         <GroupProjectsChart
           label="Total Execution Time: Find Usages (cmake, member)"
           :measure="['%syncAction FindUsages', 'findUsagesInToolWindow']"
-          :projects="['clion/cmake/findUsages/member (SlotsNeeded)', 'radler/cmake/findUsages/member (SlotsNeeded)']"
-          :aliases="['clion/cmake/findUsages/member (SlotsNeeded)', 'radler/cmake/findUsages/member (SlotsNeeded)']"
+          :projects="['radler/cmake/findUsages/member (SlotsNeeded)']"
+          :aliases="['radler/cmake/findUsages/member (SlotsNeeded)']"
         />
       </div>
     </section>
     <section class="flex gap-x-6 flex-col md:flex-row">
       <div class="flex-1 min-w-0">
         <GroupProjectsChart
-          label="[Radler] Total Execution Time"
+          label="Total Execution Time"
           :measure="['%syncAction FindUsages', 'findUsagesInToolWindow']"
           :projects="[
             'radler/luau/findUsages/enumerable (LuauOpcode)',
@@ -97,7 +97,7 @@
     <section class="flex gap-x-6 flex-col md:flex-row">
       <div class="flex-1 min-w-0">
         <GroupProjectsChart
-          label="[Radler] Number of Usages"
+          label="Number of Usages"
           measure="findUsagesInToolWindow#number"
           :projects="[
             'radler/luau/findUsages/enumerable (LuauOpcode)',

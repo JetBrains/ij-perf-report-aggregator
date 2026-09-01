7c6c9fa6326ad95382180a5af9f970ba01e9cbf9
Victor Turansky <victor.turansky@gmail.com>
2026-09-01 18:46:14 +0200

    [webstorm] fix formatting

--- dashboard/new-dashboard/src/components/webstorm/PerformanceDashboardBuiltInVsNEXT.vue
+++ dashboard/new-dashboard/src/components/webstorm/PerformanceDashboardBuiltInVsNEXT.vue
@@ -132,7 +132,13 @@ const groups = [
   {
     label: "Typing",
     measure: "typing",
-    projects: ["eslint-plugin-jest/typing/eslintPluginJest", "axios/typing/axios", "toh-pt6/typing/toh-pt6", "react-todo-js/typing/react-todo", "vue-template/typing/vue-template"],
+    projects: [
+      "eslint-plugin-jest/typing/eslintPluginJest",
+      "axios/typing/axios",
+      "toh-pt6/typing/toh-pt6",
+      "react-todo-js/typing/react-todo",
+      "vue-template/typing/vue-template",
+    ],
   },
   {
     label: "Completion First Element",

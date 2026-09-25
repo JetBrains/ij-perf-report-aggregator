fd39554ebc094b361ff7547ae28df52cfb7ccdae
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2026-09-25 17:49:00 +0200

    Fix util.Close swallowing all errors except ErrClosedPipe

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -9,11 +9,7 @@ import (
 
 func Close(c io.Closer) {
 	err := c.Close()
-	if err != nil && !errors.Is(err, os.ErrClosed) && errors.Is(err, io.ErrClosedPipe) {
-		var pathError *os.PathError
-		if errors.As(err, &pathError) && errors.Is(pathError, os.ErrClosed) {
-			return
-		}
+	if err != nil && !errors.Is(err, os.ErrClosed) && !errors.Is(err, io.ErrClosedPipe) {
 		slog.Error("cannot close", "error", err)
 	}
 }
